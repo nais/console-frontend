c86568f55c2c2a0b92e88f2d5c0b9a461280a37e
Roger Bjørnstad <roger.bjornstad@nav.no>
2023-08-16 14:06:13 +0200

    Logs (#5)
    add page for app container logs

---
+++ src/routes/team/[team]/[env]/app/[app]/instance/[instance]/log/+page.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+	import Card from '$lib/Card.svelte';
+
+	import type { PageData } from './$houdini';
+	import { page } from '$app/stores';
+	import { Alert } from '@nais/ds-svelte-community';
+	import { PendingValue } from '$houdini';
+	import Loading from '$lib/Loading.svelte';
+
+	export let data: PageData;
+	let instanceName = $page.params.instance;
+	$: ({ AppLog } = data);
+
+	function getLogLevel(message: string) {
+		const logLevel = message.match(/"level":"(\w+)"/);
+		if (logLevel) {
+			return logLevel[1].toUpperCase();
+		}
+		return 'INFO';
+	}
+</script>
+
+{#if $AppLog.errors}
+	<Alert variant="error">
+		{#each $AppLog.errors as error}
+			{error.message}
+		{/each}
+	</Alert>
+{:else if $AppLog.data}
+	<Card>
+		<h4>{instanceName}</h4>
+		{#if $AppLog.data.app.name === PendingValue}
+			{#each new Array(5) as _, index}
+				<div class="log" id="ll-{index}">
+					<div class="logline">
+						<a class="index" href="#ll-{index}">{index}</a>
+						<span class="timestamp">
+							<Loading />
+						</span>
+						<span class="level"><Loading /></span>
+						<span class="message">
+							<Loading />
+						</span>
+					</div>
+				</div>
+			{/each}
+		{:else}
+			{#each $AppLog.data.app.instances as instance}
+				{#if instanceName === instance.name}
+					{#each instance.log as log, index}
+						<div class="log" id="ll-{index}">
+							<div class="logline">
+								<a class="index" href="#ll-{index}">{index}</a>
+								<span class="timestamp">
+									{log.time.toLocaleString()}
+								</span>
+								<span class="level {getLogLevel(log.message)}">{getLogLevel(log.message)}</span>
+								<span class="message">
+									{log.message}
+								</span>
+							</div>
+						</div>
+					{/each}
+				{/if}
+			{/each}
+		{/if}
+	</Card>
+{/if}
+
+<style>
+	.log {
+		color: rgb(31, 35, 40);
+		color-scheme: light;
+		display: block;
+		font-family: ui-monospace, SFMono-Regular, 'SF Mono', Menlo, Consolas, 'Liberation Mono',
+			monospace;
+		font-size: 12px;
+		line-height: 18px;
+		padding-bottom: 4px;
+		padding-top: 4px;
+	}
+
+	.logline {
+		display: flex;
+		flex-direction: row;
+	}
+	.log a {
+		color: silver;
+		text-decoration: none;
+		min-width: 20px;
+	}
+
+	.timestamp {
+		color: rgb(0, 0, 0);
+		display: inline-block;
+		margin-right: 8px;
+		min-width: 180px;
+		text-align: right;
+	}
+
+	.level {
+		color: rgb(0, 0, 0);
+		display: inline-block;
+		margin-right: 8px;
+		min-width: 50px;
+		text-align: left;
+	}
+
+	.ERROR {
+		color: rgb(255, 0, 0);
+	}
+
+	.WARN {
+		color: rgb(255, 165, 0);
+	}
+
+	.message {
+		overflow-wrap: break-word;
+		overflow-x: auto;
+	}
+</style>
