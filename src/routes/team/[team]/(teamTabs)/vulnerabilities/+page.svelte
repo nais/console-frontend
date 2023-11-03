58ba07911ef807fa970e1a312f71fcf2f7a4a3e4
Tommy Trøen <tommy.troen@nav.no>
2023-11-03 14:55:07 +0100

    wip: add sorting
    Co-authored-by: ybelmekk <youssef.bel.mekki@nav.no>
    Co-authored-by: sindrerh2 <sindre.rodseth.hansen@nav.no>

--- src/routes/team/[team]/(teamTabs)/vulnerabilities/+page.svelte
+++ src/routes/team/[team]/(teamTabs)/vulnerabilities/+page.svelte
@@ -10,87 +10,59 @@
     import {afterNavigate} from "$app/navigation";
     import {logEvent} from "$lib/amplitude";
 
-
     $: teamName = $page.params.team;
     export let data: PageData;
     $: ({TeamVulnerabilities} = data);
     $: team = $TeamVulnerabilities.data?.team;
 
+    let sortKey = {
+        key: 'name',
+        reverse: true
+    };
 
-    function sortBy(type: string, multiplier: number) {
+    const sortData = (key) => {
+        console.log('sort:' + key)
         if (team !== undefined) {
+            if (sortKey.key === key) {
+                sortKey.reverse = !sortKey.reverse;
+            } else {
+                delete sortKey.reverse;
+                sortKey.key = key;
+            }
             team.apps.edges.sort((a, b) => {
-                if (a.node.name !== PendingValue && b.node.name !== PendingValue) {
-                    if (a.node.dependencyTrack !== null && b.node.dependencyTrack !== null) {
-                        if (type === "critical") {
-                            if (a.node.dependencyTrack.summary.critical > b.node.dependencyTrack.summary.critical) {
-                                return 1 * multiplier;
-                            }
-                            if (a.node.dependencyTrack.summary.critical < b.node.dependencyTrack.summary.critical) {
-                                return -1 * multiplier;
-                            }
+                    if (a.node.name !== PendingValue && b.node.name !== PendingValue) {
+                        if (key === 'env') {
+                            return (a.node[key].name > b.node[key].name ? 1 : -1) * (sortKey.reverse ? 1 : -1)
                         }
-                        if (type === "high") {
-                            if (a.node.dependencyTrack.summary.high > b.node.dependencyTrack.summary.high) {
-                                return 1 * multiplier;
-                            }
-                            if (a.node.dependencyTrack.summary.high < b.node.dependencyTrack.summary.high) {
-                                return -1 * multiplier;
-                            }
+                        if (key === 'name'){
+                            return (a.node[key] > b.node[key] ? 1 : -1) * (sortKey.reverse ? 1 : -1)
                         }
-                        if (type === "medium") {
-                            if (a.node.dependencyTrack.summary.medium > b.node.dependencyTrack.summary.medium) {
-                                return 1 * multiplier;
+                        if (['critical', 'high', 'medium', 'low'].includes(key)){
+                            if (a.node.dependencyTrack !== null && b.node.dependencyTrack !== null) {
+                                return (a.node.dependencyTrack.summary[key] > b.node.dependencyTrack.summary[key] ? 1 : -1) * (sortKey.reverse ? 1 : -1)
+                            } else if (a.node.dependencyTrack !== null && b.node.dependencyTrack === null) {
+                                return 1 * (sortKey.reverse ? 1 : -1);
+                            } else if (a.node.dependencyTrack === null && b.node.dependencyTrack !== null) {
+                                return -1 * (sortKey.reverse ? 1 : -1);
                             }
-                            if (a.node.dependencyTrack.summary.medium < b.node.dependencyTrack.summary.medium) {
-                                return -1 * multiplier;
-                            }
-                        }
-                        if (type === "low") {
-                            if (a.node.dependencyTrack.summary.low > b.node.dependencyTrack.summary.low) {
-                                return 1 * multiplier;
-                            }
-                            if (a.node.dependencyTrack.summary.low < b.node.dependencyTrack.summary.low) {
-                                return -1 * multiplier;
-                            }
-                        }
-
-                    } else if (a.node.dependencyTrack !== null && b.node.dependencyTrack === null) {
-                        return 1 * multiplier;
-                    } else if (a.node.dependencyTrack === null && b.node.dependencyTrack !== null) {
-                        return -1 * multiplier;
-                    }
-                    if (type === "name") {
-                        if (a.node.name > b.node.name) {
-                            return 1 * multiplier;
-                        }
-                        if (a.node.name < b.node.name) {
-                            return -1 * multiplier;
                         }
                     }
-                    if (type === "env") {
-                        if (a.node.env.name > b.node.env.name) {
-                            return 1 * multiplier;
-                        }
-                        if (a.node.env.name < b.node.env.name) {
-                            return -1 * multiplier;
-                        }
-                    }
+                    return 0;
                 }
-                return 0;
-            })
-            team = team;
+            );
+            team = team
         }
-    }
+    };
 
     afterNavigate((nav) => {
         let props = {};
         if (nav.to?.route.id != null) {
             props = {routeID: nav.to.route.id};
         }
-        console.log('page')
         logEvent('pageview', props);
     });
+
+
 </script>
 
 {#if $TeamVulnerabilities.errors}
@@ -106,31 +78,23 @@
                 <Thead>
                 <Th>
                     Name
-                    <!-- Sort by name LOW to HIGH -->
-                    <button class="triangle_upp" on:click={() => {sortBy("name", 1)}}></button>
-                    <!-- Sort by name HIGH to LOW -->
-                    <button class="triangle_down" on:click={() => {sortBy("name", -1)}}></button>
+                    <button on:click={() => sortData('name')} class="arrow {sortKey.reverse ? '' : 'up'}"></button>
                 </Th>
                 <Th>Env
-                    <button class="triangle_upp" on:click={() => {sortBy("env", 1)}}></button>
-                    <button class="triangle_down" on:click={() => {sortBy("env", -1)}}></button>
+                   <button on:click={() => sortData('env')} class="arrow {sortKey.reverse ? '' : 'up'}"></button>
                 </Th>
                 <Th>Findings</Th>
                 <Th>Critical
-                    <button class="triangle_upp" on:click={() => {sortBy("critical", 1)}}></button>
-                    <button class="triangle_down" on:click={() => {sortBy("critical", -1)}}></button>
+                    <button on:click={() => sortData('critical')} class="arrow {sortKey.reverse ? '' : 'up'}"></button>
                 </Th>
                 <Th>High
-                    <button class="triangle_upp" on:click={() => {sortBy("high", 1)}}></button>
-                    <button class="triangle_down" on:click={() => {sortBy("high", -1)}}></button>
+                    <button on:click={() => sortData('high')} class="arrow {sortKey.reverse ? '' : 'up'}"></button>
                 </Th>
                 <Th>Medium
-                    <button class="triangle_upp" on:click={() => {sortBy("medium", 1)}}></button>
-                    <button class="triangle_down" on:click={() => {sortBy("medium", -1)}}></button>
+                    <button on:click={() => sortData('medium')} class="arrow {sortKey.reverse ? '' : 'up'}"></button>
                 </Th>
                 <Th>Low
-                    <button class="triangle_upp" on:click={() => {sortBy("low", 1)}}></button>
-                    <button class="triangle_down" on:click={() => {sortBy("low", -1)}}></button>
+                    <button on:click={() => sortData('low')} class="arrow {sortKey.reverse ? '' : 'up'}"></button>
                 </Th>
                 </Thead>
                 <Tbody>
@@ -144,6 +108,7 @@
                             {/each}
                         </Tr>
                     {:else}
+
                         {#each team.apps.edges as edge}
                             <Tr>
                                 <Td>
@@ -218,36 +183,27 @@
         row-gap: 1rem;
     }
 
-    .vulnerabilities {
-        margin-top: 0.5rem;
-        grid-column: 1 / span 2;
-        grid-row: 3;
-    }
-
-    .vulnerabilities h5 {
-        margin-bottom: 0.5rem;
-    }
-
     .na {
         padding-left: 10px;
         color: lightslategray;
     }
 
-    .triangle_upp {
-        padding: 0px;
-        padding-top: 1px;
-        border-left: solid 5px transparent;
-        border-right: solid 5px transparent;
-        border-bottom: solid 10px lightslategray;
-        border-top: none;
+    .arrow {
+        margin-left: 5px;
+        border: solid #6e6e6e;
+        border-width: 0 3px 3px 0;
+        display: inline-block;
+        padding: 3px;
+        transform: rotate(45deg);
+        -webkit-transform: rotate(45deg);
+        line-height: 20px;
+        background: transparent;
+        cursor: pointer;
     }
 
-    .triangle_down {
-        padding: 0px;
-        padding-top: 1px;
-        border-left: solid 5px transparent;
-        border-right: solid 5px transparent;
-        border-bottom: solid 10px lightslategray;
-        border-top: none;
+    .up {
+        transform: rotate(-135deg);
+        -webkit-transform: rotate(-135deg);
     }
+
 </style>
