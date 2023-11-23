d4c9da936d401a2821e96eae2b1c55c3c29868ac
Roger Bjørnstad <roger.bjornstad@nav.no>
2023-11-23 13:32:05 +0100

    wip: add files

---
+++ src/lib/chart/types.ts
@@ -0,0 +1,28 @@
+export interface Overage {
+	readonly overage: number;
+	readonly env: string;
+	readonly team: string;
+	readonly app: string;
+}
+
+export interface Usage {
+	readonly timestamp: Date;
+	readonly usagePercentage: number;
+}
+
+export interface Utilization {
+	readonly env: string;
+	readonly cpu: Usage[];
+	readonly memory: Usage[];
+}
+
+export interface ResourceUtilizationApp {
+	readonly cpu: {
+		readonly timestamp: Date;
+		readonly usagePercentage: number;
+	}[];
+	readonly memory: {
+		readonly timestamp: Date;
+		readonly usagePercentage: number;
+	}[];
+}
