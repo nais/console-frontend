5f11c99541a75e33a7dab1e4510a13873bb3eebf
Roger Bjørnstad <roger.bjornstad@nav.no>
2023-12-07 12:54:05 +0100

    wip

--- src/routes/team/[team]/(teamTabs)/repositories/+page.svelte
+++ src/routes/team/[team]/(teamTabs)/repositories/+page.svelte
@@ -1,35 +1,107 @@
 <script lang="ts">
-	import { PendingValue } from '$houdini';
+	import { page } from '$app/stores';
+	import { OrderByField, PendingValue } from '$houdini';
 	import Card from '$lib/Card.svelte';
 	import Pagination from '$lib/Pagination.svelte';
-	import { Skeleton, Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
+	import {
+		HelpText,
+		Skeleton,
+		Table,
+		Tbody,
+		Td,
+		Th,
+		Thead,
+		Tr,
+		type TableSortState
+	} from '@nais/ds-svelte-community';
+	import { sortTable } from '../../../../../helpers';
 	import type { PageData } from './$houdini';
 
 	export let data: PageData;
 
 	$: ({ Repositories } = data);
 	$: team = $Repositories.data?.team;
+
+	$: console.log(team);
+
+	$: teamName = $page.params.team;
+
+	let sortState: TableSortState = {
+		orderBy: 'NAME',
+		direction: 'ascending'
+	};
+
+	const refetch = (key: string) => {
+		const field = Object.values(OrderByField).find((value) => value === key);
+		Repositories.fetch({
+			variables: {
+				team: teamName,
+				orderBy: {
+					field: field !== undefined ? field : 'NAME',
+					direction: sortState.direction === 'descending' ? 'DESC' : 'ASC'
+				}
+			}
+		});
+	};
+	/*
+	Triage
+Can read and clone this repository. Can also manage issues and pull requests.
+
+Read
+Can read and clone this repository. Can also open and comment on issues and
+										pull requests.
+
+										Write
+Can read, clone, and push to this repository. Can also manage issues and pull requests.
+
+Maintain
+Can read, clone, and push to this repository. They can also manage issues, pull requests, and some repository settings.
+
+Admin
+Can read, clone, and push to this repository. Can also manage issues, pull requests, and repository settings, including adding collaborators.
+
+ISOC Triage & Follow up
+Gir ISOC mulighet til å følge opp
+	*/
 </script>
 
 {#if team && team !== undefined}
 	<Card>
 		<h3>Authorized repositories</h3>
 
-		<Table size={'small'}>
+		<Table
+			size="small"
+			sort={sortState}
+			on:sortChange={(e) => {
+				const { key } = e.detail;
+				sortState = sortTable(key, sortState, refetch);
+			}}
+		>
 			<Thead>
 				<Tr>
-					<Th>Repository</Th>
+					<Th sortable={true} sortKey="NAME">Repository</Th>
+					<Th>Authorizations</Th>
+					<Th sortable={true} sortKey="ROLE">Role</Th>
 				</Tr>
 			</Thead>
 			<Tbody>
 				{#each team.githubRepositories.edges as repo}
 					<Tr>
 						{#if repo.node.name === PendingValue}
-							{#each new Array(1).fill('text') as variant}
+							{#each new Array(3).fill('text') as variant}
 								<Td><Skeleton {variant} /></Td>
 							{/each}
 						{:else}
 							<Td>{repo.node.name}</Td>
+							<Td>{repo.node.authorizations}</Td>
+							<Td
+								><div class="roleHelpText">
+									{repo.node.roleName}<HelpText title="Role description"
+										>Can read and clone this repository. Can also open and comment on issues and
+										pull requests.</HelpText
+									>
+								</div></Td
+							>
 						{/if}
 					</Tr>
 				{/each}
@@ -49,9 +121,13 @@
 			}}
 		/>
 	</Card>
-{:else}
-	<p>Loading...</p>
 {/if}
 
 <style>
+	.roleHelpText {
+		display: flex;
+		gap: 0.5rem;
+		font-family: monospace;
+		font-size: 1rem;
+	}
 </style>
