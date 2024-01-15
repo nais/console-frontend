fcde978b592e26e5bf2ff95d8a062c940c80fa76
Carl Hedgren <carl.hedgren@nav.no>
2024-01-15 12:59:40 +0100

    Add secretfield component

---
+++ src/lib/components/SecretField.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { EyeIcon } from "@nais/ds-svelte-community/icons";
+
+	let show = false; 
+    export let value: String;
+
+    function toggle() {
+        show = !show 
+    }
+</script>
+
+{#if show}
+<input type="text" bind:value={value} />
+{:else}
+<input type="password" bind:value={value} />
+{/if}
+<button on:click={toggle}><EyeIcon/></button>
+
+<style>
+    button {
+        background: transparent;
+        border: none; 
+        margin-left: -30px; 
+    }
+</style>
