38f55b2b11572e5da07eccb69757406ea36dd006
Carl Hedgren <carl.hedgren@nav.no>
2024-01-15 14:15:46 +0100

    Add a new-secret-entry component

---
+++ src/routes/team/[team]/(teamTabs)/secrets/NewSecretEntry.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Button, TextField } from '@nais/ds-svelte-community';
+</script>
+
+<div class="entry">
+	<div class="keyfield">
+		<TextField hideLabel size="small" htmlSize={14} placeholder="New value" />
+	</div>
+	<div>
+		<TextField hideLabel size="small" htmlSize={14} placeholder="New secret" />
+	</div>
+	<Button size="small">Add</Button>
+</div>
+
+<style>
+	.entry {
+		display: flex;
+	}
+</style>
