a868edb084c8e10bbca678d4e44a1c655fc9c9a1
Carl Hedgren <carl.hedgren@nav.no>
2024-01-17 15:48:34 +0100

    Secrets: Do a deep copy to reconcile new values
    todo: Merge the update results in when clicking the
    trash-save and reset the state.

--- src/lib/components/SecretField.svelte
+++ src/lib/components/SecretField.svelte
@@ -5,30 +5,42 @@
 	let show = false;
 	export let key: string;
 	export let value: string;
+	export let i: number, j: number, k: number;
+
+	export let update:
+		| {
+				env: string;
+				secrets: {
+					name: string;
+					id: string;
+					data: {
+						key: string;
+						value: string;
+					}[];
+				}[];
+		  }[]
+		| undefined;
+
 	function toggle() {
 		show = !show;
 	}
 
-	function updateKey(e) {
-		key = e.target.value;
+	function updateKv() {
+		console.log("secretfilec", update, i, j, k)
+		if (update) {
+			update[i].secrets[j].data[k] = { key: key, value: value };
+			show = !show;
+		}
 	}
 
-	function updateValue(e) {
-		value = e.target.value;
-	}
-
-	function deleteKv() {
-
-	}
+	let deleteKv = () => {}
 </script>
 
 {#if show}
 	<div class="entry">
-		<TextField hideLabel size="small" htmlSize={14} bind:value={key}
-							 on:change={updateKey}/>
-		<TextField hideLabel size="small" htmlSize={14} bind:value
-							 on:change={updateValue}/>
-		<Button size="small" on:click={toggle}><FloppydiskIcon /></Button>
+		<TextField hideLabel size="small" htmlSize={14} bind:value={key} />
+		<TextField hideLabel size="small" htmlSize={14} bind:value />
+		<Button size="small" on:click={updateKv}><FloppydiskIcon /></Button>
 		<Button size="small" on:click={deleteKv}><TrashIcon /></Button>
 	</div>
 {:else}
@@ -52,5 +64,5 @@
 <style>
 	.entry {
 		display: flex;
-  }
+	}
 </style>
--- src/routes/team/[team]/(teamTabs)/secrets/+page.svelte
+++ src/routes/team/[team]/(teamTabs)/secrets/+page.svelte
@@ -7,13 +7,39 @@
 	import { TrashIcon } from '@nais/ds-svelte-community/icons';
 	import NewSecretEntry from './NewSecretEntry.svelte';
 	import type { PageData } from './$houdini';
-	import { graphql } from '$houdini';
+	import { graphql, type Secrets$result } from '$houdini';
 	import { page } from '$app/stores';
 
 	export let data: PageData;
 
 	$: ({ Secrets } = data);
 
+	$: mkUpdate($Secrets.data?.secrets);
+    
+	type update =
+		| {
+				env: string;
+				secrets: {
+					name: string;
+					id: string;
+					data: {
+						key: string;
+						value: string;
+					}[];
+				}[];
+		  }[]
+		| undefined;
+
+	let update: update;
+
+	let mkUpdate = (secret: Secrets$result['secrets'] | undefined | null) => {
+		if ( !secret) {
+			return;
+		}
+		
+		update = JSON.parse(JSON.stringify(secret));
+		console.log("yes", update )
+	};
 	const deleteSecret = graphql(`
 		mutation deleteSecret($name: String!, $team: Slug!, $env: String!) {
 			deleteSecret(name: $name, team: $team, env: $env)
@@ -21,9 +47,14 @@
 	`);
 
 	const updateSecret = graphql(`
-		mutation updateSecret($name: String!, $team: Slug!, $env: String!, $data: [SecretTupleInput!]!) {
+		mutation updateSecret(
+			$name: String!
+			$team: Slug!
+			$env: String!
+			$data: [SecretTupleInput!]!
+		) {
 			updateSecret(name: $name, team: $team, env: $env, data: $data) {
-			 id
+				id
 			}
 		}
 	`);
@@ -33,7 +64,7 @@
 
 {#if $Secrets.data}
 	<div class="grid">
-		{#each $Secrets.data.secrets as secrets}
+		{#each $Secrets.data.secrets as secrets, i}
 			<Card columns={12}>
 				<h3>{secrets.env.name}</h3>
 				<Table size="small">
@@ -42,14 +73,14 @@
 						<Th>Name</Th>
 					</Thead>
 					<Tbody>
-						{#each secrets.secrets as secret}
+						{#each secrets.secrets as secret, j}
 							<TrExpander>
 								<svelte:fragment slot="row-content">
 									<Td>{secret.name}</Td>
 								</svelte:fragment>
 								<div slot="expander-content">
-									{#each secret.data as data}
-										<SecretField key={data.key} value={data.value} />
+									{#each secret.data as data, k}
+										<SecretField i={i} j={j} k={k} key={data.key} value={data.value} bind:update={update} />
 									{/each}
 									<NewSecretEntry></NewSecretEntry>
 									<div>
@@ -63,16 +94,33 @@
 											<p>Other App</p>
 										</details>
 									</div>
-									<Button variant="primary" size="small" on:click={async () => {
-									  await updateSecret.mutate({ name: secret.name, team: team, env: secrets.env.name, data: [] });
-										Secrets.fetch();
-									}}>
+									<Button
+										variant="primary"
+										size="small"
+										on:click={async () => {
+											await updateSecret.mutate({
+												name: secret.name,
+												team: team,
+												env: secrets.env.name,
+												data: []
+											});
+											Secrets.fetch();
+										}}
+									>
 										<TrashIcon />
 									</Button>
-									<Button variant="danger" size="small" on:click={async () => {
-									  await deleteSecret.mutate({ name: secret.name, team: team, env: secrets.env.name });
-										Secrets.fetch();
-									}}>
+									<Button
+										variant="danger"
+										size="small"
+										on:click={async () => {
+											await deleteSecret.mutate({
+												name: secret.name,
+												team: team,
+												env: secrets.env.name
+											});
+											Secrets.fetch();
+										}}
+									>
 										<TrashIcon />
 									</Button>
 								</div>
