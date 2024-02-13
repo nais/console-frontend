9b62100a76a275adf0e8c162239afe99358985c9
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-02-13 11:30:53 +0100

    handle -> handleFetch

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
 import type { HandleFetch } from '@sveltejs/kit';
 
-export const handle: HandleFetch = async ({ event, request, fetch }) => {
+export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 	const cookies = event.request.headers.get('cookie');
 	if (request.url.startsWith('http://nais-api/') && cookies) {
 		request.headers.set('cookie', cookies);
