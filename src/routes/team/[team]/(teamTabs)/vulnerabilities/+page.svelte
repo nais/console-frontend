0248ce071da07d454ebf323814d7a053f84d761d
ybelMekk <youssef.bel.mekki@nav.no>
2024-02-26 09:51:23 +0100

    fix(vuln): add description for table

--- src/routes/team/[team]/(teamTabs)/vulnerabilities/+page.svelte
+++ src/routes/team/[team]/(teamTabs)/vulnerabilities/+page.svelte
@@ -59,7 +59,7 @@
 		<Card columns={12}>
 			<VulnerabilitiesGraph team={teamName} />
 		</Card>
-		<Card columns={12}>
+		<Card columns={12}> <h3>Highlighting vulnerabilities for each application</h3>
 			<Table
 				size="small"
 				sort={sortState}
