285a470044d761aa602dfe12566d3167931033a7
Tommy Trøen <tommy.troen@nav.no>
2024-03-22 16:13:31 +0100

    feat: add circleprogressbar and refactor a bit

--- src/routes/team/[team]/[env]/postgres/[postgres]/+page.svelte
+++ src/routes/team/[team]/[env]/postgres/[postgres]/+page.svelte
@@ -6,6 +6,7 @@
 	import CpuIcon from '$lib/icons/CpuIcon.svelte';
 	import MemoryIcon from '$lib/icons/MemoryIcon.svelte';
 	import { Alert, CopyButton, HelpText } from '@nais/ds-svelte-community';
+	import { ExternalLinkIcon } from '@nais/ds-svelte-community/icons';
 	import {
 		CheckmarkCircleFillIcon,
 		CircleSlashFillIcon,
@@ -14,12 +15,14 @@
 	} from '@nais/ds-svelte-community/icons';
 	import prettyBytes from 'pretty-bytes';
 	import type { PageData } from './$houdini';
+	import CircleProgressBar from '../CircleProgressBar.svelte';
 
 	export let data: PageData;
 	$: ({ SqlInstance } = data);
 	$: instance = $SqlInstance.data?.sqlInstance;
 	$: teamName = $page.params.team;
 	$: envName = $page.params.env;
+	$: postgres = $page.params.postgres;
 </script>
 
 {#if $SqlInstance.errors}
@@ -28,178 +31,166 @@
 			{error.message}
 		{/each}
 	</Alert>
-{:else}
+{:else if instance && instance.id !== PendingValue}
+	<div style="display: flex; align-items: center; column-gap: 1rem; margin-bottom: 1rem;">
+		<div style="display: flex; align-items: center; column-gap: 0.5rem;" >
+			SQL Instance:
+			<a
+				href="https://console.cloud.google.com/sql/instances/{postgres}/overview?project={instance.projectId}&supportedpurview=project"
+				>{postgres}<ExternalLinkIcon title="Google Cloud Console" font-size="1.5rem" /></a
+			>
+		</div>
+		<div style="display: flex; align-items: center; column-gap: 0.5rem;" >
+			<span>Version:</span>
+			<span style="font-weight: bold">{instance.type}</span>
+		</div>
+		<div style="display: flex; align-items: center; column-gap: 0.5rem;" >
+			<span> Status: </span>
+			{#if instance.isHealthy}
+				<CheckmarkCircleFillIcon style="color: var(--a-surface-success); font-size: 1.2rem" />
+			{:else}
+				<CircleSlashFillIcon style="color: var(--a-icon-danger); font-size: 1.2rem" />
+			{/if}
+		</div>
+		<div style="display: flex; align-items: center; column-gap: 0.5rem;" >
+			{#if instance.highAvailability}
+				<span>HA:</span>
+				<CheckmarkCircleFillIcon style="color: var(--a-surface-success); font-size: 1.2rem" />
+			{/if}
+		</div>
+	</div>
 	<div class="grid">
-		{#if instance && instance.id !== PendingValue}
-			<Card columns={3} borderColor="#91dc75">
-				<div class="summaryCard">
-					<div class="summaryIcon" style="--bg-color: #91dc75">
-						<CostIcon size="32" color="#91dc75" />
-					</div>
-					<div class="summary">
-						<h4>
-							Cost
-							<HelpText title="">Total SQL instance cost for the last 30 days.</HelpText>
-						</h4>
-						<p class="metric">
-							€{Math.round(instance.cost)}
-						</p>
-					</div>
+		<Card columns={3}>
+			<div class="summaryCard">
+				<div class="summaryIcon" style="--bg-color: #91dc75">
+					<CostIcon size="32" color="#91dc75" />
 				</div>
-			</Card>
-			<Card columns={3} borderColor="#83bff6">
-				<div class="summaryCard">
-					<div class="summaryIcon" style="--bg-color: #83bff6">
-						<CpuIcon size="32" color="#83bff6" />
-					</div>
-					<div class="summary">
-						<h4>
-							CPU utilization
-							<HelpText title="Current CPU utilization"
-								>CPU utilization for the last elapsed hour for team.
-							</HelpText>
-						</h4>
-						<p class="metric">
-							{Math.round(instance.metrics.cpu.utilization).toFixed(1)}% of {instance.metrics.cpu.cores.toLocaleString()}
-						</p>
-					</div>
-				</div>
-			</Card>
-			<Card columns={3} borderColor="#91dc75">
-				<div class="summaryCard">
-					<div class="summaryIcon" style="--bg-color: #91dc75">
-						<MemoryIcon size="32" color="#91dc75" />
-					</div>
-					<div class="summary">
-						<h4>
-							Memory utilization
-							<HelpText title="Current memory utilization"
-								>Memory utilization for the last elapsed hour.
-							</HelpText>
-						</h4>
-						<p class="metric">
-							{Math.round(instance.metrics.memory.utilization).toFixed(1)}% of {prettyBytes(
-								instance.metrics.memory.quotaBytes
-							)}
-						</p>
-					</div>
-				</div>
-			</Card>
-
-			<Card columns={3} borderColor="#91dc75">
-				<div class="summaryCard">
-					<div class="summaryIcon" style="--bg-color: #91dc75">
-						<FloppydiskIcon font-size="32" color="#91dc75" />
-					</div>
-					<div class="summary">
-						<h4>
-							Disk utilization
-							<HelpText title="Current memory utilization"
-								>Disk utilization for the last elapsed hour.
-							</HelpText>
-						</h4>
-						<p class="metric">
-							{Math.round(instance.metrics.disk.utilization).toFixed(1)}% of {prettyBytes(
-								instance.metrics.disk.quotaBytes
-							)}
-						</p>
-					</div>
-				</div>
-			</Card>
-			<Card columns={3} borderColor="#91dc75">
-				<div class="summaryCard">
-					<div class="summary">
-						<h4>
-							Health status
-							{#if instance.isHealthy}
-								<CheckmarkCircleFillIcon
-									style="color: var(--a-surface-success); font-size: 1.2rem"
-								/>
-							{:else}
-								<CircleSlashFillIcon style="color: var(--a-icon-danger); font-size: 1.2rem" />
-							{/if}
-						</h4>
-					</div>
-				</div>
-			</Card>
-			<Card columns={3} borderColor="#91dc75">
-				<div class="summaryCard">
-					<div class="summary">
-						<h4>
-							High availability enabled
-							{#if instance.highAvailability}
-								<CheckmarkCircleFillIcon
-									style="color: var(--a-surface-success); font-size: 1.2rem"
-								/>
-							{:else}
-								<CircleSlashFillIcon style="color: var(--a-icon-danger); font-size: 1.2rem" />
-							{/if}
-						</h4>
-					</div>
-				</div>
-			</Card>
-			<Card columns={3} borderColor="#91dc75">
-				<div class="summaryCard">
-					<div class="summary">
-						<h4>Connections 2 av 10</h4>
-					</div>
-				</div>
-			</Card>
-			<Card columns={3} borderColor="#91dc75">
-				<div class="summaryCard">
-					<div class="summary">
-						<h4>Databases: 1,2</h4>
-					</div>
-				</div>
-			</Card>
-			<Card columns={8} borderColor="#91dc75">
-				<h4>Information</h4>
-				{#if instance.app}
-					Used by application: <a
-						href="/team/{teamName}/{envName}/app/{instance.app.name.toString()}"
-						>{instance.app.name}</a
-					>
-				{:else}
-					<ExclamationmarkTriangleFillIcon
-						style="color: var(--a-icon-warning)"
-						title="The SQL instance does not belong to any application resource"
-					/>
-					The SQL instance does not belong to any application resource
-				{/if}
-				{#if instance.type}
-					<p>Database version: {instance.type}</p>
-				{:else}
-					<p>Database type: None</p>
-				{/if}
-				{#if instance.connectionName}
-					<p style="display: flex; align-items: center;">
-						Connection name: {instance.connectionName}
-						<CopyButton
-							size="small"
-							variant="action"
-							copyText={instance.connectionName.toString()}
-						/>
+				<div class="summary">
+					<h4>
+						Cost
+						<HelpText title="">Total SQL instance cost for the last 30 days.</HelpText>
+					</h4>
+					<p class="metric">
+						€{Math.round(instance.cost)}
 					</p>
-				{/if}
-				{#if instance.tier}
-					<p>Tier: {instance.tier}</p>
-				{/if}
-				<p>Last backup: {new Date(2024).toLocaleString()}</p>
-			</Card>
-			<Card columns={4} borderColor="#91dc75">
-				<h4>Backup settings</h4>
-				<p>Automatic backups: {instance.backupConfiguration.enabled ? 'Enabled' : 'Disabled'}</p>
-				<p>Backup start time: {instance.backupConfiguration.startTime}</p>
-				{#if instance.backupConfiguration.retainedBackups}
-					<p>Retained backups: {instance.backupConfiguration.retainedBackups}</p>
-				{/if}
-			</Card>
-			<Card columns={12} borderColor="#91dc75">
-				<h4>Links</h4>
-				<p>link 1</p>
-				<p>link 2</p>
-			</Card>
-		{/if}
+				</div>
+			</div>
+		</Card>
+		<Card columns={3}>
+			<div class="summaryCard">
+				<div>
+					<CircleProgressBar progress={instance.metrics.cpu.utilization / 100} />
+				</div>
+				<div class="summary">
+					<h4>
+						CPU utilization
+						<HelpText title="Current CPU utilization"
+							>CPU utilization for the last elapsed hour for team.
+						</HelpText>
+					</h4>
+					<p class="metric">
+						{Math.round(instance.metrics.cpu.utilization).toFixed(1)}% of {instance.metrics.cpu.cores.toLocaleString()}
+					</p>
+				</div>
+			</div>
+		</Card>
+		<Card columns={3}>
+			<div class="summaryCard">
+				<div>
+					<CircleProgressBar progress={instance.metrics.memory.utilization / 100} />
+				</div>
+				<div class="summary">
+					<h4>
+						Memory utilization
+						<HelpText title="Current memory utilization"
+							>Memory utilization for the last elapsed hour.
+						</HelpText>
+					</h4>
+					<p class="metric">
+						{Math.round(instance.metrics.memory.utilization).toFixed(1)}% of {prettyBytes(
+							instance.metrics.memory.quotaBytes
+						)}
+					</p>
+				</div>
+			</div>
+		</Card>
+
+		<Card columns={3}>
+			<div class="summaryCard">
+				<div>
+					<CircleProgressBar progress={instance.metrics.disk.utilization / 100} />
+				</div>
+				<div class="summary">
+					<h4>
+						Disk utilization
+						<HelpText title="Current memory utilization"
+							>Disk utilization for the last elapsed hour.
+						</HelpText>
+					</h4>
+					<p class="metric">
+						{Math.round(instance.metrics.disk.utilization).toFixed(1)}% of {prettyBytes(
+							instance.metrics.disk.quotaBytes
+						)}
+					</p>
+				</div>
+			</div>
+		</Card>
+		<Card columns={8}>
+			<h4>Information</h4>
+			{#if instance.app}
+				Used by application: <a href="/team/{teamName}/{envName}/app/{instance.app.name.toString()}"
+					>{instance.app.name}</a
+				>
+			{:else}
+				<ExclamationmarkTriangleFillIcon
+					style="color: var(--a-icon-warning)"
+					title="The SQL instance does not belong to any application resource"
+				/>
+				The SQL instance does not belong to any application resource
+			{/if}
+			{#if instance.type}
+				<p>Database version: {instance.type}</p>
+			{:else}
+				<p>Database type: None</p>
+			{/if}
+			{#if instance.connectionName}
+				<p style="display: flex; align-items: center;">
+					Connection name: {instance.connectionName}
+					<CopyButton size="small" variant="action" copyText={instance.connectionName.toString()} />
+				</p>
+			{/if}
+			{#if instance.tier}
+				<p>Tier: {instance.tier}</p>
+			{/if}
+			<p>Last backup: {new Date(2024).toLocaleString()}</p>
+		</Card>
+		<Card columns={4}>
+			<h4>Backup settings</h4>
+			<p>Automatic backups: {instance.backupConfiguration.enabled ? 'Enabled' : 'Disabled'}</p>
+			<p>Backup start time: {instance.backupConfiguration.startTime}</p>
+			{#if instance.backupConfiguration.retainedBackups}
+				<p>Retained backups: {instance.backupConfiguration.retainedBackups}</p>
+			{/if}
+		</Card>
+		<Card columns={3}>
+			<div class="summaryCard">
+				<div class="summary">
+					<h4>Connections 2 av 10</h4>
+				</div>
+			</div>
+		</Card>
+		<Card columns={3}>
+			<div class="summaryCard">
+				<div class="summary">
+					<h4>Databases: 1,2</h4>
+				</div>
+			</div>
+		</Card>
+		<Card columns={12}>
+			<h4>Links</h4>
+			<p>link 1</p>
+			<p>link 2</p>
+		</Card>
 	</div>
 {/if}
 
@@ -213,12 +204,11 @@
 
 	.summaryIcon {
 		display: flex;
-		background-color: color-mix(in srgb, var(--bg-color) 10%, white);
 		justify-content: center;
 		align-items: center;
 		width: 50px;
 		height: 50px;
-		border: 2px solid var(--bg-color);
+		border: 1px solid var(--bg-color);
 		border-radius: 5px;
 	}
 
