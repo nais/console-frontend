3316c3d1d07017472786b27cfd6c54be98065a7e
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-04-11 14:41:29 +0200

    Prevent accessing settings page for other team

---
+++ src/routes/team/[team]/+layout.ts
@@ -0,0 +1,37 @@
+import { load_TeamRoles } from '$houdini';
+import { error } from '@sveltejs/kit';
+import { get } from 'svelte/store';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async (event) => {
+	const roles = await load_TeamRoles({
+		event,
+		variables: { team: event.params.team },
+		blocking: true
+	});
+
+	if (!roles) {
+		error(501, 'Something went wrong when loading the page');
+	}
+
+	const current = get(roles.TeamRoles);
+	if (!current) {
+		error(404, 'Team not found');
+	}
+
+	if (current.errors) {
+		console.error('Error loading team roles: ', current.errors);
+		error(500, 'Something went wrong when loading the page');
+	}
+
+	if (!current.data) {
+		return {
+			viewerIsMember: false,
+			viewerIsOwner: false
+		};
+	}
+
+	return {
+		...current.data.team
+	};
+};
