a2339485012a4522139f3008bc7602ae9264cc40
carl hedgren <carl.hedgren@nav.no>
2024-05-15 13:38:10 +0200

    fix broken kafka stubs

--- src/lib/PersistenceIcon.svelte
+++ src/lib/PersistenceIcon.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { BucketIcon, SandboxIcon } from '@nais/ds-svelte-community/icons';
-	import Bigquery from './icons/Bigquery.svelte';
+	import BigQuery from './icons/BigQuery.svelte';
 	import Kafka from './icons/Kafka.svelte';
 	import Opensearch from './icons/Opensearch.svelte';
 	import PostgresStroke from './icons/PostgresStroke.svelte';
@@ -11,7 +11,7 @@
 {#if type === 'Bucket'}
 	<BucketIcon {...$$restProps} />
 {:else if type === 'BigQueryDataset'}
-	<Bigquery {...$$restProps} />
+	<BigQuery {...$$restProps} />
 {:else if type === 'SqlInstance'}
 	<PostgresStroke {...$$restProps} />
 {:else if type === 'Kafka'}
