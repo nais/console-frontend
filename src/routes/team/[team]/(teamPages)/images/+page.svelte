21fdc2cad3ff25e1471c0c885a6afb8931cb7f72
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-05-21 15:22:21 +0200

    feat: Add pageination

--- src/routes/team/[team]/(teamPages)/images/+page.svelte
+++ src/routes/team/[team]/(teamPages)/images/+page.svelte
@@ -1,43 +1,88 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import Card from '$lib/Card.svelte';
-	import { Link, Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
+	import Pagination from '$lib/Pagination.svelte';
+	import { Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
 	import type { PageData } from './$houdini';
+
+	import {
+		changeParams,
+		sortTable,
+		tableGraphDirection,
+		tableStateFromVariables
+	} from '$lib/pagination';
 
 	export let data: PageData;
 
-	$: teamName = $page.params.team;
+	$: team = $page.params.team;
 	$: ({ Images } = data);
-	$: images = $Images.data?.team.images.nodes;
+	$: images = $Images.data?.team.images;
+
+	$: ({ sortState, limit, offset } = tableStateFromVariables($Images.variables));
+
+	$: console.log(images);
+
+	function getTag(name: string) {
+		const split = name.split('/');
+		return split[split.length - 1];
+	}
 </script>
 
 <Card columns={12}>
-	<h2>Images - {teamName}</h2>
-	<Table size="small">
+	<h2>Images - {team}</h2>
+	<Table
+		size="small"
+		sort={sortState}
+		on:sortChange={(e) => {
+			const { key } = e.detail;
+			const ss = sortTable(key, sortState);
+			changeParams({ col: ss.orderBy, dir: tableGraphDirection[ss.direction] });
+		}}
+	>
 		<Thead>
-			<Th>Image</Th>
-			<Th>Critical</Th>
-			<Th>Risk score</Th>
+			<Th sortable={true} sortKey="NAME">Image</Th>
+			<Th sortable={true} sortKey="SEVERITY_CRITICAL">Critical</Th>
+			<Th sortable={true} sortKey="RISK_SCORE">Risk score</Th>
 			<Th>Workloads</Th>
 		</Thead>
 		<Tbody>
 			{#if images !== undefined}
-				{#each images as image}
+				{#each images.nodes as image}
 					<Tr>
 						<Td>
-							<Link href={`/team/${teamName}/images/${image.id}`}
-								><span
-									style="width: 100%; direction: rtl; text-overflow: ellipsis; white-space: nowrap; overflow: hidden"
-									title={image.name}>{image.name}</span
-								></Link
+							<span
+								style="width: 100%; direction: rtl; text-overflow: ellipsis; white-space: nowrap; overflow: hidden"
+								title={image.name}
+							>
+								<a href="/team/{team}/images/{image.projectId}"
+									>{getTag(image.name)}:{image.version}</a
+								></span
 							>
 						</Td>
-						<Td>{image.critical}</Td>
-						<Td>{image.riskScore}</Td>
-						<Td>N/A</Td>
+						<Td style="text-align: center">{image.critical}</Td>
+						<Td style="text-align: center">{image.riskScore}</Td>
+						<Td>
+							{#if image.workloadReferences.length > 0}
+								{#each image.workloadReferences as workload}
+									<a href={`/team/${team}/${workload.environment}/app/${workload.name}`}
+										>{workload.team}:{workload.environment}:{workload.name}</a
+									><br />
+								{/each}
+							{:else}
+								<span>No workloads</span>
+							{/if}
+						</Td>
 					</Tr>
 				{/each}
 			{/if}
 		</Tbody>
 	</Table>
+	<Pagination
+		pageInfo={images?.pageInfo}
+		{limit}
+		{offset}
+		changePage={(e) => {
+			changeParams({ page: e.toString() });
+		}}
+	/>
 </Card>
---
+++ src/routes/team/[team]/(teamPages)/images/+page.ts
@@ -0,0 +1,14 @@
+import { error } from '@sveltejs/kit';
+import type { ImagesVariables } from './$houdini';
+export const _ImagesVariables: ImagesVariables = ({ url }) => {
+	const page = parseInt(url.searchParams.get('page') || '1');
+	if (!page || page < 1) {
+		error(400, 'Bad pagenumber');
+	}
+	const limit = 25;
+	const offset = (page - 1) * limit;
+	const field = (url.searchParams.get('col') || 'NAME') as never;
+	const direction = (url.searchParams.get('dir') || 'ASC') as never;
+
+	return { limit, offset, orderBy: { field, direction } };
+};
