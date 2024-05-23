07bf226a66414259fceaf6d01fd3b864fa95426c
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-05-23 13:20:23 +0200

    more normalized icons

--- src/lib/icons/Unleash.svelte
+++ src/lib/icons/Unleash.svelte
@@ -12,16 +12,20 @@
 >
 	<g clip-path="url(#clip0)">
 		<circle stroke="#222" cx="35" cy="35" r="30" stroke-width="4"></circle>
-		<path d="M34.3043 17.2658V25.785V34.3043H42.8198V25.785V17.2658H34.3043Z" fill="#000"
+		<path
+			d="M34.3043 17.2658V25.785V34.3043H42.8198V25.785V17.2658H34.3043Z"
+			fill="#000"
 			transform="translate(5 5)"
-		 />
+		/>
 		<path
 			d="M25.7851 25.785V17.2658H17.2659V25.785V34.3043V42.8198H25.7851H34.3044V34.3043H25.7851V25.785Z"
 			fill="#000"
 			transform="translate(5 5)"
 		/>
-		<path d="M42.8236 34.3045H34.3043V42.8237H42.8236V34.3045Z" stroke="#000" fill="#fff"
-		
+		<path
+			d="M42.8236 34.3045H34.3043V42.8237H42.8236V34.3045Z"
+			stroke="#000"
+			fill="#fff"
 			transform="translate(5 5)"
 		/>
 	</g>
