26556f241285227a27907334ce5479ca59e343e6
Christer Edvartsen <christer.edvartsen@nav.no>
2024-05-27 11:16:56 +0200

    fix some CSS issues

--- src/lib/icons/Bucket.svelte
+++ src/lib/icons/Bucket.svelte
@@ -8,45 +8,17 @@
 	width={size}
 	height={size}
 	viewBox="0 0 24 24"
-	><defs
-		><style>
-			.cls-1 {
-				fill: #fff;
-				stroke: #000;
-			}
-			.cls-2 {
-				fill: 000;
-			}
-		</style></defs
-	><g data-name="Product Icons"
-		><rect class="cls-1" x="2" y="4" width="20" height="7" /><rect
-			class="cls-2"
-			x="20"
-			y="4"
-			width="2"
-			height="7"
-		/>
+	><g data-name="Product Icons">
+		<rect fill="#fff" stroke="#000" x="2" y="4" width="20" height="7" />
+		<rect fill="#000" x="20" y="4" width="2" height="7" />
+		<rect fill="#000" x="2" y="4" width="2" height="7" />
+		<rect x="6" y="7" width="6" height="1" />
+		<rect x="15" y="6" width="3" height="3" rx="1.5" />
 
-		<rect class="cls-2" x="2" y="4" width="2" height="7" /><rect
-			class="cls-4"
-			x="6"
-			y="7"
-			width="6"
-			height="1"
-		/><rect class="cls-4" x="15" y="6" width="3" height="3" rx="1.5" /><rect
-			class="cls-1"
-			x="2"
-			y="13"
-			width="20"
-			height="7"
-		/><rect class="cls-2" x="20" y="13" width="2" height="7" />
-
-		<rect class="cls-2" x="2" y="13" width="2" height="7" /><rect
-			class="cls-4"
-			x="6"
-			y="16"
-			width="6"
-			height="1"
-		/><rect class="cls-4" x="15" y="15" width="3" height="3" rx="1.5" /></g
+		<rect fill="#fff" stroke="#000" x="2" y="13" width="20" height="7" />
+		<rect fill="#000" x="20" y="13" width="2" height="7" />
+		<rect fill="#000" x="2" y="13" width="2" height="7" />
+		<rect x="6" y="16" width="6" height="1" />
+		<rect x="15" y="15" width="3" height="3" rx="1.5" /></g
 	></svg
 >
