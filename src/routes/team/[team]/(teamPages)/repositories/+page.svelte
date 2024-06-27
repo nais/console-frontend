dbf457969bfe07ab0c7c189a43af147d21847ed9
Johnny Horvi <jhrv@users.noreply.github.com>
2024-06-27 13:54:28 +0200

    Changed repo authorization view (#82)

--- src/routes/team/[team]/(teamPages)/repositories/+page.svelte
+++ src/routes/team/[team]/(teamPages)/repositories/+page.svelte
@@ -1,183 +1,114 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { RepositoryAuthorization, graphql } from '$houdini';
+	import { AuditEventResourceType, graphql } from '$houdini';
 	import Card from '$lib/Card.svelte';
+	import { PlusIcon, TrashIcon } from '@nais/ds-svelte-community/icons';
 	import Pagination from '$lib/Pagination.svelte';
-	import { changeParams, limitOffset, sortTable } from '$lib/pagination';
-	import {
-		Button,
-		HelpText,
-		Link,
-		Table,
-		Tbody,
-		Td,
-		Th,
-		Thead,
-		Tr,
-		type TableSortState
-	} from '@nais/ds-svelte-community';
+	import { changeParams, limitOffset } from '$lib/pagination';
+	import { HelpText, Button, Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
 	import type { PageData } from './$houdini';
-
-	/*
-	TODO: FIX THIS
-	*/
+	import ActivityLog from '$lib/components/ActivityLog.svelte';
 
 	export let data: PageData;
 
 	$: ({ Repositories } = data);
 	$: team = $Repositories.data?.team;
-
 	$: teamName = $page.params.team;
-
 	$: ({ limit, offset } = limitOffset($Repositories.variables));
 
-	let sortState: TableSortState = {
-		orderBy: 'NAME',
-		direction: 'ascending'
-	};
-
-	const roleDesc = (role: string) => {
-		switch (role) {
-			case 'TRIAGE':
-				return 'Can read and clone this repository. Can also manage issues and pull requests.';
-			case 'READ':
-				return 'Can read and clone this repository. Can also open and comment on issues and pull requests.';
-			case 'WRITE':
-				return 'Can read, clone, and push to this repository. Can also manage issues and pull requests.';
-			case 'MAINTAIN':
-				return 'Can read, clone, and push to this repository. They can also manage issues, pull requests, and some repository settings.';
-			case 'ADMIN':
-				return 'Can read, clone, and push to this repository. Can also manage issues, pull requests, and repository settings, including adding collaborators.';
-			case 'ISOC_TRIAGE_FOLLOWUP':
-				return 'Give ISOC the opportunity to follow up';
-			default:
-				return 'Unknown role';
-		}
-	};
-
-	const authorizeRepositoryForDeploy = graphql(`
-		mutation AuthorizeRepository(
-			$authorization: RepositoryAuthorization!
-			$repository: String!
-			$team: Slug!
-		) {
-			authorizeRepository(authorization: $authorization, repoName: $repository, teamSlug: $team) {
-				authorizations
-			}
+	const addRepositoryMutation = graphql(`
+		mutation AddRepository($repository: String!, $team: Slug!) {
+			addRepository(repoName: $repository, teamSlug: $team)
 		}
 	`);
-
-	const deauthorizeRepositoryForDeploy = graphql(`
-		mutation DeauthorizeRepository(
-			$authorization: RepositoryAuthorization!
-			$repository: String!
-			$team: Slug!
-		) {
-			deauthorizeRepository(authorization: $authorization, repoName: $repository, teamSlug: $team) {
-				authorizations
-			}
-		}
-	`);
-
-	const authorizeDeploy = (team: string, repository: string) => {
-		authorizeRepositoryForDeploy.mutate({
-			authorization: RepositoryAuthorization.DEPLOY,
+	const addRepository = async (team: string, repository: string) => {
+		await addRepositoryMutation.mutate({
 			repository,
 			team
 		});
+		Repositories.fetch();
 	};
-	const deauthorizeDeploy = (team: string, repository: string) => {
-		deauthorizeRepositoryForDeploy.mutate({
-			authorization: RepositoryAuthorization.DEPLOY,
+
+	const removeRepositoryMutation = graphql(`
+		mutation RemoveRepository($repository: String!, $team: Slug!) {
+			removeRepository(repoName: $repository, teamSlug: $team)
+		}
+	`);
+	const removeRepository = async (team: string, repository: string) => {
+		await removeRepositoryMutation.mutate({
 			repository,
 			team
 		});
+		Repositories.fetch();
 	};
+
+	let repoName = '';
 </script>
 
 {#if team}
-	<Card>
-		<h3>Repositories</h3>
-
-		<Table
-			size="small"
-			sort={sortState}
-			zebraStripes
-			on:sortChange={(e) => {
-				const { key } = e.detail;
-				sortState = sortTable(key, sortState);
-			}}
-		>
-			<Thead>
-				<Tr>
-					<Th sortable={true} sortKey="NAME">Repository</Th>
-					{#if data.viewerIsMember}
-						<Th>Deploy</Th>
-						<Th sortable={true} sortKey="ROLE">Role</Th>
-					{/if}
-				</Tr>
-			</Thead>
-			<Tbody>
-				{#each team.githubRepositories.nodes as repo}
-					<Tr>
-						<Td><Link href="https://github.com/{repo.name}">{repo.name}</Link></Td>
-						{#if data.viewerIsMember}
-							{#if repo.authorizations !== null && repo.name !== null}
-								<Td>
-									{#if repo.authorizations.includes('DEPLOY')}
-										<Button
-											size="xsmall"
-											variant="danger"
-											on:click={() => {
-												deauthorizeDeploy(teamName, repo.name);
-											}}
-										>
-											Deauthorize
-										</Button>
-									{:else}
-										<Button
-											size="xsmall"
-											variant="primary-neutral"
-											on:click={() => {
-												authorizeDeploy(teamName, repo.name);
-											}}
-										>
-											Authorize
-										</Button>
-									{/if}
-								</Td>
-							{/if}
+	<div class="grid">
+		<Card>
+			<div class="repository">
+				<h3>Add repository <HelpText title="Description">Adding a repository will grant it access to deployment actions on behalf of the team.</HelpText></h3>
+				<div class="roleHelpText">
+					<input
+						type="text"
+						id="repositoryName"
+						placeholder="Repository name"
+						bind:value={repoName}
+					/>
+					<Button
+						size="small"
+						variant="secondary"
+						on:click={() => {
+							addRepository(teamName, repoName);
+							repoName = '';
+						}}
+					>
+						<svelte:fragment slot="icon-left"><PlusIcon /></svelte:fragment>
+						Add
+					</Button>
+				</div>
+			</div>
+		</Card>
+		<Card>
+			<h3>Repositories</h3>
+			<Table size="small" zebraStripes>
+				<Thead>
+					<Th>Name</Th>
+					<Th style="width:150px">Action</Th>
+				</Thead>
+				<Tbody>
+					{#each team.repositories.nodes as repo}
+						<Tr>
+							<Td>{repo}</Td>
 							<Td>
-								<div class="roleHelpText">
-									{repo.roleName}
-									<HelpText placement={'left'} title="Role description">
-										The team's role for the repository.<br />{repo.roleName.toUpperCase()}:
-										{roleDesc(repo.roleName.toUpperCase())}
-									</HelpText>
-								</div>
+								<Button
+									variant="secondary"
+									size="small"
+									on:click={() => removeRepository(teamName, repo)}
+								>
+									<svelte:fragment slot="icon-left"><TrashIcon /></svelte:fragment>
+									Remove
+								</Button>
 							</Td>
-						{/if}
-					</Tr>
-				{:else}
-					<Tr>
-						<Td colspan={99} style="background:var(--a-surface-info-subtle)">
-							No GitHub repositories for this team. You can link a repository to this team by giving
-							the team permissions on the repository on GitHub.
-						</Td>
-					</Tr>
-				{/each}
-			</Tbody>
-		</Table>
-
-		<Pagination
-			pageInfo={team.githubRepositories.pageInfo}
-			{limit}
-			{offset}
-			changePage={(page) => {
-				changeParams({ page: page.toString() });
-			}}
-		/>
-	</Card>
+						</Tr>
+					{/each}
+				</Tbody>
+			</Table>
+			<Pagination
+				pageInfo={team.repositories.pageInfo}
+				{limit}
+				{offset}
+				changePage={(e) => {
+					changeParams({ page: e.toString() });
+				}}
+			/>
+		</Card>
+		{#key team}
+			<ActivityLog resourceType={AuditEventResourceType.TEAM_REPOSITORY} {teamName} />
+		{/key}
+	</div>
 {/if}
 
 <style>
@@ -186,5 +117,14 @@
 		gap: 0.5rem;
 		font-family: monospace;
 		font-size: 1rem;
+	}
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(1, 1fr);
+		gap: 1rem;
+	}
+	.repository > h3 {
+		display: flex;
+		gap: 1rem;
 	}
 </style>
--- src/routes/team/[team]/(teamPages)/repositories/+page.ts
+++ src/routes/team/[team]/(teamPages)/repositories/+page.ts
@@ -5,7 +5,7 @@ export const _RepositoriesVariables: RepositoriesVariables = ({ url }) => {
 	if (!page || page < 1) {
 		error(400, 'Bad pagenumber');
 	}
-	const limit = 50;
+	const limit = 10;
 	const offset = (page - 1) * limit;
 
 	return { limit, offset };
