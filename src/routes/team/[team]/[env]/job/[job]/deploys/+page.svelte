e1290ee8c5f78c57fcbac05e3d2cc0a51c641a73
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-10-21 15:40:26 +0200

    App/Job deployments

---
+++ src/routes/team/[team]/[env]/job/[job]/deploys/+page.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import Card from '$lib/Card.svelte';
+	import DeploymentStatus from '$lib/DeploymentStatus.svelte';
+	import Time from '$lib/Time.svelte';
+	import {
+		Alert,
+		Button,
+		Table,
+		Tbody,
+		Td,
+		Th,
+		Thead,
+		Tooltip,
+		Tr
+	} from '@nais/ds-svelte-community';
+	import { BranchingIcon } from '@nais/ds-svelte-community/icons';
+	import type { PageData } from './$houdini';
+	export let data: PageData;
+
+	$: ({ JobDeploys } = data);
+</script>
+
+{#if $JobDeploys.errors}
+	<Alert variant="error">
+		{#each $JobDeploys.errors as error}
+			{error.message}
+		{/each}
+	</Alert>
+{/if}
+
+{#if $JobDeploys.data}
+	<Card>
+		<Table size="small" zebraStripes>
+			<Thead>
+				<Th>Resource(s)</Th>
+				<Th>Created</Th>
+				<Th>Status</Th>
+				<Th>Link</Th>
+			</Thead>
+			<Tbody>
+				{#each $JobDeploys.data.team.environment.job.deploymentInfo.history.edges as edge}
+					<Tr>
+						<Td>
+							{#each edge.node.resources as resource}
+								<span style="color:var(--a-gray-600)">{resource.kind}:</span>
+								{resource.name}
+								<br />
+							{/each}
+						</Td>
+						<Td>
+							<Time time={new Date(edge.node.created)} distance={true} />
+						</Td>
+						<Td>
+							<Tooltip content={edge.node.statuses[0]?.message || ''}
+								><DeploymentStatus status={edge.node.statuses[0]?.status} /></Tooltip
+							>
+						</Td>
+						<Td>
+							{#if edge.node.repository}
+								<Button
+									size="xsmall"
+									variant="secondary"
+									href="https://github.com/{edge.node.repository}"
+									as="a"
+								>
+									<svelte:fragment slot="icon-left"><BranchingIcon /></svelte:fragment>Repo</Button
+								>
+							{/if}
+						</Td>
+					</Tr>
+				{/each}
+			</Tbody>
+		</Table>
+	</Card>
+{/if}
