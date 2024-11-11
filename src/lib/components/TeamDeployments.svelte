c8d7f13f23f9029f627b1462d1c9aab514458667
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-11-11 14:17:45 +0100

    Add loading states to TeamDeployments component and queries; enhance rendering with skeletons for improved user experience

--- src/lib/components/TeamDeployments.svelte
+++ src/lib/components/TeamDeployments.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { fragment, graphql, type TeamDeploymentsNew } from '$houdini';
+	import { fragment, graphql, PendingValue, type TeamDeploymentsNew } from '$houdini';
 	import DeploymentStatus from '$lib/DeploymentStatus.svelte';
 	import Time from '$lib/Time.svelte';
-	import { Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
+	import { Skeleton, Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
 
 	export let team: TeamDeploymentsNew;
 
@@ -10,7 +10,7 @@
 		team,
 		graphql(`
 			fragment TeamDeploymentsNew on Team {
-				deployments {
+				deployments @loading {
 					nodes {
 						statuses {
 							status
@@ -36,56 +36,88 @@
 			}
 		`)
 	);
-	$: deploysOrderedByDate = $data.deployments.nodes.sort((a, b) => {
-		return new Date(b.created).getTime() - new Date(a.created).getTime();
-	});
+
+	type Deployment = {
+		readonly team: { readonly slug: string };
+		readonly environment: { readonly name: string };
+		readonly resources: {
+			readonly group: string;
+			readonly kind: string;
+			readonly name: string;
+			readonly version: string;
+			readonly namespace: string;
+		}[];
+		readonly created: Date;
+		readonly statuses: {
+			readonly status: string;
+			readonly message: string | null;
+			readonly created: Date;
+		}[];
+	};
+
+	function orderDeploymentsByDate(nodes: Deployment[]) {
+		return nodes.sort((a, b) => {
+			return new Date(b.created).getTime() - new Date(a.created).getTime();
+		});
+	}
 </script>
 
 <h4>Deployments</h4>
-{#if $data !== null}
-	<Table size="small" zebraStripes>
-		<Thead>
-			<Th>Team</Th>
-			<Th>Environment</Th>
-			<Th>Resource(s)</Th>
-			<Th>Created</Th>
-			<Th>Status</Th>
-		</Thead>
-		<Tbody>
-			{#each deploysOrderedByDate as deploy}
-				<Tr>
-					<Td>{deploy.team.slug}</Td>
-					<Td>
-						{deploy.environment.name}
-					</Td>
-					<Td
-						>{#each deploy.resources as resource}
-							<span style="color:var(--a-gray-600)">{resource.kind}:</span>
-							{#if resource.kind === 'Application'}
-								<a
-									href="/team/{deploy.team.slug}/{deploy.environment
-										.name}/app/{resource.name}/deploys">{resource.name}</a
-								>
-							{:else if resource.kind === 'Job'}
-								<a
-									href="/team/{deploy.team.slug}/{deploy.environment
-										.name}/job/{resource.name}/deploys">{resource.name}</a
-								>
-							{:else}
-								{resource.name}
-							{/if}
-							<br />
-						{/each}</Td
-					>
-					<Td><Time time={deploy.created} distance={true} /></Td>
 
-					<Td
-						>{#if deploy.statuses.length === 0}<DeploymentStatus
-								status={'unknown'}
-							/>{:else}<DeploymentStatus status={deploy.statuses[0].status} />{/if}</Td
-					>
+<Table size="small" zebraStripes>
+	<Thead>
+		<Th>Team</Th>
+		<Th>Environment</Th>
+		<Th>Resource(s)</Th>
+		<Th>Created</Th>
+		<Th>Status</Th>
+	</Thead>
+	<Tbody>
+		{#if $data !== null}
+			{#if $data.deployments === PendingValue}
+				<Tr>
+					<Td><Skeleton variant="text" /></Td>
+					<Td><Skeleton variant="text" /></Td>
+					<Td><Skeleton variant="text" /></Td>
+					<Td><Skeleton variant="text" /></Td>
+					<Td><Skeleton variant="rectangle" /></Td>
 				</Tr>
-			{/each}
-		</Tbody>
-	</Table>
-{/if}
+			{:else}
+				{#each orderDeploymentsByDate($data.deployments.nodes) as deploy}
+					<Tr>
+						<Td>{deploy.team.slug}</Td>
+						<Td>
+							{deploy.environment.name}
+						</Td>
+						<Td
+							>{#each deploy.resources as resource}
+								<span style="color:var(--a-gray-600)">{resource.kind}:</span>
+								{#if resource.kind === 'Application'}
+									<a
+										href="/team/{deploy.team.slug}/{deploy.environment
+											.name}/app/{resource.name}/deploys">{resource.name}</a
+									>
+								{:else if resource.kind === 'Job'}
+									<a
+										href="/team/{deploy.team.slug}/{deploy.environment
+											.name}/job/{resource.name}/deploys">{resource.name}</a
+									>
+								{:else}
+									{resource.name}
+								{/if}
+								<br />
+							{/each}</Td
+						>
+						<Td><Time time={deploy.created} distance={true} /></Td>
+
+						<Td
+							>{#if deploy.statuses.length === 0}<DeploymentStatus
+									status={'unknown'}
+								/>{:else}<DeploymentStatus status={deploy.statuses[0].status} />{/if}</Td
+						>
+					</Tr>
+				{/each}
+			{/if}
+		{/if}
+	</Tbody>
+</Table>
