ef4cd6aa620740661023846a8b997d86c03f3f27
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-11-13 21:02:04 +0100

    refactor(Deploys): enhance loading state handling and improve deploys table structure

--- src/routes/Deploys.svelte
+++ src/routes/Deploys.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import { graphql, type UserDeploys$result } from '$houdini';
+	import { graphql, PendingValue, type UserDeploys$result } from '$houdini';
 	import DeploymentStatus from '$lib/DeploymentStatus.svelte';
 	import DeploysIcon from '$lib/icons/DeploysIcon.svelte';
 	import Time from '$lib/Time.svelte';
-	import { Alert, Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
+	import { Alert, Skeleton, Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
 
 	const store = graphql(`
 		query UserDeploys @load {
-			me {
+			me @loading {
 				__typename
 				... on User {
 					teams {
@@ -42,8 +42,8 @@
 	`);
 
 	const sortTeamDeploys = (userDeploys: UserDeploys$result['me']) => {
-		/*if (userDeploys === PendingValue)
-			return Array(20).fill(PendingValue) as (typeof PendingValue)[];*/
+		if (userDeploys === PendingValue)
+			return Array(20).fill(PendingValue) as (typeof PendingValue)[];
 		if (userDeploys.__typename !== 'User') return [];
 
 		let retval = userDeploys.teams.nodes
@@ -80,55 +80,55 @@
 		</Thead>
 		<Tbody>
 			{#each sortTeamDeploys($store.data.me) as deploy}
-				<!--{#if deploy == PendingValue}
+				{#if deploy == PendingValue}
 					<Tr>
-						{#each new Array(4).fill('text') as variant}
+						{#each new Array(5).fill('text') as variant}
 							<Td>
 								<Skeleton {variant} />
 							</Td>
 						{/each}
 					</Tr>
-				{:else}-->
-				<Tr>
-					<Td>
-						{#each deploy.resources as resource}
-							<span style="color:var(--a-gray-600)">{resource.kind}:</span>
-							{#if deploy.resources[0].kind === 'Job'}
-								<a
-									href="/team/{deploy.team.slug}/{deploy.environment.name}/job/{deploy.resources[0]
-										.name}"
-								>
-									{deploy.resources[0].name}</a
-								>
-							{:else}
-								<a
-									href="/team/{deploy.team.slug}/{deploy.environment.name}/app/{deploy.resources[0]
-										.name}"
-								>
-									{deploy.resources[0].name}</a
-								>
-							{/if}
-						{/each}
-					</Td>
-					<Td>
-						<a href="/team/{deploy.team.slug}">{deploy.team.slug}</a>
-					</Td>
-					<Td>
-						{deploy.environment.name}
-					</Td>
+				{:else}
+					<Tr>
+						<Td>
+							{#each deploy.resources as resource}
+								<span style="color:var(--a-gray-600)">{resource.kind}:</span>
+								{#if deploy.resources[0].kind === 'Job'}
+									<a
+										href="/team/{deploy.team.slug}/{deploy.environment.name}/job/{deploy
+											.resources[0].name}"
+									>
+										{deploy.resources[0].name}</a
+									>
+								{:else}
+									<a
+										href="/team/{deploy.team.slug}/{deploy.environment.name}/app/{deploy
+											.resources[0].name}"
+									>
+										{deploy.resources[0].name}</a
+									>
+								{/if}
+							{/each}
+						</Td>
+						<Td>
+							<a href="/team/{deploy.team.slug}">{deploy.team.slug}</a>
+						</Td>
+						<Td>
+							{deploy.environment.name}
+						</Td>
 
-					<Td>
-						<Time time={deploy.created} distance={true} />
-					</Td>
-					<Td>
-						{#if deploy.statuses.length === 0}
-							<DeploymentStatus status={'unknown'} />
-						{:else}
-							<DeploymentStatus status={deploy.statuses[0].status} />
-						{/if}
-					</Td>
-				</Tr>
-				<!--{/if}-->
+						<Td>
+							<Time time={deploy.created} distance={true} />
+						</Td>
+						<Td>
+							{#if deploy.statuses.length === 0}
+								<DeploymentStatus status={'unknown'} />
+							{:else}
+								<DeploymentStatus status={deploy.statuses[0].status} />
+							{/if}
+						</Td>
+					</Tr>
+				{/if}
 			{:else}
 				<Tr>
 					<Td colspan={999}>No deploys found</Td>
