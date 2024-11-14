2b2c2c3528cf2e3ffe01b2b121179c66f7f9b741
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-11-14 12:20:41 +0100

    suggestion(TeamInfo): enhance workload status handling and improve visual feedback for team status

--- src/lib/components/TeamInfo.svelte
+++ src/lib/components/TeamInfo.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { graphql, PendingValue } from '$houdini';
+	import { graphql, PendingValue, WorkloadState, type ValueOf } from '$houdini';
+	import Nais from '$lib/icons/Nais.svelte';
 	import { Skeleton } from '@nais/ds-svelte-community';
+	import { ExclamationmarkTriangleFillIcon } from '@nais/ds-svelte-community/icons';
 	import { get } from 'svelte/store';
 	import type { TeamInfoVariables } from './$houdini';
 
@@ -17,10 +19,46 @@
 				gitHubTeamSlug @loading
 				purpose @loading
 				slackChannel @loading
+				workloads @loading {
+					nodes {
+						status {
+							state
+						}
+					}
+				}
 			}
 		}
 	`);
 	const githubOrganization = get(page).data.githubOrganization;
+	let status: string | undefined = undefined;
+
+	$: {
+		if ($teamInfo.data?.team.workloads !== PendingValue) {
+			let states: ValueOf<typeof WorkloadState>[] = [];
+			states =
+				$teamInfo.data?.team.workloads.nodes?.map((node) => node.status.state) ??
+				[].filter((state) => {
+					if (state) {
+						return true;
+					}
+					return false;
+				});
+
+			if (states.includes(WorkloadState.FAILING)) {
+				status = WorkloadState.FAILING;
+			} else if (states?.includes(WorkloadState.NOT_NAIS)) {
+				status = WorkloadState.NOT_NAIS;
+			} else if (states?.includes(WorkloadState.NAIS)) {
+				status = WorkloadState.NAIS;
+			} else if (states?.includes(WorkloadState.UNKNOWN)) {
+				status = WorkloadState.UNKNOWN;
+			} else {
+				status = 'NAIS';
+			}
+		}
+	}
+
+	$: console.log('team status:', status);
 </script>
 
 <h4>Team summary</h4>
@@ -53,7 +91,46 @@
 		{/if}
 		<br />
 	{/if}
+
+	{#if status == 'NAIS'}
+		<div class="nais">
+			<Nais
+				size="3rem"
+				style="color: var(--a-icon-success)"
+				aria-label="All workloads are Nais"
+				role="image"
+			/>
+			<p>Keep up the good work!</p>
+		</div>
+	{:else if status == 'FAILING'}
+		<div class="nais">
+			<ExclamationmarkTriangleFillIcon
+				height="3em"
+				width="3em"
+				style="color: var(--a-icon-danger)"
+			/>
+			<p>One or more workloads are failing</p>
+		</div>
+	{:else if status == 'NOT_NAIS'}
+		<div class="nais">
+			<ExclamationmarkTriangleFillIcon
+				height="3em"
+				width="3em"
+				style="color: var(--a-icon-warning)"
+			/>
+			<p>One ore more workloads are having issues</p>
+		</div>
+	{:else}
+		<p>Team status is unknown</p>
+	{/if}
 {/if}
 
 <style>
+	.nais {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		margin-top: 1rem;
+	}
 </style>
