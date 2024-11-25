6eca2d3f48e4f9f690dacb3d78758871111de3e4
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-11-25 11:13:26 +0100

    Try to prevent client side crash

--- src/routes/team/[team]/(teamPages)/+layout.svelte
+++ src/routes/team/[team]/(teamPages)/+layout.svelte
@@ -81,169 +81,175 @@
 	}
 
 	let nav: menuGroup[];
-	$: nav = [
-		{
-			items: [
+	$: {
+		if ($inventoryCounts) {
+			nav = [
 				{
-					name: 'Overview',
-					routeId: '/team/[team]/(teamPages)',
-					withSubRoutes: false,
-					icon: HouseIcon
+					items: [
+						{
+							name: 'Overview',
+							routeId: '/team/[team]/(teamPages)',
+							withSubRoutes: false,
+							icon: HouseIcon
+						}
+					]
+				},
+				{
+					items: [
+						{
+							name: 'Applications',
+							routeId: '/team/[team]/(teamPages)/applications',
+							withSubRoutes: true,
+							icon: SandboxIcon,
+							inventoryCount: $inventoryCounts.data?.team.inventoryCounts.applications.total,
+							notNais:
+								$inventoryCounts.data?.team.inventoryCounts.applications.notNais !== PendingValue
+									? ($inventoryCounts.data?.team.inventoryCounts.applications.notNais ?? 0) > 0
+									: false
+						},
+						{
+							name: 'Jobs',
+							routeId: '/team/[team]/(teamPages)/jobs',
+							withSubRoutes: true,
+							icon: ArrowCirclepathIcon,
+							inventoryCount: $inventoryCounts.data?.team.inventoryCounts.jobs.total,
+							notNais:
+								$inventoryCounts.data?.team.inventoryCounts.jobs.notNais !== PendingValue
+									? ($inventoryCounts.data?.team.inventoryCounts.jobs.notNais ?? 0) > 0
+									: false
+						},
+						{
+							name: 'Secrets',
+							routeId: '/team/[team]/(teamPages)/secrets',
+							extraRoutes: ['/team/[team]/(teamPages)/[env]/secret/[secret]'],
+							withSubRoutes: true,
+							icon: QuietZoneIcon,
+							memberOnly: true
+						}
+					]
+				},
+				{
+					items: [
+						{
+							name: 'Postgres',
+							routeId: '/team/[team]/(teamPages)/postgres',
+							extraRoutes: ['/team/[team]/(teamPages)/[env]/postgres/[postgres]'],
+							withSubRoutes: true,
+							icon: DatabaseIcon,
+							inventoryCount: $inventoryCounts.data?.team.inventoryCounts.sqlInstances.total
+						},
+						{
+							name: 'Buckets',
+							routeId: '/team/[team]/(teamPages)/buckets',
+							extraRoutes: ['/team/[team]/(teamPages)/[env]/bucket/[bucket]'],
+							withSubRoutes: true,
+							icon: BucketIcon,
+							inventoryCount: $inventoryCounts.data?.team.inventoryCounts.buckets.total
+						},
+						{
+							name: 'Redis',
+							routeId: '/team/[team]/(teamPages)/redis',
+							extraRoutes: ['/team/[team]/(teamPages)/[env]/redis/[redis]'],
+							withSubRoutes: true,
+							icon: Redis,
+							inventoryCount: $inventoryCounts.data?.team.inventoryCounts.redisInstances.total
+						},
+						{
+							name: 'OpenSearch',
+							routeId: '/team/[team]/(teamPages)/opensearch',
+							extraRoutes: ['/team/[team]/(teamPages)/[env]/opensearch/[opensearch]'],
+							withSubRoutes: true,
+							icon: Opensearch,
+							inventoryCount: $inventoryCounts.data?.team.inventoryCounts.openSearchInstances.total
+						},
+						{
+							name: 'Kafka topics',
+							routeId: '/team/[team]/(teamPages)/kafka',
+							extraRoutes: ['/team/[team]/(teamPages)/[env]/kafka/[kafka]'],
+							withSubRoutes: true,
+							icon: Kafka,
+							inventoryCount: $inventoryCounts.data?.team.inventoryCounts.kafkaTopics.total
+						},
+						{
+							name: 'BigQuery',
+							routeId: '/team/[team]/(teamPages)/bigquery',
+							extraRoutes: ['/team/[team]/(teamPages)/[env]/bigquery/[bigquery]'],
+							withSubRoutes: true,
+							icon: BigQuery,
+							inventoryCount: $inventoryCounts.data?.team.inventoryCounts.bigQueryDatasets.total
+						},
+						{
+							name: 'Unleash',
+							routeId: '/team/[team]/(teamPages)/unleash',
+							extraRoutes: ['/team/[team]/(teamPages)/[env]/unleash/[unleash]'],
+							withSubRoutes: true,
+							icon: Unleash
+						}
+					]
+				},
+				{
+					items: [
+						{
+							name: 'Deploys',
+							routeId: '/team/[team]/(teamPages)/deploy',
+							withSubRoutes: true,
+							icon: ArrowsSquarepathIcon
+						},
+						{
+							name: 'Cost',
+							routeId: '/team/[team]/(teamPages)/cost',
+							withSubRoutes: true,
+							icon: CostIcon
+						},
+						{
+							name: 'Utilization',
+							routeId: '/team/[team]/(teamPages)/utilization',
+							withSubRoutes: true,
+							icon: LineGraphStackedIcon
+						},
+						{
+							name: 'Vulnerabilities',
+							routeId: '/team/[team]/(teamPages)/vulnerabilities',
+							withSubRoutes: true,
+							icon: VirusIcon
+						}
+					]
+				},
+				{
+					items: [
+						{
+							name: 'Members',
+							routeId: '/team/[team]/(teamPages)/members',
+							withSubRoutes: true,
+							icon: PersonGroupIcon
+						},
+						{
+							name: 'Repositories',
+							routeId: '/team/[team]/(teamPages)/repositories',
+							withSubRoutes: true,
+							icon: BranchingIcon
+						},
+						{
+							name: 'Settings',
+							routeId: '/team/[team]/(teamPages)/settings',
+							withSubRoutes: true,
+							memberOnly: true,
+							icon: CogIcon
+						},
+						{
+							name: 'Activity log',
+							routeId: '/team/[team]/(teamPages)/activity-log',
+							withSubRoutes: true,
+							memberOnly: true,
+							icon: ShieldLockIcon
+						}
+					]
 				}
-			]
-		},
-		{
-			items: [
-				{
-					name: 'Applications',
-					routeId: '/team/[team]/(teamPages)/applications',
-					withSubRoutes: true,
-					icon: SandboxIcon,
-					inventoryCount: $inventoryCounts.data?.team.inventoryCounts.applications.total,
-					notNais:
-						$inventoryCounts.data?.team.inventoryCounts.applications.notNais !== PendingValue
-							? ($inventoryCounts.data?.team.inventoryCounts.applications.notNais ?? 0) > 0
-							: false
-				},
-				{
-					name: 'Jobs',
-					routeId: '/team/[team]/(teamPages)/jobs',
-					withSubRoutes: true,
-					icon: ArrowCirclepathIcon,
-					inventoryCount: $inventoryCounts.data?.team.inventoryCounts.jobs.total,
-					notNais:
-						$inventoryCounts.data?.team.inventoryCounts.jobs.notNais !== PendingValue
-							? ($inventoryCounts.data?.team.inventoryCounts.jobs.notNais ?? 0) > 0
-							: false
-				},
-				{
-					name: 'Secrets',
-					routeId: '/team/[team]/(teamPages)/secrets',
-					extraRoutes: ['/team/[team]/(teamPages)/[env]/secret/[secret]'],
-					withSubRoutes: true,
-					icon: QuietZoneIcon,
-					memberOnly: true
-				}
-			]
-		},
-		{
-			items: [
-				{
-					name: 'Postgres',
-					routeId: '/team/[team]/(teamPages)/postgres',
-					extraRoutes: ['/team/[team]/(teamPages)/[env]/postgres/[postgres]'],
-					withSubRoutes: true,
-					icon: DatabaseIcon,
-					inventoryCount: $inventoryCounts.data?.team.inventoryCounts.sqlInstances.total
-				},
-				{
-					name: 'Buckets',
-					routeId: '/team/[team]/(teamPages)/buckets',
-					extraRoutes: ['/team/[team]/(teamPages)/[env]/bucket/[bucket]'],
-					withSubRoutes: true,
-					icon: BucketIcon,
-					inventoryCount: $inventoryCounts.data?.team.inventoryCounts.buckets.total
-				},
-				{
-					name: 'Redis',
-					routeId: '/team/[team]/(teamPages)/redis',
-					extraRoutes: ['/team/[team]/(teamPages)/[env]/redis/[redis]'],
-					withSubRoutes: true,
-					icon: Redis,
-					inventoryCount: $inventoryCounts.data?.team.inventoryCounts.redisInstances.total
-				},
-				{
-					name: 'OpenSearch',
-					routeId: '/team/[team]/(teamPages)/opensearch',
-					extraRoutes: ['/team/[team]/(teamPages)/[env]/opensearch/[opensearch]'],
-					withSubRoutes: true,
-					icon: Opensearch,
-					inventoryCount: $inventoryCounts.data?.team.inventoryCounts.openSearchInstances.total
-				},
-				{
-					name: 'Kafka topics',
-					routeId: '/team/[team]/(teamPages)/kafka',
-					extraRoutes: ['/team/[team]/(teamPages)/[env]/kafka/[kafka]'],
-					withSubRoutes: true,
-					icon: Kafka,
-					inventoryCount: $inventoryCounts.data?.team.inventoryCounts.kafkaTopics.total
-				},
-				{
-					name: 'BigQuery',
-					routeId: '/team/[team]/(teamPages)/bigquery',
-					extraRoutes: ['/team/[team]/(teamPages)/[env]/bigquery/[bigquery]'],
-					withSubRoutes: true,
-					icon: BigQuery,
-					inventoryCount: $inventoryCounts.data?.team.inventoryCounts.bigQueryDatasets.total
-				},
-				{
-					name: 'Unleash',
-					routeId: '/team/[team]/(teamPages)/unleash',
-					extraRoutes: ['/team/[team]/(teamPages)/[env]/unleash/[unleash]'],
-					withSubRoutes: true,
-					icon: Unleash
-				}
-			]
-		},
-		{
-			items: [
-				{
-					name: 'Deploys',
-					routeId: '/team/[team]/(teamPages)/deploy',
-					withSubRoutes: true,
-					icon: ArrowsSquarepathIcon
-				},
-				{
-					name: 'Cost',
-					routeId: '/team/[team]/(teamPages)/cost',
-					withSubRoutes: true,
-					icon: CostIcon
-				},
-				{
-					name: 'Utilization',
-					routeId: '/team/[team]/(teamPages)/utilization',
-					withSubRoutes: true,
-					icon: LineGraphStackedIcon
-				},
-				{
-					name: 'Vulnerabilities',
-					routeId: '/team/[team]/(teamPages)/vulnerabilities',
-					withSubRoutes: true,
-					icon: VirusIcon
-				}
-			]
-		},
-		{
-			items: [
-				{
-					name: 'Members',
-					routeId: '/team/[team]/(teamPages)/members',
-					withSubRoutes: true,
-					icon: PersonGroupIcon
-				},
-				{
-					name: 'Repositories',
-					routeId: '/team/[team]/(teamPages)/repositories',
-					withSubRoutes: true,
-					icon: BranchingIcon
-				},
-				{
-					name: 'Settings',
-					routeId: '/team/[team]/(teamPages)/settings',
-					withSubRoutes: true,
-					memberOnly: true,
-					icon: CogIcon
-				},
-				{
-					name: 'Activity log',
-					routeId: '/team/[team]/(teamPages)/activity-log',
-					withSubRoutes: true,
-					memberOnly: true,
-					icon: ShieldLockIcon
-				}
-			]
+			];
+		} else {
+			nav = [];
 		}
-	];
+	}
 
 	function memberOnly(
 		nav: menuGroup[],
