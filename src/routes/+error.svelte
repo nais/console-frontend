e6b8520e1d51bc9f96b0dad1de700ca8772e5052
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-11-27 16:52:24 +0100

    Custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/stores';
+	import { BodyShort, Heading } from '@nais/ds-svelte-community';
+</script>
+
+<div>
+	<Heading size="large">{$page.status}</Heading>
+	<BodyShort>
+		{$page.error?.message || 'Unknown error'}
+	</BodyShort>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 20vh;
+	}
+</style>
