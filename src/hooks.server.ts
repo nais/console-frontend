065bc8575e974aa28194d5d2bfc2961268a46339
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-12-17 10:12:50 +0100

    Add back server hooks
    Co-authored-by: Roger Bjørnstad <roger.bjornstad@nav.no>

---
+++ src/hooks.server.ts
@@ -0,0 +1,17 @@
+import { env } from '$env/dynamic/private';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
+
+export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
+	const cookies = event.request.headers.get('cookie');
+	if (request.url.startsWith('http://nais-api/') && cookies) {
+		request.headers.set('cookie', cookies);
+	}
+
+	return fetch(request);
+};
+
+export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.tenantName = env.TENANT_NAME || '';
+	event.locals.githubOrganization = env.GITHUB_ORGANIZATION || '';
+	return await resolve(event);
+};
