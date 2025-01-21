5dee33ecdbe673a5f943047fa7f68c2ceff73605
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-01-21 11:11:52 +0100

    Remove GitHubLink component

--- src/lib/components/GitHubLink.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	interface Props {
-		repository: string;
-	}
-
-	let { repository }: Props = $props();
-</script>
-
-<a href="https://github.com/{repository}" target="_blank">{repository}</a>
