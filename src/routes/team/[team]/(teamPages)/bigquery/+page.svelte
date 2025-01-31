4512925f29da19a0cb024bb9c0c7efbd550b9dc3
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-01-31 13:36:04 +0100

    feat: add pagination support to BigQuery query with customizable row count and cursor navigation

--- src/routes/team/[team]/(teamPages)/bigquery/+page.svelte
+++ src/routes/team/[team]/(teamPages)/bigquery/+page.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
-	import { BigQueryDatasetOrderField, PendingValue } from '$houdini';
 	import Card from '$lib/Card.svelte';
-	import WorkloadLink from '$lib/components/WorkloadLink.svelte';
 
+	import {
+		BigQueryDatasetOrderField,
+		OrderDirection,
+		type BigQueryDatasetOrderField$options,
+		type OrderDirection$options
+	} from '$houdini';
 	import Cost from '$lib/components/Cost.svelte';
 	import SummaryCard from '$lib/components/SummaryCard.svelte';
 	import GraphErrors from '$lib/GraphErrors.svelte';
+	import BigQueryIcon from '$lib/icons/BigQueryIcon.svelte';
+	import SortAscendingIcon from '$lib/icons/SortAscendingIcon.svelte';
+	import SortDescendingIcon from '$lib/icons/SortDescendingIcon.svelte';
 	import Pagination from '$lib/Pagination.svelte';
-	import { resourceLink } from '$lib/utils/links';
 	import { changeParams } from '$lib/utils/searchparams.svelte';
-	import { Skeleton, Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
-	import { WalletIcon } from '@nais/ds-svelte-community/icons';
+	import { BodyShort, Button, Detail } from '@nais/ds-svelte-community';
+	import {
+		ActionMenu,
+		ActionMenuDivider,
+		ActionMenuRadioGroup,
+		ActionMenuRadioItem
+	} from '@nais/ds-svelte-community/experimental.js';
+	import { ChevronDownIcon, WalletIcon } from '@nais/ds-svelte-community/icons';
 	import type { PageData } from './$houdini';
 
 	interface Props {
@@ -19,25 +31,54 @@
 
 	let { data }: Props = $props();
 
-	let { BigQuery, teamSlug } = $derived(data);
+	let { BigQuery /*, teamSlug*/ } = $derived(data);
 
-	let tableSort = $derived({
-		orderBy: $BigQuery.variables?.orderBy?.field,
-		direction: $BigQuery.variables?.orderBy?.direction
-	});
+	let rows: number = $derived.by(
+		() => $BigQuery.variables?.first ?? $BigQuery.variables?.last ?? 10
+	);
 
-	const tableSortChange = (key: string) => {
-		if (key === tableSort.orderBy) {
-			const direction = tableSort.direction === 'ASC' ? 'DESC' : 'ASC';
-			tableSort.direction = direction;
-		} else {
-			tableSort.orderBy = BigQueryDatasetOrderField[key as keyof typeof BigQueryDatasetOrderField];
-			tableSort.direction = 'ASC';
-		}
+	let after: string = $derived($BigQuery.variables?.after ?? '');
+	let before: string = $derived($BigQuery.variables?.before ?? '');
 
+	let orderField: keyof typeof BigQueryDatasetOrderField = $derived(
+		$BigQuery.variables?.orderBy?.field ?? BigQueryDatasetOrderField.NAME
+	);
+
+	let orderDirection: keyof typeof OrderDirection = $derived(
+		$BigQuery.variables?.orderBy?.direction ?? OrderDirection.ASC
+	);
+
+	const handleSortDirection = (key: string) => {
+		changeQuery({ direction: key as OrderDirection$options });
+	};
+
+	const handleSortField = (key: string) => {
+		changeQuery({
+			field: key as keyof typeof BigQueryDatasetOrderField
+		});
+	};
+
+	const handleNumberOfRows = (value: number) => {
+		console.log(value);
+		changeQuery({ newRows: value, resetPagination: true });
+	};
+
+	const changeQuery = (
+		params: {
+			field?: BigQueryDatasetOrderField$options;
+			direction?: OrderDirection$options;
+			newRows?: number;
+			after?: string;
+			before?: string;
+			resetPagination?: boolean;
+		} = {}
+	) => {
 		changeParams({
-			direction: tableSort.direction,
-			field: tableSort.orderBy || BigQueryDatasetOrderField.NAME
+			direction: params.direction || orderDirection,
+			field: params.field || orderField,
+			rows: params.newRows?.toString() || rows.toString(),
+			before: params.resetPagination ? '' : (params.before ?? before),
+			after: params.resetPagination ? '' : (params.after ?? after)
 		});
 	};
 </script>
@@ -47,6 +88,12 @@
 {#if $BigQuery.data}
 	{@const cost = $BigQuery.data.team.cost}
 	{@const datasets = $BigQuery.data.team.bigQueryDatasets}
+	<div class="header">
+		<div class="heading">
+			<BigQueryIcon size="32px" />
+			<h3>BigQuery datasets</h3>
+		</div>
+	</div>
 	<div class="summary-grid">
 		<Card columns={3}>
 			<SummaryCard
@@ -57,16 +104,120 @@
 				{#snippet icon({ color })}
 					<WalletIcon height="32px" width="32px" {color} />
 				{/snippet}
-				{#if cost !== PendingValue}
-					<Cost cost={cost.daily.sum} />
-				{:else}
-					<Skeleton variant="text" />
-				{/if}
+				<Cost cost={cost.daily.sum} />
 			</SummaryCard>
 		</Card>
 	</div>
 	<Card columns={12}>
-		<Table
+		<div class="list">
+			<div class="list-header">
+				<div class="count">
+					<BodyShort size="small" style="font-weight: bold;">
+						{datasets.pageInfo.totalCount} entries
+					</BodyShort>
+				</div>
+				<div style="display: flex; gap: 1rem;">
+					<div style="display: flex; gap: 1rem;">
+						<ActionMenu>
+							{#snippet trigger(props)}
+								<Button
+									variant="tertiary-neutral"
+									size="small"
+									iconPosition="right"
+									{...props}
+									icon={ChevronDownIcon}
+								>
+									<span style="font-weight: normal"># of rows</span>
+								</Button>
+							{/snippet}
+							{#key orderField}
+								<ActionMenuRadioGroup value={orderField} label="Order by">
+									<ActionMenuRadioItem
+										value={BigQueryDatasetOrderField.NAME}
+										onselect={(value) => {
+											handleSortField(value as string);
+										}}>Name</ActionMenuRadioItem
+									>
+
+									<ActionMenuRadioItem
+										value={BigQueryDatasetOrderField.ENVIRONMENT}
+										onselect={(value) => {
+											handleSortField(value as string);
+										}}>Environment</ActionMenuRadioItem
+									>
+								</ActionMenuRadioGroup>
+							{/key}
+							<ActionMenuDivider />
+							{#key orderDirection}
+								<ActionMenuRadioGroup value={orderDirection} label="Sort direction">
+									<ActionMenuRadioItem
+										value={OrderDirection.ASC}
+										onselect={(value) => {
+											handleSortDirection(value as string);
+										}}
+									>
+										<div class="icon">
+											<SortAscendingIcon size="1rem" />Ascending
+										</div>
+									</ActionMenuRadioItem>
+									<ActionMenuRadioItem
+										value={OrderDirection.DESC}
+										onselect={(value) => {
+											handleSortDirection(value as string);
+										}}
+									>
+										<div class="icon">
+											<SortDescendingIcon size="1rem" />Descending
+										</div>
+									</ActionMenuRadioItem>
+								</ActionMenuRadioGroup>
+							{/key}
+							<ActionMenuDivider />
+							{#key rows}
+								<ActionMenuRadioGroup value={rows} label="Rows per page">
+									<ActionMenuRadioItem
+										value="1"
+										onselect={(value) => handleNumberOfRows(value as number)}>1</ActionMenuRadioItem
+									>
+									<ActionMenuRadioItem
+										value="5"
+										onselect={(value) => handleNumberOfRows(value as number)}>5</ActionMenuRadioItem
+									>
+									<ActionMenuRadioItem
+										value="10"
+										onselect={(value) => handleNumberOfRows(value as number)}
+										>10</ActionMenuRadioItem
+									>
+									<ActionMenuRadioItem
+										value="25"
+										onselect={(value) => handleNumberOfRows(value as number)}
+										>25</ActionMenuRadioItem
+									>
+									<ActionMenuRadioItem
+										value="50"
+										onselect={(value) => handleNumberOfRows(value as number)}
+										>50</ActionMenuRadioItem
+									>
+								</ActionMenuRadioGroup>
+							{/key}
+						</ActionMenu>
+					</div>
+				</div>
+			</div>
+			{#each datasets.nodes as ds}
+				<div class="list-item">
+					<div class="activity-link-wrapper">
+						<div class="list-link">
+							{ds.name}
+							<Detail>{ds.environment.name}</Detail>
+						</div>
+					</div>
+				</div>
+			{:else}
+				<p>No BigQuery datasets</p>
+			{/each}
+		</div>
+		<!--Table
 			size="small"
 			zebraStripes
 			sort={{
@@ -85,33 +236,21 @@
 			<Tbody>
 				{#each datasets.nodes as ds}
 					<Tr>
-						{#if ds !== PendingValue}
-							<Td>
-								<a href={resourceLink(ds.environment.name, teamSlug, 'bigquery', ds.name)}>
-									{ds.name}
-								</a>
-							</Td>
-							<Td>
-								{ds.environment.name}
-							</Td>
-							<Td>
-								{#if ds.workload}
-									<WorkloadLink workload={ds.workload} showIcon={true} />
-								{:else}
-									<em>No owner</em>
-								{/if}
-							</Td>
-						{:else}
-							<Td>
-								<Skeleton variant="text" />
-							</Td>
-							<Td>
-								<Skeleton variant="text" />
-							</Td>
-							<Td>
-								<Skeleton variant="text" />
-							</Td>
-						{/if}
+						<Td>
+							<a href={resourceLink(ds.environment.name, teamSlug, 'bigquery', ds.name)}>
+								{ds.name}
+							</a>
+						</Td>
+						<Td>
+							{ds.environment.name}
+						</Td>
+						<Td>
+							{#if ds.workload}
+								<WorkloadLink workload={ds.workload} showIcon={true} />
+							{:else}
+								<em>No owner</em>
+							{/if}
+						</Td>
 					</Tr>
 				{:else}
 					<Tr>
@@ -119,22 +258,87 @@
 					</Tr>
 				{/each}
 			</Tbody>
-		</Table>
-		{#if datasets.pageInfo !== PendingValue}
-			{#if datasets.pageInfo.hasPreviousPage || datasets.pageInfo.hasNextPage}
-				<Pagination
-					page={datasets.pageInfo}
-					loaders={{
-						loadPreviousPage: () => BigQuery.loadPreviousPage(),
-						loadNextPage: () => BigQuery.loadNextPage()
-					}}
-				/>
-			{/if}
+		</Table-->
+		{#if datasets.pageInfo.hasPreviousPage || datasets.pageInfo.hasNextPage}
+			<Pagination
+				page={datasets.pageInfo}
+				loaders={{
+					loadPreviousPage: () => {
+						changeQuery({ before: datasets.pageInfo.startCursor ?? '' });
+						BigQuery.loadPreviousPage({ last: rows });
+					},
+					loadNextPage: () => {
+						changeQuery({ after: datasets.pageInfo.endCursor ?? '' });
+						BigQuery.loadNextPage({ first: rows });
+					}
+				}}
+			/>
 		{/if}
 	</Card>
 {/if}
 
 <style>
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		align-self: stretch;
+		margin: 1rem 0;
+		.heading {
+			display: flex;
+			align-items: center;
+			width: 50%;
+			gap: 4px;
+			h3 {
+				margin: 0;
+			}
+		}
+	}
+
+	.list {
+		border: 1px solid var(--a-border-default);
+		border-radius: 4px;
+
+		.list-header {
+			background-color: var(--active-color);
+			border-radius: 4px 4px 0 0;
+			border-bottom: 1px solid var(--a-border-default);
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 8px 12px;
+		}
+		.count {
+			font-weight: bold;
+		}
+		.list-item {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 8px 12px;
+
+			&:not(:last-of-type) {
+				border-bottom: 1px solid var(--a-border-default);
+			}
+
+			&:hover {
+				background-color: var(--a-surface-subtle);
+			}
+
+			.list-link {
+				:global(a) {
+					font-weight: var(--a-font-weight-bold);
+					&:not(:active) {
+						color: var(--a-text-defualt);
+					}
+					text-decoration: none;
+					&:hover {
+						text-decoration: underline;
+					}
+				}
+			}
+		}
+	}
 	.summary-grid {
 		display: grid;
 		grid-template-columns: repeat(12, 1fr);
--- src/routes/team/[team]/(teamPages)/bigquery/+page.ts
+++ src/routes/team/[team]/(teamPages)/bigquery/+page.ts
@@ -1,14 +1,20 @@
+import type { BigQueryDatasetOrder } from '$houdini';
 import {
 	BigQueryDatasetOrderField,
 	type BigQueryDatasetOrderField$options,
 	type OrderDirection$options
 } from '$houdini';
+import type { AfterLoadEvent } from '../jobs/$houdini';
 import type { BigQueryVariables } from './$houdini';
 
 export const _BigQueryVariables: BigQueryVariables = ({ url }) => {
 	const field = (url.searchParams.get('field') ||
 		BigQueryDatasetOrderField.NAME) as BigQueryDatasetOrderField$options;
 	const direction = (url.searchParams.get('direction') || 'ASC') as OrderDirection$options;
+	const rows: number = parseInt(url.searchParams.get('rows') || '10');
+
+	const after = url.searchParams.get('after') || '';
+	const before = url.searchParams.get('before') || '';
 
 	// Date 30 days ago
 	const from = new Date();
@@ -18,5 +24,23 @@ export const _BigQueryVariables: BigQueryVariables = ({ url }) => {
 	const to = new Date();
 	to.setDate(to.getDate() - 1);
 
-	return { orderBy: { field: field, direction: direction }, from, to };
+	return {
+		orderBy: { field: field, direction: direction } as BigQueryDatasetOrder,
+		...(before ? { before, last: rows } : { after, first: rows }),
+		from,
+		to
+	};
 };
+
+export function _houdini_afterLoad({ data, event: { url } }: AfterLoadEvent) {
+	return {
+		data,
+		initialRows: parseInt(url.searchParams.get('rows') || '10'),
+		initialAfter: url.searchParams.get('after') || '',
+		initialBefore: url.searchParams.get('before') || ''
+		/*initialField:
+			(url.searchParams.get('field') as keyof typeof BigQueryDatasetOrderField) ||
+			BigQueryDatasetOrderField.NAME,
+		initialDirection: (url.searchParams.get('direction') as keyof typeof OrderDirection) || 'ASC'*/
+	};
+}
