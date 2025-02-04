6ebb227004d07f6d2ff91c6dfbd4ca8420bd7805
Andreas Nordahl <andnorda@gmail.com>
2025-02-04 14:34:13 +0100

    ProgressBar component

---
+++ src/lib/ProgressBar.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	let progress = $state(10);
+
+	$effect(() => {
+		const id = setInterval(() => {
+			progress += (Math.random() * 50) / progress;
+		}, 100);
+
+		return () => clearInterval(id);
+	});
+
+	export function fade(node: HTMLElement) {
+		return {
+			delay: 300,
+			duration: 1,
+			css: (t: number) => `opacity: ${t}`
+		};
+	}
+
+	function complete(node: HTMLElement) {
+		return {
+			duration: 200,
+			css: (t: number, u: number) => `
+                width: ${progress + (100 - progress) * u}vw; 
+            `
+		};
+	}
+</script>
+
+<div class="progress-bar" style="width: {progress}vw;" in:fade out:complete></div>
+
+<style>
+	.progress-bar {
+		position: fixed;
+		display: block;
+		top: 0;
+		left: 0;
+		height: 3px;
+		background: #0076ff;
+		z-index: 2147483647;
+		transition: width 300ms ease-out;
+	}
+</style>
