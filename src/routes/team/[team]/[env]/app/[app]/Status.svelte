3483d2c214933676d241b24b68ce178f5f6707af
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-02-07 15:47:41 +0100

    feat: enhance Status component to improve application state display and error indication

--- src/routes/team/[team]/[env]/app/[app]/Status.svelte
+++ src/routes/team/[team]/[env]/app/[app]/Status.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { fragment, graphql, type AppStatus } from '$houdini';
-	import Nais from '$lib/icons/Nais.svelte';
+	import { fragment, graphql, WorkloadState, type AppStatus } from '$houdini';
 	import {
+		CheckmarkCircleFillIcon,
 		ExclamationmarkTriangleFillIcon,
-		QuestionmarkDiamondFillIcon
+		QuestionmarkDiamondFillIcon,
+		XMarkOctagonFillIcon
 	} from '@nais/ds-svelte-community/icons';
 
 	interface Props {
@@ -37,97 +38,26 @@
 
 {#if $data.status}
 	{@const state = $data.status.state}
-	<div class="card {state.toString()}">
-		{#if $data.status.state === 'NAIS'}
-			<h4>Status</h4>
-			<div class="iconWrapper">
-				<Nais
-					size="5rem"
-					style="color: var(--a-icon-success)"
-					aria-label="Application is nais"
-					role="image"
-				/>
-				{#if $data.status?.errors.length > 0}
-					<p>
-						<a href="/team/{$data.team.slug}/{$data.environment.name}/app/{$data.name}/status"
-							>{$data.status.errors.length} todo{$data.status.errors.length > 1 ? 's' : ''}</a
-						>
-					</p>
-				{/if}
-			</div>
-		{:else if $data.status.state === 'FAILING'}
-			<h4>Status <ExclamationmarkTriangleFillIcon style="color: var(--a-icon-danger)" /></h4>
-			<div>
-				Application is failing.<br />
-				<a
-					class="status"
-					href="/team/{$data.team.slug}/{$data.environment.name}/app/{$data.name}/status"
-				>
-					{$data.status.errors.length}
-					{$data.status.errors.length > 1 ? 'issues' : 'issue'}
-				</a> detected.
-			</div>
-		{:else if $data.status.state === 'NOT_NAIS'}
-			<h4>Status <ExclamationmarkTriangleFillIcon style="color: var(--a-icon-warning)" /></h4>
-			<div>
-				Application is not nais.<br />
-				<a
-					class="status"
-					href="/team/{$data.team.slug}/{$data.environment.name}/app/{$data.name}/status"
-				>
-					{$data.status.errors.length}
-					{$data.status.errors.length > 1 ? 'issues' : 'issue'}
-				</a>
-				detected.
-			</div>
-		{:else if $data.status.state === 'UNKNOWN'}
-			<h4>Status <QuestionmarkDiamondFillIcon /></h4>
-			<div>Application status is unknown.</div>
+	{@const nErrors = $data.status.errors.length}
+	<div style="display: flex; gap: var(--a-spacing-1);">
+		{#if state === WorkloadState.NAIS}
+			<CheckmarkCircleFillIcon style="color: var(--a-icon-success); font-size: 1.25rem" />
+			Application is nais.
+		{:else if state === WorkloadState.NOT_NAIS}
+			<ExclamationmarkTriangleFillIcon style="color: var(--a-icon-warning); font-size: 1.25rem" />
+			Application is not nais.
+		{:else if state === WorkloadState.FAILING}
+			<XMarkOctagonFillIcon style="color: var(--a-icon-danger); font-size: 1.25rem" />
+			Application is failing.
+		{:else if state === WorkloadState.UNKNOWN}
+			<QuestionmarkDiamondFillIcon style="color: var(--a-icon-action); font-size: 1.25rem" />
+			Application status unknown.
 		{/if}
 	</div>
+	{#if nErrors > 0}
+		<a href="/team/{$data.team.slug}/{$data.environment.name}/job/{$data.name}/status">
+			{nErrors}
+			{`${state === WorkloadState.NAIS ? 'todo' : 'issue'}${nErrors > 1 ? 's' : ''}`}
+		</a>
+	{/if}
 {/if}
-
-<style>
-	h4 {
-		margin-bottom: 8px;
-		display: flex;
-		align-items: center;
-		gap: 0.3rem;
-	}
-	.iconWrapper {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		vertical-align: middle;
-		padding-top: 1rem;
-	}
-
-	.card {
-		border-radius: 0.5rem;
-		padding: 1rem;
-		grid-column: span 4;
-		grid-row: span 1;
-		background-color: var(--a-bg-default);
-		border: 1px solid var(--active-color-strong);
-	}
-
-	.UNKOWN {
-		background-color: var(--a-bg-default);
-	}
-
-	.NAIS {
-		background-color: var(--a-bg-default);
-	}
-
-	.NOT_NAIS {
-		background-color: var(--a-surface-warning-moderate);
-		color: var(--a-text-on-warning);
-		border: 1px solid var(--a-border-warning);
-	}
-
-	.FAILING {
-		background-color: var(--a-surface-danger-subtle);
-		border: 1px solid var(--a-border-danger);
-	}
-</style>
