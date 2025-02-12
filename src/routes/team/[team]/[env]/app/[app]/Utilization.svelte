2c9d00bd7e37a0ebe5bbdfecddabdf8e23538910
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-02-12 11:57:30 +0100

    fix: remove Scaling component and integrate scaling data into Utilization component

--- src/routes/team/[team]/[env]/app/[app]/Utilization.svelte
+++ src/routes/team/[team]/[env]/app/[app]/Utilization.svelte
@@ -25,6 +25,23 @@
 					environment {
 						name
 					}
+					resources {
+						scaling {
+							maxInstances
+							minInstances
+							strategies {
+								__typename
+								... on KafkaLagScalingStrategy {
+									consumerGroup
+									threshold
+									topicName
+								}
+								... on CPUScalingStrategy {
+									threshold
+								}
+							}
+						}
+					}
 					utilization {
 						cpuUsage: current(resourceType: CPU)
 						cpuRequests: requested(resourceType: CPU)
@@ -35,6 +52,16 @@
 			`)
 		)
 	);
+
+	const renameStrategy = (type: string) => {
+		if (type === 'CPUScalingStrategy') {
+			return 'CPU usage';
+		} else if (type === 'KafkaLagScalingStrategy') {
+			return 'Kafka Lag';
+		} else {
+			return 'Unknown';
+		}
+	};
 </script>
 
 <Heading level="3" size="small" spacing>Utilization</Heading>
@@ -46,6 +73,7 @@
 		{@const memUsage = $data.utilization.memoryUsage}
 		{@const cpuUtil = cpuUtilization(cpu, cpuUsage)}
 		{@const memUtil = memoryUtilization(mem, memUsage)}
+
 		<div>
 			<IconWithText
 				icon={CpuIcon}
@@ -62,6 +90,31 @@
 				size="medium"
 			/>
 		</div>
+		{#if $data.resources.scaling}
+			{@const scaling = $data.resources.scaling}
+			{#if scaling.minInstances !== scaling.maxInstances}
+				<Heading level="4" size="xsmall">Scaling configuration</Heading>
+				<div class="resources">
+					<div>
+						{scaling.minInstances} - {scaling.maxInstances} instances based on
+						{#if scaling.strategies && scaling.strategies.length > 0}
+							{#each scaling.strategies as strategy, i (strategy.__typename + i)}
+								{#if i > 0}
+									<br />and{/if}
+								<b>{renameStrategy(strategy.__typename)}</b>
+								{#if strategy.__typename === 'KafkaLagScalingStrategy'}
+									(threshold: {strategy.threshold})
+								{:else}
+									(threshold: {strategy.threshold}%)
+								{/if}
+							{/each}
+						{:else}
+							<b>CPU usage</b> (threshold: 50%)
+						{/if}
+					</div>
+				</div>
+			{/if}
+		{/if}
 		<div style="margin-top: var(--a-spacing-2)">
 			<a href="/team/{$data.team.slug}/{$data.environment.name}/app/{$data.name}/utilization">
 				View details
@@ -75,5 +128,9 @@
 		display: flex;
 		flex-direction: column;
 		gap: var(--a-spacing-2);
+		color: var(--a-text-subtle);
+	}
+	.resources {
+		color: var(--a-text-subtle);
 	}
 </style>
