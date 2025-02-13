cff846d5398915bb97f58300ad692bc3616f74fa
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-02-13 08:57:23 +0100

    fix: add unique keys to TeamDeployments component for improved rendering performance

--- src/lib/components/TeamDeployments.svelte
+++ src/lib/components/TeamDeployments.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { fragment, graphql, PendingValue, type TeamDeployments } from '$houdini';
+	import { fragment, graphql, type TeamDeployments } from '$houdini';
 	import DeploymentStatus from '$lib/DeploymentStatus.svelte';
 	import Time from '$lib/Time.svelte';
-	import { Skeleton, Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
+	import { Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
 	import WorkloadLink from './WorkloadLink.svelte';
 
 	interface Props {
@@ -16,8 +16,9 @@
 			team,
 			graphql(`
 				fragment TeamDeployments on Team {
-					deployments @loading {
-						nodes @loading {
+					deployments {
+						nodes {
+							id
 							statuses {
 								nodes {
 									state
@@ -27,6 +28,7 @@
 							}
 							resources {
 								nodes {
+									id
 									kind
 									name
 								}
@@ -53,65 +55,47 @@
 	</Thead>
 	<Tbody>
 		{#if $data.deployments !== null}
-			{#each $data.deployments.nodes as deploy}
+			{#each $data.deployments.nodes as deploy (deploy.id)}
 				<Tr>
-					{#if deploy !== PendingValue}
-						<Td
-							>{#each deploy.resources.nodes as resource}
-								{#if resource.kind === 'Application'}
-									<WorkloadLink
-										workload={{
-											__typename: 'App',
-											environment: { name: deploy.environmentName },
-											team: { slug: deploy.teamSlug },
-											name: resource.name
-										}}
-										showIcon={true}
-									/>
-								{:else if resource.kind === 'Job' || resource.kind === 'Naisjob'}
-									<WorkloadLink
-										workload={{
-											__typename: 'Job',
-											environment: { name: deploy.environmentName },
-											team: { slug: deploy.teamSlug },
-											name: resource.name
-										}}
-										showIcon={true}
-									/>
-								{:else}
-									<span style="color:var(--a-gray-600)">{resource.kind}:</span>
-									{resource.name}
-								{/if}
-								<br />
-							{/each}</Td
-						>
-						<Td>
-							{deploy.environmentName}
-						</Td>
-						<Td><Time time={deploy.createdAt} distance={true} /></Td>
+					<Td
+						>{#each deploy.resources.nodes as resource (resource.id)}
+							{#if resource.kind === 'Application'}
+								<WorkloadLink
+									workload={{
+										__typename: 'App',
+										environment: { name: deploy.environmentName },
+										team: { slug: deploy.teamSlug },
+										name: resource.name
+									}}
+									showIcon={true}
+								/>
+							{:else if resource.kind === 'Job' || resource.kind === 'Naisjob'}
+								<WorkloadLink
+									workload={{
+										__typename: 'Job',
+										environment: { name: deploy.environmentName },
+										team: { slug: deploy.teamSlug },
+										name: resource.name
+									}}
+									showIcon={true}
+								/>
+							{:else}
+								<span style="color:var(--a-gray-600)">{resource.kind}:</span>
+								{resource.name}
+							{/if}
+							<br />
+						{/each}</Td
+					>
+					<Td>
+						{deploy.environmentName}
+					</Td>
+					<Td><Time time={deploy.createdAt} distance={true} /></Td>
 
-						<Td
-							>{#if deploy.statuses.nodes.length === 0}<DeploymentStatus
-									status={'UNKNOWN'}
-								/>{:else}<DeploymentStatus status={deploy.statuses.nodes[0].state} />{/if}</Td
-						>
-					{:else}
-						<Td>
-							<Skeleton variant="text" />
-						</Td>
-						<Td>
-							<Skeleton variant="text" />
-						</Td>
-						<Td>
-							<Skeleton variant="text" />
-						</Td>
-						<Td>
-							<Skeleton variant="text" />
-						</Td>
-						<Td>
-							<Skeleton variant="text" />
-						</Td>
-					{/if}
+					<Td
+						>{#if deploy.statuses.nodes.length === 0}<DeploymentStatus
+								status={'UNKNOWN'}
+							/>{:else}<DeploymentStatus status={deploy.statuses.nodes[0].state} />{/if}</Td
+					>
 				</Tr>
 			{:else}
 				<Tr>
