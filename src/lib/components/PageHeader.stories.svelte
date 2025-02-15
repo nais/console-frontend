45ffb6aa041597e323101218b54bc1853148b9e5
Andreas Nordahl <andnorda@gmail.com>
2025-02-15 07:06:42 +0100

    Page header component

---
+++ src/lib/components/PageHeader.stories.svelte
@@ -0,0 +1,40 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import PageHeader from './PageHeader.svelte';
+
+	const { Story } = defineMeta({
+		component: PageHeader,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Manifest">
+	<PageHeader
+		breadcrumbs={[
+			{ label: 'devteam', href: '#' },
+			{ label: 'Applications', href: '#' },
+			{ label: 'app-w-all-storage', href: '#' }
+		]}
+		heading="Manifest"
+		subheading="dev"
+	/>
+</Story>
+
+<Story name="Job">
+	<PageHeader
+		breadcrumbs={[
+			{ label: 'nais', href: '#' },
+			{ label: 'jobs', href: '#' }
+		]}
+		heading="slack-teams-notification"
+		subheading="prod-gcp"
+	/>
+</Story>
+
+<Story name="Jobs">
+	<PageHeader breadcrumbs={[{ label: 'nais', href: '#' }]} heading="Jobs" />
+</Story>
+
+<Story name="Team">
+	<PageHeader breadcrumbs={[]} heading="nais" />
+</Story>
