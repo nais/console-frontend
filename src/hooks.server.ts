d29fb2bb11e00fdd86e5c6b21751100310de8cb1
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-02-17 11:20:06 +0100

    fix: conditionally remove 'Link' header from server response

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -13,10 +13,13 @@ export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.tenantName = env.TENANT_NAME || '';
 	event.locals.githubOrganization = env.GITHUB_ORGANIZATION || '';
+
 	const response = await resolve(event, {
 		filterSerializedResponseHeaders: () => true
 	});
 
-	response.headers.delete('link');
+	if (response.headers.get('Link')) {
+		response.headers.delete('Link');
+	}
 	return response;
 };
