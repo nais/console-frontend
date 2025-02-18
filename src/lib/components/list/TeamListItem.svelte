aeb56f5e693c0a9cb2674d9a92d3be7dbf0983d6
Andreas Nordahl <andnorda@gmail.com>
2025-02-18 17:34:24 +0100

    IconLabel in TeamListItem

--- src/lib/components/list/TeamListItem.svelte
+++ src/lib/components/list/TeamListItem.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { Heading, Link } from '@nais/ds-svelte-community';
 	import { PersonGroupIcon } from '@nais/ds-svelte-community/icons';
-	import IconWithText from '../IconWithText.svelte';
+	import IconLabel from '../IconLabel.svelte';
 	import ListItem from './ListItem.svelte';
 
 	const { team }: { team: { slug: string; purpose: string } } = $props();
 </script>
 
 <ListItem>
-	<IconWithText size="large" icon={PersonGroupIcon} description={team.purpose}>
-		{#snippet text()}
-			<Heading level="4" size="xsmall">
-				<Link href="/team/{team.slug}">{team.slug}</Link>
-			</Heading>
-		{/snippet}
-	</IconWithText>
+	<IconLabel
+		label={team.slug}
+		icon={PersonGroupIcon}
+		description={team.purpose}
+		size="large"
+		level="3"
+		href="/team/{team.slug}"
+	/>
 </ListItem>
