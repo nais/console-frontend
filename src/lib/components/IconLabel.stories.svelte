7d98c5d2e0511244833a8892e44409f3e87d25eb
Andreas Nordahl <andnorda@gmail.com>
2025-02-18 16:31:27 +0100

    new icon label component

---
+++ src/lib/components/IconLabel.stories.svelte
@@ -0,0 +1,58 @@
+<script module>
+	import {
+		PackageIcon,
+		PersonGroupIcon,
+		RocketIcon,
+		WalletIcon
+	} from '@nais/ds-svelte-community/icons';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import IconLabel from './IconLabel.svelte';
+
+	const { Story } = defineMeta({
+		component: IconLabel,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Menu item">
+	<IconLabel label="Cost" icon={WalletIcon} />
+</Story>
+
+<Story name="Team without desc">
+	<IconLabel label="nais" href="/team/nais" icon={PersonGroupIcon} size="large" level="3" />
+</Story>
+
+<Story name="Team">
+	<IconLabel
+		label="nais"
+		href="/team/nais"
+		icon={PersonGroupIcon}
+		description="The Nais platform team"
+		size="large"
+		level="3"
+	/>
+</Story>
+
+<Story name="Team long desc">
+	<IconLabel
+		label="nais"
+		href="/team/nais"
+		icon={PersonGroupIcon}
+		description="The Nais platform team lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+		size="large"
+		level="3"
+	/>
+</Story>
+
+<Story name="Workload link">
+	<IconLabel
+		label="dataprodukt-annulleringer"
+		href="/team/tbd/dev-gcp/app/dataprodukt-annulleringer"
+		icon={PackageIcon}
+		description="tbd / dev-gcp"
+	/>
+</Story>
+
+<Story name="Last deploy">
+	<IconLabel label="22 days ago" icon={RocketIcon} size="small" />
+</Story>
---
+++ src/lib/components/IconLabel.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import { BodyShort, Detail, Heading, Link } from '@nais/ds-svelte-community';
+	import type { HeadingProps } from '@nais/ds-svelte-community/components/typography/Heading/type.js';
+	import type { Component } from 'svelte';
+
+	const {
+		label,
+		href,
+		icon: Icon,
+		description,
+		...rest
+	}: {
+		label: string;
+		href?: string;
+		icon: Component;
+		description?: string;
+	} & (
+		| {
+				size?: 'small' | 'medium';
+		  }
+		| {
+				size: 'large';
+				level: HeadingProps['level'];
+		  }
+	) = $props();
+</script>
+
+<div
+	class={[
+		'icon-label',
+		`icon-label--${rest.size ?? 'medium'}`,
+		{ 'icon-label--with-desc': !!description }
+	]}
+>
+	<Icon />
+	<div>
+		{#if rest.size === 'small'}
+			<Detail>
+				{#if href}
+					<Link {href}>{label}</Link>
+				{:else}
+					{label}
+				{/if}
+			</Detail>
+		{:else if rest.size === 'large'}
+			<Heading size="small" level={rest.level}>
+				{#if href}
+					<Link {href}>{label}</Link>
+				{:else}
+					{label}
+				{/if}
+			</Heading>
+		{:else}
+			<BodyShort>
+				{#if href}
+					<Link {href}>{label}</Link>
+				{:else}
+					{label}
+				{/if}
+			</BodyShort>
+		{/if}
+		{#if description}
+			<Detail>{description}</Detail>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.icon-label {
+		display: flex;
+		align-items: center;
+		gap: var(--a-spacing-2);
+		font-size: 1.25rem;
+
+		> * {
+			flex: 1 1 0;
+		}
+
+		:global(a) {
+			color: inherit;
+			text-decoration: none;
+
+			&:hover {
+				text-decoration: underline;
+			}
+
+			&:active,
+			&:focus-visible {
+				color: var(--ac-link-focus-text, var(--a-text-on-action));
+			}
+		}
+	}
+	.icon-label--small {
+		gap: var(--a-spacing-1);
+		font-size: var(--a-font-size-small);
+	}
+	.icon-label--medium {
+		gap: var(--a-spacing-2);
+	}
+	.icon-label--large {
+		gap: var(--a-spacing-2);
+	}
+	.icon-label--with-desc {
+		font-size: 1.5rem;
+	}
+	.icon-label--large.icon-label--with-desc {
+		gap: var(--a-spacing-3);
+	}
+</style>
