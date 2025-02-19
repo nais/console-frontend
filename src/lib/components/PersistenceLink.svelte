1c0cc57e1bd13590572722bf79dce0e63afb9212
Andreas Nordahl <andnorda@gmail.com>
2025-02-19 15:09:47 +0100

    oops

--- src/lib/components/PersistenceLink.svelte
+++ src/lib/components/PersistenceLink.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-	import BigQueryIcon from '$lib/icons/BigQueryIcon.svelte';
-	import KafkaIcon from '$lib/icons/KafkaIcon.svelte';
-	import OpenSearchIcon from '$lib/icons/OpenSearchIcon.svelte';
-	import RedisIcon from '$lib/icons/RedisIcon.svelte';
-	import ValkeyIcon from '$lib/icons/ValkeyIcon.svelte';
-	import { BucketIcon, DatabaseIcon } from '@nais/ds-svelte-community/icons';
+	import { Heading, Link } from '@nais/ds-svelte-community';
 
 	interface Props {
 		instance: {
@@ -17,59 +12,34 @@
 				readonly slug: string;
 			};
 		};
-		showIcon?: boolean;
 	}
 
-	let { instance, showIcon = false }: Props = $props();
+	let { instance }: Props = $props();
+
+	const urlName = () => {
+		switch (instance.__typename) {
+			case 'BigQueryDataset':
+				return 'bigquery';
+			case 'Bucket':
+				return 'bucket';
+			case 'KafkaTopic':
+				return 'kafka';
+			case 'OpenSearch':
+				return 'opensearch';
+			case 'RedisInstance':
+				return 'redis';
+			case 'SqlInstance':
+				return 'postgres';
+			case 'ValkeyInstance':
+				return 'valkey';
+			default:
+				throw new Error(`Unknown instance type ${instance.__typename}`);
+		}
+	};
 </script>
 
-{#if instance.__typename === 'BigQueryDataset'}
-	<a href="/team/{instance.team.slug}/{instance.environment.name}/bigquery/{instance.name}">
-		{#if showIcon}
-			<BigQueryIcon />
-		{/if}
+<Heading size="small" level="4">
+	<Link href="/team/{instance.team.slug}/{instance.environment.name}/{urlName()}/{instance.name}">
 		{instance.name}
-	</a>
-{:else if instance.__typename === 'Bucket'}
-	<a href="/team/{instance.team.slug}/{instance.environment.name}/bucket/{instance.name}">
-		{#if showIcon}
-			<BucketIcon />
-		{/if}
-		{instance.name}
-	</a>
-{:else if instance.__typename === 'KafkaTopic'}
-	{#if showIcon}
-		<KafkaIcon />
-	{/if}
-	<a href="/team/{instance.team.slug}/{instance.environment.name}/kafka/{instance.name}"
-		>{instance.name}</a
-	>
-{:else if instance.__typename === 'OpenSearch'}
-	{#if showIcon}
-		<OpenSearchIcon />
-	{/if}
-	<a href="/team/{instance.team.slug}/{instance.environment.name}/opensearch/{instance.name}"
-		>{instance.name}</a
-	>
-{:else if instance.__typename === 'RedisInstance'}
-	{#if showIcon}
-		<RedisIcon />
-	{/if}
-	<a href="/team/{instance.team.slug}/{instance.environment.name}/redis/{instance.name}"
-		>{instance.name}</a
-	>
-{:else if instance.__typename === 'SqlInstance'}
-	{#if showIcon}
-		<DatabaseIcon />
-	{/if}
-	<a href="/team/{instance.team.slug}/{instance.environment.name}/postgres/{instance.name}"
-		>{instance.name}</a
-	>
-{:else if instance.__typename === 'ValkeyInstance'}
-	{#if showIcon}
-		<ValkeyIcon />
-	{/if}
-	<a href="/team/{instance.team.slug}/{instance.environment.name}/valkey/{instance.name}">
-		{instance.name}
-	</a>
-{/if}
+	</Link>
+</Heading>
