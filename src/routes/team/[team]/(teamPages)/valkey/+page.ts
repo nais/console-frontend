fe866b5b9cd4da0350e771b761c80889f39ddbaa
Andreas Nordahl <andnorda@gmail.com>
2025-02-20 08:53:02 +0100

    PersistencePage in valkey page

--- src/routes/team/[team]/(teamPages)/valkey/+page.ts
+++ src/routes/team/[team]/(teamPages)/valkey/+page.ts
@@ -1,14 +1,12 @@
-import {
-	load_Valkey,
-	ValkeyInstanceOrderField,
-	type OrderDirection$options,
-	type ValkeyInstanceOrder,
-	type ValkeyInstanceOrderField$options
-} from '$houdini';
+import { ValkeyInstanceOrderField } from '$houdini';
+import { urlToOrderDirection, urlToOrderField } from '$lib/components/OrderByMenu.svelte';
 import { error } from '@sveltejs/kit';
+import { endOfYesterday, startOfMonth, subMonths } from 'date-fns';
 import type { AfterLoadEvent, PageLoad } from './$houdini';
 
-export const load: PageLoad = async (event) => {
+const rows = 25;
+
+export const _ValkeyVariables: PageLoad = async (event) => {
 	const { url } = event;
 	const parent = await event.parent();
 
@@ -16,40 +14,20 @@ export const load: PageLoad = async (event) => {
 		error(404, 'Valkey not enabled');
 	}
 
-	const field = (url.searchParams.get('field') ||
-		ValkeyInstanceOrderField.NAME) as ValkeyInstanceOrderField$options;
-	const direction = (url.searchParams.get('direction') || 'ASC') as OrderDirection$options;
-	const rows: number = parseInt(url.searchParams.get('rows') || '10');
-
 	const after = url.searchParams.get('after') || '';
 	const before = url.searchParams.get('before') || '';
 
-	// First day of previous month
-	const from = new Date();
-	from.setMonth(from.getMonth() - 1, 1); // Set to first day of previous month
-	from.setHours(0, 0, 0, 0); // Reset time to start of the day
-
-	// Date yesterday
-	const to = new Date();
-	to.setDate(to.getDate() - 1);
-	to.setHours(23, 59, 59, 999); // Set time to end of the day
-
 	return {
-		...(await load_Valkey({
-			event,
-			variables: {
-				team: event.params.team,
-				orderBy: { field: field, direction: direction } as ValkeyInstanceOrder,
-				...(before ? { before, last: rows } : { after, first: rows }),
-				to,
-				from
-			}
-		}))
+		orderBy: {
+			field: urlToOrderField(ValkeyInstanceOrderField, ValkeyInstanceOrderField.NAME, url),
+			direction: urlToOrderDirection(url)
+		},
+		...(before ? { before, last: rows } : { after, first: rows }),
+		from: startOfMonth(subMonths(new Date(), 1)),
+		to: endOfYesterday()
 	};
 };
 
 export function _houdini_afterLoad({ data }: AfterLoadEvent) {
-	return {
-		data
-	};
+	return { data };
 }
