b9edf5c943fa9afab150629b516710dc2cc45818
Andreas Nordahl <andnorda@gmail.com>
2025-02-24 16:27:32 +0100

    beautify header

--- src/routes/PageHeader.svelte
+++ src/routes/PageHeader.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import SearchButton from '$lib/components/search/SearchButton.svelte';
 	import { docURL } from '$lib/doc';
 	import Feedback from '$lib/feedback/Feedback.svelte';
 	import { Button } from '@nais/ds-svelte-community';
+	import {
+		ActionMenu,
+		ActionMenuDivider,
+		ActionMenuItem
+	} from '@nais/ds-svelte-community/experimental.js';
+	import { ChevronDownIcon, LeaveIcon } from '@nais/ds-svelte-community/icons';
 	import Logo from '../Logo.svelte';
 
 	interface Props {
@@ -26,58 +33,83 @@
 				<Logo height="2rem" />
 				<span class="nais-console">Nais Console</span>
 			</a>
-		</div>
-		<div class="right">
-			<SearchButton />
 			<nav>
-				<ul>
-					<li>
-						<a href="/utilization">Utilization</a>
-					</li>
-					<li>
-						<a href={docURL()}>Docs</a>
-					</li>
-					{#if user?.isAdmin}
-						<li><a href="/admin">Admin</a></li>
-					{/if}
-				</ul>
+				<a href="/utilization" class:active={page.url.pathname === '/utilization'}>Utilization</a>
+				<a href={docURL()}>Docs</a>
 			</nav>
-			<div class="cap">
-				{user ? user.name : 'unauthorized'}
-				- <a href="/oauth2/logout">Logout</a>
-			</div>
 			<Button variant="primary-neutral" size="small" onclick={() => (feedbackOpen = true)}
-				>Feedback</Button
+				><span style="font-weight: 400">Feedback</span></Button
 			>
 			{#if feedbackOpen}
 				<Feedback bind:open={feedbackOpen} />
 			{/if}
 		</div>
+		<div class="right">
+			<SearchButton />
+			<ActionMenu>
+				{#snippet trigger(props)}
+					<button class="user-menu-button" {...props}>
+						{user ? user.name : 'unauthorized'}
+						<ChevronDownIcon />
+					</button>
+				{/snippet}
+				{#if !user?.isAdmin}
+					<a href="/admin" class="unstyled action-menu-link">
+						<ActionMenuItem>🕴️Admin</ActionMenuItem>
+					</a>
+					<ActionMenuDivider />
+				{/if}
+				<a href="/oauth2/logout" class="unstyled action-menu-link">
+					<ActionMenuItem>
+						<LeaveIcon />
+						Logout
+					</ActionMenuItem>
+				</a>
+			</ActionMenu>
+		</div>
 	</div>
 </div>
 
 <style>
+	.action-menu-link {
+		text-decoration: none;
+		display: grid;
+	}
+
+	.user-menu-button {
+		background: none;
+		border: none;
+		padding: 1rem;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		gap: var(--a-spacing-1);
+
+		&:hover {
+			background-color: var(--a-surface-inverted-hover);
+		}
+	}
 	.right {
 		display: flex;
-		gap: 2rem;
+		gap: var(--a-spacing-4);
 		align-items: center;
 	}
-	.cap {
-		text-transform: capitalize;
-	}
+
 	.header-left {
 		display: flex;
-		gap: 2rem;
+		gap: var(--a-spacing-8);
+		align-items: center;
 	}
 
 	.header {
 		background: var(--a-surface-inverted);
 		color: var(--a-text-on-inverted);
 		display: flex;
-		padding: 0.75rem 2rem;
+		padding-inline: 2rem;
 		min-width: 1000px;
 		position: relative;
 	}
+
 	.header-content {
 		display: flex;
 		flex-direction: row;
@@ -97,36 +129,23 @@
 		font-weight: bold;
 	}
 	nav {
-		height: 100%;
-	}
-	ul {
-		height: 100%;
 		display: flex;
-		align-items: stretch;
-		flex-direction: row;
-		margin: 0;
-		padding: 0;
+		gap: var(--a-spacing-1);
 	}
-	li {
-		display: flex;
-		list-style: none;
-		align-items: stretch;
-	}
-	li > a {
+	nav > a {
 		text-decoration: none;
 		color: var(--a-text-on-inverted);
 		background-color: var(--a-surface-inverted);
 		display: flex;
 		align-items: center;
-		padding: 0 1rem;
-	}
-	li > a:hover {
-		background-color: var(--a-surface-inverted-hover);
+		padding: 1rem;
+
+		&.active,
+		&:hover {
+			background-color: var(--a-surface-inverted-hover);
+		}
 	}
 
-	.cap a {
-		color: var(--a-text-on-inverted);
-	}
 	.nais-console {
 		background: linear-gradient(to left, var(--active-color-strong), #ffffff);
 		background-clip: text;
