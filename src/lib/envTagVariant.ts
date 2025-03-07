d21bfe4531f969dceaef560d52549ee1e4c75fc2
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-03-07 10:31:36 +0100

    fix: refactor envTagVariant function to use switch statement for better clarity

--- src/lib/envTagVariant.ts
+++ src/lib/envTagVariant.ts
@@ -1,14 +1,26 @@
 import type { TagProps } from '@nais/ds-svelte-community/components/Tag/type.js';
 
+// HACK: mantained manually - should have been provided by Nais API
 export const envTagVariant = (env: string): TagProps['variant'] => {
-	if (env.includes('prod')) {
-		if (env.includes('fss')) {
+	switch (env) {
+		case 'non-prod':
+		case 'sandbox':
+		case 'dev-gcp':
+		case 'ci':
+		case 'test':
+		case 'dev':
+			return 'neutral-filled';
+		case 'prod-gcp':
+		case 'prod':
+			return 'info-moderate';
+		case 'prod-fss':
 			return 'alt3-moderate';
-		}
-		return 'info-moderate';
+		case 'dev-fss':
+			return 'neutral-moderate';
+		default:
+			if (env.includes('prod')) {
+				return 'info-moderate';
+			}
+			return 'neutral-filled';
 	}
-	if (env.includes('fss')) {
-		return 'neutral-moderate';
-	}
-	return 'neutral-filled';
 };
