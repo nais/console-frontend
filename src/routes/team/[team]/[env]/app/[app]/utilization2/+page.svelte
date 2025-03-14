dd26897505289e87adc2e6c540058e6c0cfc5dce
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-03-14 14:25:16 +0100

    Utilization mk2 (#142)
    * add instance field to UtilizationSample and update utilization queries for enhanced data representation
    
    * refactor utilization chart component to enhance data processing and improve visualization
    
    * rename new page to  resource utilization2
    
    * old page
    
    * refactor load function to improve start date calculation based on interval

---
+++ src/routes/team/[team]/[env]/app/[app]/utilization2/+page.svelte
@@ -0,0 +1,310 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import EChart from '$lib/chart/EChart.svelte';
+	import GraphErrors from '$lib/GraphErrors.svelte';
+	import type { EChartsOption } from 'echarts';
+
+	import { UtilizationResourceType } from '$houdini';
+	import { euroValueFormatter } from '$lib/chart/cost_transformer';
+	import { cpuUtilization, memoryUtilization, yearlyOverageCost } from '$lib/utils/resources';
+	import { changeParams } from '$lib/utils/searchparams';
+	import {
+		BodyLong,
+		Heading,
+		Loader,
+		ToggleGroup,
+		ToggleGroupItem
+	} from '@nais/ds-svelte-community';
+	import { format } from 'date-fns';
+	import type { CallbackDataParams } from 'echarts/types/dist/shared';
+	import prettyBytes from 'pretty-bytes';
+	import type { PageProps } from './$houdini';
+
+	let { data }: PageProps = $props();
+	let { ResourceUtilizationForApp2 } = $derived(data);
+
+	type resourceUsage = {
+		readonly timestamp: Date;
+		readonly value: number;
+		readonly instance: string;
+	}[];
+
+	const interval = $derived(page.url.searchParams.get('interval') ?? '7d');
+
+	const visualizationColors: string[] = [
+		'#FFC166',
+		'#99DEAD',
+		'#CCE1FF',
+		'#C0B2D2',
+		'#66CBEC',
+		'#99C4DD'
+	];
+
+	function options(
+		data: resourceUsage,
+		request: number,
+		limit?: number,
+		valueFormatter: (value: number) => string = (value: number) =>
+			value == null ? '-' : value.toLocaleString('en-GB', { maximumFractionDigits: 4 })
+	): EChartsOption {
+		const safeData = data ?? [];
+
+		const uniqueTimestamps = Array.from(new Set(safeData.map((d) => d.timestamp.getTime())));
+
+		const opts = {
+			animation: false,
+			tooltip: {
+				trigger: 'axis',
+				formatter: (value: CallbackDataParams[]) => {
+					const dot = (color: string) =>
+						`<div style="height: 8px; width: 8px; border-radius: 50%; background-color: ${color};"></div>`;
+					const div = document.createElement('div');
+
+					div.innerHTML = `
+							<div>${format(
+								Array.isArray(value) && Array.isArray(value[0]?.value)
+									? (value[0].value[0] as number)
+									: 0,
+								'dd/MM/yyyy HH:mm'
+							)}</div>
+							<hr style="border: none; height: 1px; background-color: var(--a-border-subtle);" />
+							<div style="display: grid; grid-template-columns: auto auto; column-gap: 0.5rem;">
+								${value
+									.map(
+										(v) =>
+											`<div style="display: flex; align-items: center; gap: 0.25rem;">${dot(
+												v.color?.toString() ?? ''
+											)}${v.seriesName}:</div><div style="text-align: right;">${valueFormatter(
+												(Array.isArray(v.value) ? v.value[1] : null) as number
+											)}</div>`
+									)
+									.join('')}
+							</div>
+						`;
+					return div;
+				},
+				axisPointer: {
+					animation: false
+				}
+			},
+			xAxis: {
+				type: 'time',
+				boundaryGap: false,
+				axisLabel: { formatter: { month: '{MMM} {d}', day: '{dd}.{MM}' } }
+			},
+			yAxis: {
+				type: 'value',
+				// name: 'Usage of requested resources',
+				axisLabel: {
+					formatter: (value: number) => value.toLocaleString('en-GB', { maximumFractionDigits: 4 })
+				}
+			},
+			series: [
+				...Array.from(new Set(safeData.map((d) => d.instance))).map((instance, index) => ({
+					name: `Usage - ${instance}`,
+					type: 'line',
+					data: safeData
+						.filter((d) => d.instance === instance)
+						.map((d) => [d.timestamp.getTime(), d.value]),
+					showSymbol: false,
+					color: visualizationColors[index % visualizationColors.length],
+					areaStyle: {
+						opacity: 0.2
+					}
+				})),
+				{
+					data: uniqueTimestamps.map((timestamp) => [timestamp, request]),
+					type: 'line',
+					name: 'Requested',
+					showSymbol: false,
+					color: requestColor,
+					lineStyle: { color: requestColor },
+					markLine: {
+						symbol: 'none',
+						data: [
+							{
+								yAxis: request,
+								label: { formatter: 'Requested', position: 'end', color: requestColor },
+								lineStyle: { type: 'solid', color: 'transparent' }
+							}
+						]
+					}
+				},
+				limit
+					? {
+							data: uniqueTimestamps.map((timestamp) => [timestamp, limit]),
+							type: 'line',
+							name: 'Limit',
+							showSymbol: false,
+							color: limitColor,
+							lineStyle: { color: limitColor },
+							markLine: {
+								symbol: 'none',
+								data: [
+									{
+										yAxis: limit,
+										label: { formatter: 'Limit', position: 'end', color: limitColor },
+										lineStyle: { type: 'solid', color: 'transparent' }
+									}
+								]
+							}
+						}
+					: null
+			]
+		} as EChartsOption;
+
+		return opts;
+	}
+
+	const limitColor = '#DE2E2E';
+	const requestColor = '#3386E0';
+</script>
+
+<GraphErrors errors={$ResourceUtilizationForApp2.errors} />
+
+<div class="wrapper">
+	<BodyLong>
+		These graphs help you analyze your app's CPU and memory usage over time.
+		<ul>
+			<li>Blue Line (Requests): The guaranteed CPU or memory allocation for your app.</li>
+			<li>Red Line (Limits, if present): The maximum allowed usage before restrictions apply.</li>
+			<li>Shaded Area: The actual resource consumption over time.</li>
+		</ul>
+		Your app can exceed the request line, which is expected if additional resources are available:
+		<ul>
+			<li>
+				For CPU: Exceeding requests may cause throttling, leading to reduced performance but no
+				crashes.
+			</li>
+			<li>
+				For Memory: Exceeding the limit causes termination (OOMKilled) because memory cannot be
+				throttled.
+			</li>
+		</ul>
+		<div>To optimize costs while maintaining performance:</div>
+		<div>✅ If usage is consistently below requests, consider lowering requests to save money.</div>
+		<div>✅ If CPU usage is frequently throttled, increasing requests may improve performance.</div>
+		<div>
+			✅ If memory usage hits the limit, increasing requests or optimizing memory use may prevent
+			crashes.
+		</div>
+	</BodyLong>
+	<div class="section">
+		<Heading level="2" size="medium" spacing>Memory usage</Heading>
+		{#if $ResourceUtilizationForApp2.data}
+			{@const utilization =
+				$ResourceUtilizationForApp2.data.team.environment.application.utilization}
+			<BodyLong spacing>
+				At the latest data point, usage is {(
+					memoryUtilization(utilization.requested_memory, utilization.current_memory) * 100
+				).toFixed(0)}% of {prettyBytes(utilization.requested_memory, {
+					locale: 'en',
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2
+				})} requested memory. Based on this data point, the estimated annual cost of unused memory of
+				{euroValueFormatter(
+					yearlyOverageCost(
+						UtilizationResourceType.MEMORY,
+						utilization.requested_memory,
+						utilization.current_memory
+					)
+				)}.
+			</BodyLong>
+			<div style="justify-self: end;">
+				<ToggleGroup
+					value={interval}
+					onchange={(interval) => changeParams({ interval }, { noScroll: true })}
+				>
+					{#each ['1h', '6h', '1d', '7d', '30d'] as interval (interval)}
+						<ToggleGroupItem value={interval}>{interval}</ToggleGroupItem>
+					{/each}
+				</ToggleGroup>
+			</div>
+			<EChart
+				options={options(
+					utilization.memory_series.map((d) => {
+						return {
+							timestamp: d.timestamp,
+							value: d.value / 1024 / 1024 / 1024,
+							instance: d.instance
+						};
+					}),
+					utilization.requested_memory / 1024 / 1024 / 1024,
+					utilization.limit_memory ? utilization.limit_memory / 1024 / 1024 / 1024 : undefined,
+					(value) =>
+						value == null
+							? '-'
+							: prettyBytes(value * 1024 ** 3, {
+									locale: 'en',
+									minimumFractionDigits: 2,
+									maximumFractionDigits: 2
+								})
+				)}
+				style="height: 400px"
+			/>
+		{:else}
+			<div style="height: 504px; display: flex; justify-content: center; align-items: center;">
+				<Loader size="3xlarge" />
+			</div>
+		{/if}
+	</div>
+	<div class="section">
+		<Heading level="2" size="medium" spacing>CPU usage</Heading>
+		{#if $ResourceUtilizationForApp2.data}
+			{@const utilization =
+				$ResourceUtilizationForApp2.data.team.environment.application.utilization}
+			<BodyLong spacing>
+				At the latest data point, usage is {cpuUtilization(
+					utilization.requested_cpu,
+					utilization.current_cpu
+				)}% of {utilization.requested_cpu.toLocaleString('en-GB', {
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2
+				})} requested CPUs. Based on this data point, the estimated annual cost of unused CPU of {euroValueFormatter(
+					yearlyOverageCost(
+						UtilizationResourceType.CPU,
+						utilization.requested_cpu,
+						utilization.current_cpu
+					)
+				)}.
+			</BodyLong>
+			<div style="justify-self: end;">
+				<ToggleGroup
+					value={interval}
+					onchange={(interval) => changeParams({ interval }, { noScroll: true })}
+				>
+					{#each ['1h', '6h', '1d', '7d', '30d'] as interval (interval)}
+						<ToggleGroupItem value={interval}>{interval}</ToggleGroupItem>
+					{/each}
+				</ToggleGroup>
+			</div>
+			<EChart
+				options={options(
+					utilization.cpu_series,
+					utilization.requested_cpu,
+					utilization.limit_cpu ? utilization.limit_cpu : undefined,
+					(value: number) =>
+						value == null
+							? '-'
+							: `${value.toLocaleString('en-GB', { maximumFractionDigits: 4 })} CPUs`
+				)}
+				style="height: 400px"
+			/>
+		{:else}
+			<div style="height: 504px; display: flex; justify-content: center; align-items: center;">
+				<Loader size="3xlarge" />
+			</div>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		display: grid;
+		gap: var(--a-spacing-6);
+	}
+
+	.section {
+		display: grid;
+	}
+</style>
