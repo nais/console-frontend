4c803b4831b93b712bb302b1b31e947d2af5a3cf
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-04-02 15:35:49 +0200

    Allow targeting external API server

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-CLUSTER=$1
-NAMESPACE=$2
-SERVICE=$3
-
-# Validate input
-if [ -z "$CLUSTER" ] || [ -z "$NAMESPACE" ] || [ -z "$SERVICE" ]; then
-	echo "Usage: $0 <cluster> <namespace> <service>"
-	exit 1
-fi
-
-while true; do
-    echo "Starting port-forwarding..."
-	kubectl --context "$CLUSTER" --namespace "$NAMESPACE" port-forward svc/"$SERVICE" 3000:80 --request-timeout 2m
-
-    echo "Port-forwarding crashed. Restarting in 2 seconds..."
-    sleep 2
-done
+echo "No need to do port forward, just set VITE_GRAPHQL_ENDPOINT AND VITE_PROXY_ENDPOINT to the target console url in .env.\nSee .env.example"
+exit 1
