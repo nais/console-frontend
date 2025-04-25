07adab6b74afc5299939773f3b742757a22fc9ed
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-04-25 10:57:44 +0200

    fix: update severity color mappings for improved clarity

--- src/lib/utils/vulnerabilities.ts
+++ src/lib/utils/vulnerabilities.ts
@@ -8,30 +8,30 @@ export function severityToColor({
 	if (isText) {
 		switch (severity) {
 			case 'critical':
-				return 'var(--ax-danger-400, --a-red-400)';
+				return 'var(--ax-danger-600)';
 			case 'high':
-				return 'color-mix(in oklab, var(--ax-danger-400, --a-red-400), var(--ax-warning-400, --a-orange-400))';
+				return 'color-mix(in oklab, var(--ax-danger-600), var(--ax-warning-400))';
 			case 'medium':
-				return 'var(--ax-warning-400, --a-orange-400)';
+				return 'var(--ax-warning-400)';
 			case 'low':
-				return 'var(--ax-success-400, --a-green-400)';
+				return 'var(--ax-success-500)';
 			case 'unassigned':
 			default:
-				return 'var(--ax-neutral-400, --a-gray-400)';
+				return 'var(--ax-neutral-600)';
 		}
 	} else {
 		switch (severity) {
 			case 'critical':
-				return 'var(--ax-danger-600, --a-red-200)';
+				return 'var(--ax-danger-600)';
 			case 'high':
-				return 'color-mix(in oklab, var(--ax-danger-600, --a-red-200), var(--ax-warning-200, --a-orange-200))';
+				return 'color-mix(in oklab, var(--ax-danger-600), var(--ax-warning-200))';
 			case 'medium':
-				return 'var(--ax-warning-200, --a-orange-200)';
+				return 'var(--ax-warning-200)';
 			case 'low':
-				return 'var(--ax-success-400, --a-green-200)';
+				return 'var(--ax-success-400)';
 			case 'unassigned':
 			default:
-				return 'var(--ax-neutral-200, --a-gray-200)';
+				return 'var(--ax-neutral-400)';
 		}
 	}
 }
