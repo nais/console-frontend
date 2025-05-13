8aad91baef15dd8fab101e24a0bf82b5bbce0fd0
frodesundby <frode.sundby@nav.no>
2025-05-13 15:24:21 +0200

    Moved docs link and grafana link to app menu in header
    Co-authored-by: Roger Bjørnstad <roger.bjornstad@nav.no>

--- src/routes/PageHeader.svelte
+++ src/routes/PageHeader.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import SearchButton from '$lib/components/search/SearchButton.svelte';
-	import { docURL } from '$lib/doc';
+	import { docURL, tenantURL } from '$lib/doc';
 	import Feedback from '$lib/feedback/Feedback.svelte';
+	import GrafanaIcon from '$lib/icons/GrafanaIcon.svelte';
 	import { themeSwitch } from '$lib/stores/theme.svelte';
 	import { Button } from '@nais/ds-svelte-community';
 	import {
 		ActionMenu,
 		ActionMenuCheckboxItem,
 		ActionMenuDivider,
+		ActionMenuGroup,
 		ActionMenuItem,
 		InternalHeader,
 		InternalHeaderButton,
 		InternalHeaderTitle,
 		InternalHeaderUserButton
 	} from '@nais/ds-svelte-community/experimental';
-	import { ChatElipsisIcon, CogIcon, LeaveIcon } from '@nais/ds-svelte-community/icons';
+	import {
+		BooksIcon,
+		ChatElipsisIcon,
+		CogIcon,
+		ExternalLinkIcon,
+		LeaveIcon,
+		MenuGridIcon
+	} from '@nais/ds-svelte-community/icons';
 	import Logo from '../Logo.svelte';
 
 	interface Props {
@@ -46,7 +55,6 @@
 	>
 		Utilization
 	</InternalHeaderButton>
-	<InternalHeaderButton as="a" href={docURL()}>Docs</InternalHeaderButton>
 
 	<div class="aksel-stack__spacer aksel-stack__spacer"></div>
 	<div class="feedback-button-wrapper">
@@ -67,13 +75,36 @@
 	<SearchButton />
 	<ActionMenu>
 		{#snippet trigger(props)}
+			<InternalHeaderButton {...props}>
+				<MenuGridIcon title="Applications" />
+			</InternalHeaderButton>
+		{/snippet}
+		<ActionMenuGroup label="Nais resources">
+			<ActionMenuItem icon={BooksIcon}>
+				<a href={docURL()} class="action-menu-link" target="_blank" rel="noopener noreferrer">
+					Docs <ExternalLinkIcon /></a
+				>
+			</ActionMenuItem>
+			<ActionMenuItem icon={GrafanaIcon}>
+				<a
+					href={tenantURL('grafana')}
+					class="action-menu-link"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Grafana <ExternalLinkIcon />
+				</a>
+			</ActionMenuItem>
+		</ActionMenuGroup>
+	</ActionMenu>
+	<ActionMenu>
+		{#snippet trigger(props)}
 			<InternalHeaderUserButton name={user ? user.name : 'unauthorized'} {...props} />
 		{/snippet}
 
 		{#if user?.isAdmin}
-			<ActionMenuItem
-				><a href="/admin" class="action-menu-link" style="text-decoration: none;"
-					><CogIcon />Admin</a
+			<ActionMenuItem>
+				<a href="/admin" class="action-menu-link" style="text-decoration: none;"><CogIcon />Admin</a
 				></ActionMenuItem
 			>
 			<ActionMenuDivider />
@@ -112,5 +143,9 @@
 		display: flex;
 		place-items: center;
 		padding: 0 1rem;
+	}
+	.action-menu-link {
+		color: var(--ax-text-neutral);
+		text-decoration: none;
 	}
 </style>
