cbff055041c0f39a218a9caf42c20133558183e2
frodesundby <frode.sundby@nav.no>
2025-05-15 14:57:40 +0200

    WIP: enhance TenantCost query with  sorting functionality
    Co-authored-by: Roger Bjørnstad <roger.bjornstad@nav.no>

--- src/routes/cost/+page.svelte
+++ src/routes/cost/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-	import GraphErrors from '$lib/GraphErrors.svelte';
-	import { Table, Tbody, Td, Th, Thead, Tr } from '@nais/ds-svelte-community';
-	import type { PageProps } from './$houdini';
-	import { SvelteSet } from 'svelte/reactivity';
 	import type { TenantCost$result } from '$houdini';
+	import { euroValueFormatter } from '$lib/chart/cost_transformer';
+	import GraphErrors from '$lib/GraphErrors.svelte';
+	import { round } from '$lib/utils/resources';
+	import { Table, Tbody, Td, Th, Thead, Tr, type TableSortState } from '@nais/ds-svelte-community';
+	import { SvelteSet } from 'svelte/reactivity';
+	import type { PageProps } from './$houdini';
 
 	let { data }: PageProps = $props();
 	let tenantCost = $derived(data.TenantCost);
 	let services = new SvelteSet<string>();
 	type TeamData = {
-		name: string;
+		slug: string;
 		services: {
 			[service: string]: {
 				cost: number;
@@ -17,7 +19,11 @@
 		};
 		sum: number;
 	};
-	let teamData = [] as TeamData[];
+
+	let sortState: TableSortState = $state({
+		orderBy: 'SUM',
+		direction: 'descending'
+	});
 
 	// Extract services from the tenant cost data
 	$tenantCost.data?.teams.nodes.forEach((team: TenantCost$result['teams']['nodes'][0]) => {
@@ -27,49 +33,112 @@
 			});
 		});
 	});
+	type SortBy = 'SUM' | 'TEAM' | string;
 
-	$tenantCost.data?.teams.nodes.forEach((team: TenantCost$result['teams']['nodes'][0]) => {
-		teamData.push({
-			name: team.slug,
-			sum: team.cost.daily.series.reduce((acc, day) => {
-				acc += day.sum;
-				return acc;
-			}, 0),
-			services: team.cost.daily.series.reduce(
-				(acc, day) => {
-					day.services.forEach((service) => {
-						if (!acc[service.service]) {
-							acc[service.service] = { cost: 0 };
-						}
-						acc[service.service].cost += service.cost;
-					});
-					return acc;
-				},
-				{} as { [service: string]: { cost: number } }
-			)
-		});
-	});
+	const sortTable = (key: SortBy, sortState: TableSortState) => {
+		if (!sortState) {
+			sortState = {
+				orderBy: key,
+				direction: 'descending'
+			};
+		} else if (sortState.orderBy === key) {
+			if (sortState.direction === 'ascending') {
+				sortState.direction = 'descending';
+			} else {
+				sortState.direction = 'ascending';
+			}
+		} else {
+			sortState.orderBy = key;
+			if (key === 'SUM') {
+				sortState.direction = 'ascending';
+			} else {
+				sortState.direction = 'descending';
+			}
+		}
+
+		return sortState;
+	};
+	$inspect(sortState, 'sortState');
+
+	// Extract team data and calculate the sum of costs
+	let teamData = $derived(
+		($tenantCost.data?.teams.nodes
+			.map((team: TenantCost$result['teams']['nodes'][0]) => {
+				return {
+					slug: team.slug,
+					sum: team.cost.daily.series.reduce((acc, day) => {
+						acc += day.sum;
+						return acc;
+					}, 0),
+					services: team.cost.daily.series.reduce(
+						(acc, day) => {
+							day.services.forEach((service) => {
+								if (!acc[service.service]) {
+									acc[service.service] = { cost: 0 };
+								}
+								acc[service.service].cost += service.cost;
+							});
+							return acc;
+						},
+						{} as { [service: string]: { cost: number } }
+					)
+				};
+			})
+			.sort((a, b) => {
+				if (sortState.orderBy === 'SUM') {
+					return sortState.direction === 'ascending' ? a.sum - b.sum : b.sum - a.sum;
+				} else if (sortState.orderBy === 'TEAM') {
+					return sortState.direction === 'ascending'
+						? a.slug.localeCompare(b.slug)
+						: b.slug.localeCompare(a.slug);
+				} else {
+					if (a.services[sortState.orderBy.toLowerCase()] === undefined) {
+						a.services[sortState.orderBy.toLowerCase()] = { cost: 0 };
+					}
+					if (b.services[sortState.orderBy.toLowerCase()] === undefined) {
+						b.services[sortState.orderBy.toLowerCase()] = { cost: 0 };
+					}
+					return sortState.direction === 'ascending'
+						? a.services[sortState.orderBy.toLowerCase()].cost -
+								b.services[sortState.orderBy.toLowerCase()].cost
+						: b.services[sortState.orderBy.toLowerCase()].cost -
+								a.services[sortState.orderBy.toLowerCase()].cost;
+				}
+			}) as TeamData[]) || []
+	);
 </script>
 
 <div class="container">
 	<GraphErrors errors={$tenantCost.errors} />
 	{#if $tenantCost.data}
-		<Table>
+		<Table
+			size="small"
+			sort={sortState}
+			onsortchange={(key) => {
+				sortState = sortTable(key as SortBy, sortState);
+			}}
+		>
 			<Thead>
 				<Tr>
-					<Th>Team</Th>
+					<Th sortable={true} sortKey="TEAM">Team</Th>
 					{#each Array.from(services).toSorted() as service (service)}
-						<Th>{service}</Th>
+						<Th sortable={true} sortKey={service.toUpperCase()}>{service}</Th>
 					{/each}
+					<Th sortable={true} sortKey="SUM">Sum</Th>
 				</Tr>
 			</Thead>
 			<Tbody>
 				{#each teamData as team (team)}
 					<Tr>
-						<Td>{team.name}</Td>
+						<Td>{team.slug}</Td>
 						{#each Array.from(services).toSorted() as service (service)}
-							<Td>{team.services[service]?.cost ?? 0}</Td>
+							<Td
+								>{team.services[service]?.cost
+									? euroValueFormatter(round(team.services[service]?.cost))
+									: '-'}</Td
+							>
 						{/each}
+						<Td>{euroValueFormatter(round(team.sum))}</Td>
 					</Tr>
 				{/each}
 			</Tbody>
