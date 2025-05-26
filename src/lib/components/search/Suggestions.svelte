cc594ceb246428f75d391514fb36f26a7111641b
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-05-26 12:51:41 +0200

    feat: add valkey suggestion and reorder search prefix suggestions for improved clarity

--- src/lib/components/search/Suggestions.svelte
+++ src/lib/components/search/Suggestions.svelte
@@ -1,13 +1,14 @@
 <div>
 	You can filter your searches with prefixes. Try one of the following:
 	<code>app:myApp</code>
-	<code>job:myJob</code>
-	<code>team:myTeam</code>
-	<code>sql:mySqlinstance</code>
-	<code>os:myOpenSearchInstance</code>
-	<code>kafka:myKafkaTopic</code>
 	<code>bq:myBigQueryDataSet</code>
 	<code>bucket:myBucket</code>
+	<code>job:myJob</code>
+	<code>kafka:myKafkaTopic</code>
+	<code>os:myOpenSearchInstance</code>
+	<code>sql:mySqlinstance</code>
+	<code>team:myTeam</code>
+	<code>valkey:myValkeyInstance</code>
 </div>
 
 <style>
