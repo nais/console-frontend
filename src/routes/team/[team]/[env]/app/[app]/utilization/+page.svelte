569499e9fee97b5236794140b5880c5c7902a022
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-06-26 13:10:28 +0200

    Add annotations to application utilization

--- src/routes/team/[team]/[env]/app/[app]/utilization/+page.svelte
+++ src/routes/team/[team]/[env]/app/[app]/utilization/+page.svelte
@@ -4,8 +4,9 @@
 	import GraphErrors from '$lib/GraphErrors.svelte';
 	import type { EChartsOption } from 'echarts';
 
-	import { UtilizationResourceType } from '$houdini';
+	import { UtilizationResourceType, type ResourceUtilizationForApp$result } from '$houdini';
 	import { docURL } from '$lib/doc';
+	import { themeSwitch } from '$lib/stores/theme.svelte';
 	import {
 		euroValueFormatter,
 		formatKubernetesCPU,
@@ -46,10 +47,134 @@
 
 	const interval = $derived(page.url.searchParams.get('interval') ?? '7d');
 
+	const deploymentIcon =
+		'path://M19.606 3.758A7.82 7.82 0 0 0 12.97 5.97l-1.78 1.78H6.5a.75.75 0 0 0-.53.22l-3 3a.75.75 0 0 0 .383 1.265l3.578.716c.782.156 1.5.484 2.114.943L7.47 15.47a.75.75 0 1 0 1.06 1.06l1.576-1.575c.459.614.787 1.332.943 2.114l.716 3.578a.75.75 0 0 0 1.265.383l3-3a.75.75 0 0 0 .22-.53v-4.69l1.78-1.78a7.82 7.82 0 0 0 2.212-6.636.75.75 0 0 0-.636-.636m-8.431 10.128a6.74 6.74 0 0 1 1.345 2.89l.44 2.203 1.79-1.79V12.5a.75.75 0 0 1 .22-.53l2-2a6.32 6.32 0 0 0 1.843-4.783A6.32 6.32 0 0 0 14.03 7.03l-2 2a.75.75 0 0 1-.53.22H6.81l-1.789 1.79 2.204.44a6.74 6.74 0 0 1 2.89 1.345l1.355-1.355a.75.75 0 1 1 1.06 1.06z';
+	const scaleUpIcon =
+		'path://M12.53 4.47a.75.75 0 0 0-1.06 0l-3.5 3.5a.75.75 0 0 0 .53 1.28h7a.75.75 0 0 0 .53-1.28zM12 6.06l1.69 1.69h-3.38zm-3.5 8.69a.75.75 0 0 0-.53 1.28l3.5 3.5a.75.75 0 0 0 1.06 0l3.5-3.5a.75.75 0 0 0-.53-1.28zm3.5 3.19-1.69-1.69h3.38z';
+
+	type groupedLogs = {
+		timestamp: number;
+		logs: ResourceUtilizationForApp$result['team']['environment']['application']['activityLog']['nodes'];
+	};
+
+	/**
+	 * groupedActivityLog returns a grouped array of activity logs, where each group contains logs that occurred within the same 10-minute interval.
+	 * @param activityLog
+	 */
+	function groupedActivityLog(
+		activityLog:
+			| ResourceUtilizationForApp$result['team']['environment']['application']['activityLog']
+			| null
+	) {
+		if (!activityLog) {
+			return [];
+		}
+		let intervalSeconds: number;
+		switch (interval) {
+			case '1h':
+				intervalSeconds = 1 * 60; // 1 minute
+				break;
+			case '6h':
+				intervalSeconds = 4 * 60; // 4 minutes
+				break;
+			case '1d':
+				intervalSeconds = 10 * 60; // 10 minutes
+				break;
+			case '30d':
+				intervalSeconds = 12 * 60 * 60; // 12 hours
+				break;
+			default:
+				intervalSeconds = 1.5 * 60 * 60; // 1.5 hour
+				break;
+		}
+
+		return activityLog.nodes.reduceRight((acc, log) => {
+			let logTime = Math.floor(log.createdAt.getTime() / 1000);
+			if (acc.length > 0 && logTime - acc.at(-1)!.timestamp < intervalSeconds) {
+				acc.at(-1)!.logs.push(log);
+			} else {
+				acc.push({
+					timestamp: logTime,
+					logs: [log]
+				});
+			}
+			return acc;
+		}, [] as groupedLogs[]);
+	}
+
+	function formatAnnotation(log: groupedLogs) {
+		if (log.logs.length === 0) {
+			return '';
+		}
+		const div = document.createElement('div');
+		div.style.color = 'var(--ax-text-neutral)';
+		log.logs.forEach((l) => {
+			if (l.__typename == 'DeploymentActivityLogEntry') {
+				div.innerHTML += `<div>${format(l.createdAt, 'dd/MM/yyyy HH:mm')} - New release</div>`;
+			} else if (l.__typename == 'ApplicationScaledActivityLogEntry') {
+				div.innerHTML += `<div>${format(l.createdAt, 'dd/MM/yyyy HH:mm')} - Scaled ${l.appScaled.direction} to ${l.appScaled.newSize}</div>`;
+			} else {
+				div.innerHTML += `<div>${format(l.createdAt, 'dd/MM/yyyy HH:mm')} - ${l.__typename}</div>`;
+			}
+		});
+		return div.outerHTML;
+	}
+
+	function formatTooltip(valueFormatter: (value: number) => string) {
+		return (value: CallbackDataParams[] | CallbackDataParams) => {
+			const dot = (color: string) =>
+				`<div style="height: 8px; width: 8px; border-radius: 50%; background-color: ${color};"></div>`;
+			const div = document.createElement('div');
+			div.style.color = 'var(--ax-text-neutral)';
+
+			const fomattedTime = () => {
+				if (Array.isArray(value)) {
+					if (Array.isArray(value[0]?.value)) {
+						return format(value[0].value[0] as number, 'dd/MM/yyyy HH:mm');
+					} else {
+						return format(value[0].value as Date, 'dd/MM/yyyy HH:mm');
+					}
+				} else if (Object.keys(value).includes('value') && value.value) {
+					return format(value.value as Date, 'dd/MM/yyyy HH:mm');
+				} else if (value.data && typeof value.data == 'object' && 'xAxis' in value.data) {
+					return format(value.data.xAxis as Date, 'dd/MM/yyyy HH:mm');
+				}
+			};
+
+			div.innerHTML += `
+		<div>${fomattedTime()}</div>
+		<hr style="border: none; height: 1px; background-color: var(--ax-border-neutral-subtle);" />`;
+
+			if (!Array.isArray(value)) {
+				if (value.componentType == 'markLine' || value.componentType == 'markPoint') {
+					return value.name;
+				}
+				return '';
+			}
+
+			div.innerHTML += `
+				<div style="display: grid; grid-template-columns: auto auto; column-gap: 0.5rem;">
+					${value
+						?.map(
+							(v) =>
+								`<div style="display: flex; align-items: center; gap: 0.25rem;">${dot(
+									v.color?.toString() ?? ''
+								)}${v.seriesName}</div><div style="text-align: right;">${valueFormatter(
+									(Array.isArray(v.value) ? v.value[1] : null) as number
+								)}</div>`
+						)
+						.join('')}
+				</div>
+			`;
+			return div;
+		};
+	}
+
 	function options(
 		data: resourceUsage,
 		request: resourceUsage,
 		limit?: resourceUsage,
+		activityLog: groupedLogs[] | null = null,
 		valueFormatter: (value: number) => string = (value: number) =>
 			value == null ? '-' : value.toLocaleString('en-GB', { maximumFractionDigits: 4 })
 	): EChartsOption {
@@ -67,34 +192,7 @@
 			animation: false,
 			tooltip: {
 				trigger: 'axis',
-				formatter: (value: CallbackDataParams[]) => {
-					const dot = (color: string) =>
-						`<div style="height: 8px; width: 8px; border-radius: 50%; background-color: ${color};"></div>`;
-					const div = document.createElement('div');
-
-					div.innerHTML = `
-							<div>${format(
-								Array.isArray(value) && Array.isArray(value[0]?.value)
-									? (value[0].value[0] as number)
-									: 0,
-								'dd/MM/yyyy HH:mm'
-							)}</div>
-							<hr style="border: none; height: 1px; background-color: var(--ax-border-neutral-subtle);" />
-							<div style="display: grid; grid-template-columns: auto auto; column-gap: 0.5rem;">
-								${value
-									.map(
-										(v) =>
-											`<div style="display: flex; align-items: center; gap: 0.25rem;">${dot(
-												v.color?.toString() ?? ''
-											)}${v.seriesName}</div><div style="text-align: right;">${valueFormatter(
-												(Array.isArray(v.value) ? v.value[1] : null) as number
-											)}</div>`
-									)
-									.join('')}
-							</div>
-						`;
-					return div;
-				},
+				formatter: formatTooltip(valueFormatter),
 				axisPointer: {
 					animation: false
 				}
@@ -114,6 +212,7 @@
 				...Array.from(new Set(safeData.map((d) => d.instance))).map((instance, index) => ({
 					name: instance,
 					type: 'line',
+					z: 10,
 					data: safeData
 						.filter((d) => d.instance === instance)
 						.map((d) => [d.timestamp.getTime(), d.value]),
@@ -126,6 +225,7 @@
 				...Array.from(new Set(safeRequest.map((d) => d.instance))).map((instance) => ({
 					name: 'Request',
 					type: 'line',
+					z: 10,
 					data: safeRequest
 						.filter((d) => d.instance === instance)
 						.map((d) => [d.timestamp.getTime(), d.value]),
@@ -146,6 +246,7 @@
 				...Array.from(new Set(safeLimit.map((d) => d.instance))).map((instance) => ({
 					name: 'Limit',
 					type: 'line',
+					z: 10,
 					data: safeLimit
 						.filter((d) => d.instance === instance)
 						.map((d) => [d.timestamp.getTime(), d.value]),
@@ -162,6 +263,85 @@
 				}))
 			]
 		} as EChartsOption;
+
+		let startTime = Infinity;
+		let endTime = 0;
+		safeData.forEach((d) => {
+			if (d.timestamp.getTime() < startTime) {
+				startTime = Math.floor(d.timestamp.getTime() / 1000);
+			}
+			if (d.timestamp.getTime() > endTime) {
+				endTime = Math.floor(d.timestamp.getTime() / 1000);
+			}
+		});
+
+		if (activityLog && Array.isArray(opts.series)) {
+			opts.series = [
+				{
+					type: 'line',
+					z: -10,
+					markPoint: {
+						animation: false,
+						symbolSize: 16,
+						tooltip: {
+							trigger: 'item'
+						},
+						data: activityLog
+							.map((log) => {
+								let logTime = log.timestamp;
+
+								if (endTime < logTime || startTime > logTime) {
+									return null;
+								}
+								const hasDeployment = log.logs.some(
+									(l) => l.__typename == 'DeploymentActivityLogEntry'
+								);
+								return {
+									symbol: hasDeployment ? deploymentIcon : scaleUpIcon,
+									name: formatAnnotation(log),
+									xAxis: logTime * 1000,
+									y: '10'
+								};
+							})
+							.filter((d) => d !== null),
+						label: {
+							show: false
+						},
+						itemStyle: {
+							color: themeSwitch.theme == 'dark' ? '#777f8d' : '#6f7785'
+						}
+					},
+					markLine: {
+						animation: false,
+						symbol: 'none',
+						lineStyle: {
+							type: 'solid',
+							color: themeSwitch.theme == 'dark' ? '#777f8d' : '#6f7785'
+						},
+						tooltip: {
+							trigger: 'item'
+						},
+						label: {
+							show: false
+						},
+						data: activityLog
+							.map((log) => {
+								let logTime = log.timestamp;
+
+								if (endTime < logTime || startTime > logTime) {
+									return null;
+								}
+								return {
+									name: formatAnnotation(log),
+									xAxis: logTime * 1000
+								};
+							})
+							.filter((d) => d !== null)
+					}
+				},
+				...opts.series
+			];
+		}
 
 		return opts;
 	}
@@ -210,6 +390,9 @@
 <div class="wrapper">
 	{#if $ResourceUtilizationForApp.data}
 		{@const utilization = $ResourceUtilizationForApp.data.team.environment.application.utilization}
+		{@const activityLog = groupedActivityLog(
+			$ResourceUtilizationForApp.data.team.environment.application.activityLog
+		)}
 		<div class="grid">
 			<div class="card">
 				<Heading level="2" size="medium" spacing
@@ -394,6 +577,7 @@
 								value: d.value / 1024 / 1024 / 1024
 							};
 						}),
+						activityLog,
 						(value) =>
 							value == null
 								? '-'
@@ -462,6 +646,7 @@
 						utilization.cpu_series,
 						utilization.requested_cpu_series,
 						utilization.limit_cpu_series,
+						activityLog,
 						(value: number) =>
 							value == null
 								? '-'
@@ -564,5 +749,10 @@
 		display: inline-block;
 		align-items: center;
 		padding: var(--ax-space-8) var(--ax-space-32);
+	}
+
+	.chart-wrapper :global(svg > g > path) {
+		/* hack to fix the fill rule of annotation icons */
+		fill-rule: evenodd;
 	}
 </style>
