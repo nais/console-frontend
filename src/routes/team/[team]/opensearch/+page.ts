a50546b4a7fbcfb13505313aaefec2f349247a12
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-07-03 16:08:52 +0200

    feat: update import paths to '$types', implement loading functions, and add OpenSearch GraphQL query

--- src/routes/team/[team]/opensearch/+page.ts
+++ src/routes/team/[team]/opensearch/+page.ts
@@ -1,13 +1,13 @@
-import { OpenSearchOrderField } from '$houdini';
+import { load_OpenSearch, OpenSearchOrderField } from '$houdini';
 import { urlToOrderDirection, urlToOrderField } from '$lib/components/OrderByMenu.svelte';
 import { error } from '@sveltejs/kit';
 import { startOfMonth, subMonths } from 'date-fns';
-import type { PageLoad } from './$houdini';
 import { get } from 'svelte/store';
+import type { PageLoad } from './$houdini';
 
 const rows = 25;
 
-export const _OpenSearchVariables: PageLoad = async (event) => {
+export const load: PageLoad = async (event) => {
 	const { url } = event;
 	const parent = await event.parent();
 
@@ -21,12 +21,18 @@ export const _OpenSearchVariables: PageLoad = async (event) => {
 	const before = url.searchParams.get('before') || '';
 
 	return {
-		orderBy: {
-			field: urlToOrderField(OpenSearchOrderField, OpenSearchOrderField.NAME, url),
-			direction: urlToOrderDirection(url)
-		},
-		...(before ? { before, last: rows } : { after, first: rows }),
-		from: startOfMonth(subMonths(new Date(), 12)),
-		to: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000)
+		...(await load_OpenSearch({
+			event,
+			variables: {
+				team: event.params.team,
+				orderBy: {
+					field: urlToOrderField(OpenSearchOrderField, OpenSearchOrderField.NAME, event.url),
+					direction: urlToOrderDirection(event.url)
+				},
+				...(before ? { before, last: rows } : { after, first: rows }),
+				from: startOfMonth(subMonths(new Date(), 12)),
+				to: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000)
+			}
+		}))
 	};
 };
