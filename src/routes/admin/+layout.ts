2b4e1f877d9be3129efcb2085f9c7ba3a776eec8
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-07-04 14:43:13 +0200

    refactor: change user info data fetching method to use store getter

--- src/routes/admin/+layout.ts
+++ src/routes/admin/+layout.ts
@@ -1,9 +1,10 @@
 import { error } from '@sveltejs/kit';
+import { get } from 'svelte/store';
 
 export async function load({ parent }) {
 	const pd = await parent();
 
-	const userInfoData = await pd.UserInfo.fetch();
+	const userInfoData = get(pd.UserInfo);
 
 	if (!(userInfoData.data?.me.__typename === 'User' && userInfoData.data?.me.isAdmin)) {
 		error(403, 'You are not allowed to view this page');
