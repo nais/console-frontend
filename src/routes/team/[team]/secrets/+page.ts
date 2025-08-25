a9ad6ed0806b28815e16ad2f6419e7feddb93b2e
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-08-25 12:38:54 +0200

    Use URL params for pagination

--- src/routes/team/[team]/secrets/+page.ts
+++ src/routes/team/[team]/secrets/+page.ts
@@ -1,6 +1,7 @@
 import { load_Secrets, OrderDirection, SecretOrderField, type SecretFilter } from '$houdini';
 import { urlToOrderDirection, urlToOrderField } from '$lib/components/OrderByMenu.svelte';
 
+const rows = 25;
 export async function load(event) {
 	const filter = event.url.searchParams.get('filter') || '';
 
@@ -11,6 +12,7 @@ export async function load(event) {
 	}
 
 	const after = event.url.searchParams.get('after') || '';
+	const before = event.url.searchParams.get('before') || '';
 
 	return {
 		...(await load_Secrets({
@@ -21,7 +23,7 @@ export async function load(event) {
 					field: urlToOrderField(SecretOrderField, SecretOrderField.NAME, event.url),
 					direction: urlToOrderDirection(event.url, OrderDirection.ASC)
 				},
-				after,
+				...(before ? { before, last: rows } : { after, first: rows }),
 				filter: filterVar
 			}
 		}))
