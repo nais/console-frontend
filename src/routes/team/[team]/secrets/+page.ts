15adb7c2ff5e06e176769439f2bb8c623e96c1d9
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-08-25 12:27:53 +0200

    Make pagination default in secrets

--- src/routes/team/[team]/secrets/+page.ts
+++ src/routes/team/[team]/secrets/+page.ts
@@ -1,7 +1,6 @@
 import { load_Secrets, OrderDirection, SecretOrderField, type SecretFilter } from '$houdini';
 import { urlToOrderDirection, urlToOrderField } from '$lib/components/OrderByMenu.svelte';
 
-const rows = 25;
 export async function load(event) {
 	const filter = event.url.searchParams.get('filter') || '';
 
@@ -12,7 +11,6 @@ export async function load(event) {
 	}
 
 	const after = event.url.searchParams.get('after') || '';
-	const before = event.url.searchParams.get('before') || '';
 
 	return {
 		...(await load_Secrets({
@@ -23,7 +21,7 @@ export async function load(event) {
 					field: urlToOrderField(SecretOrderField, SecretOrderField.NAME, event.url),
 					direction: urlToOrderDirection(event.url, OrderDirection.ASC)
 				},
-				...(before ? { before, last: rows } : { after, first: rows }),
+				after,
 				filter: filterVar
 			}
 		}))
