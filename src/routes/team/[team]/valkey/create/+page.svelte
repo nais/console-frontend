8bb2c0fa7ca57124ed2afb506ed2d450e30ed0b9
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-08-28 13:31:12 +0200

    Add some stuff

--- src/routes/team/[team]/valkey/create/+page.svelte
+++ src/routes/team/[team]/valkey/create/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import { page } from '$app/state';
-	import { ValkeyMaxMemoryPolicy, ValkeySize, ValkeyTier } from '$houdini';
-	import { BodyLong, Button, ErrorMessage, Select, TextField } from '@nais/ds-svelte-community';
+	import {
+		ValkeyMaxMemoryPolicy,
+		ValkeySize,
+		ValkeyTier,
+		type ValkeySize$options,
+		type ValkeyTier$options
+	} from '$houdini';
+	import {
+		BodyLong,
+		BodyShort,
+		Button,
+		ErrorMessage,
+		Select,
+		TextField
+	} from '@nais/ds-svelte-community';
 	import type { PageProps } from './$houdini';
 
 	let { data }: PageProps = $props();
@@ -12,6 +25,32 @@
 	const environments = $derived($CreateValkeyEnvironments.data?.team.environments ?? []);
 
 	const form = $derived(page.form);
+
+	const planCosts: Record<ValkeyTier$options, Record<ValkeySize$options, number>> = {
+		SINGLE_NODE: {
+			RAM_1GB: Infinity,
+			RAM_4GB: 64,
+			RAM_8GB: 128,
+			RAM_14GB: 214,
+			RAM_28GB: 394,
+			RAM_56GB: 651,
+			RAM_112GB: 1277,
+			RAM_200GB: 2554
+		},
+		HIGH_AVAILABILITY: {
+			RAM_1GB: 60,
+			RAM_4GB: 171,
+			RAM_8GB: 342,
+			RAM_14GB: 462,
+			RAM_28GB: 737,
+			RAM_56GB: 1337,
+			RAM_112GB: 2640,
+			RAM_200GB: 5057
+		}
+	};
+
+	let tier = $derived((form?.tier as ValkeyTier$options) ?? ValkeyTier.HIGH_AVAILABILITY);
+	let size = $derived((form?.size as ValkeySize$options) ?? ValkeySize.RAM_1GB);
 </script>
 
 <BodyLong>This is an explanation on how to create a Valkey instance.</BodyLong>
@@ -35,19 +74,13 @@
 		{/each}
 	</Select>
 
-	<Select
-		size="small"
-		label="Tier"
-		name="tier"
-		required
-		value={form?.tier ?? ValkeyTier.HIGH_AVAILABILITY}
-	>
+	<Select size="small" label="Tier" name="tier" required bind:value={tier}>
 		{#each Object.values(ValkeyTier) as opt (opt)}
 			<option value={opt}>{opt}</option>
 		{/each}
 	</Select>
 
-	<Select size="small" label="Size" name="size" required value={form?.size ?? ValkeySize.RAM_1GB}>
+	<Select size="small" label="Size" name="size" required bind:value={size}>
 		{#each Object.values(ValkeySize) as opt (opt)}
 			<option value={opt}>{opt}</option>
 		{/each}
@@ -64,6 +97,15 @@
 			<option value={opt}>{opt}</option>
 		{/each}
 	</Select>
+
+	<BodyShort>
+		Estimated cost: <strong
+			>{planCosts[tier][size].toLocaleString('no-NO', {
+				style: 'currency',
+				currency: 'EUR'
+			})}</strong
+		> per month
+	</BodyShort>
 
 	<Button type="submit">Create Valkey instance</Button>
 </form>
