e95d08593e34aba1c79c2635b4acbaf61c8eb1b7
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-08-28 13:31:12 +0200

    Add some stuff

---
+++ src/routes/team/[team]/[env]/valkey/[valkey]/Manifest.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import ExternalLink from '$lib/components/ExternalLink.svelte';
+	import { docURL } from '$lib/doc';
+	import { CopyButton, Heading } from '@nais/ds-svelte-community';
+
+	interface Props {
+		instanceName: string;
+		teamSlug: string;
+	}
+
+	let { instanceName, teamSlug }: Props = $props();
+
+	const niceName = $derived(instanceName.replace(`valkey-${teamSlug}-`, ''));
+
+	const workloadManifest = $derived(`spec:
+  valkey:
+    - instance: ${niceName}`);
+</script>
+
+<div class="card">
+	<Heading level="2" size="medium" spacing>Use this Valkey</Heading>
+
+	<Heading level="3" size="xsmall">Documentation</Heading>
+	<div class="value">
+		<ExternalLink href={docURL('/persistence/valkey/how-to/create-application/')}
+			>How-to guide</ExternalLink
+		>
+	</div>
+
+	<Heading level="3" size="xsmall">
+		Manifest
+
+		<CopyButton
+			activeText="Manifest copied"
+			title="Copy manifest to clipboard"
+			variant="neutral"
+			copyText={workloadManifest}
+			size="xsmall"
+		/>
+	</Heading>
+	<pre class="manifest">{workloadManifest}</pre>
+</div>
+
+<style>
+	.value {
+		margin: 0.5rem 1rem;
+	}
+
+	.manifest {
+		display: block;
+		font-size: var(--ax-font-size-small);
+		word-break: break-word;
+		white-space: pre-wrap;
+		margin: 0.5rem 1rem;
+	}
+</style>
