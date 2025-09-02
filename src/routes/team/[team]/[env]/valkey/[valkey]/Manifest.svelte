f93ba25f1b54a836d185c7912e2e943cf402f270
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-09-02 14:40:40 +0200

    Add toml modal to valkey

--- src/routes/team/[team]/[env]/valkey/[valkey]/Manifest.svelte
+++ src/routes/team/[team]/[env]/valkey/[valkey]/Manifest.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
+	import { fragment, graphql, type ValkeyManifestFragment } from '$houdini';
 	import ExternalLink from '$lib/components/ExternalLink.svelte';
 	import { docURL } from '$lib/doc';
-	import { CopyButton, Heading } from '@nais/ds-svelte-community';
+	import { Alert, BodyLong, Button, CopyButton, Heading, Modal } from '@nais/ds-svelte-community';
 
 	interface Props {
-		instanceName: string;
+		valkey: ValkeyManifestFragment;
 		teamSlug: string;
 	}
 
-	let { instanceName, teamSlug }: Props = $props();
+	let { valkey, teamSlug }: Props = $props();
 
-	const niceName = $derived(instanceName.replace(`valkey-${teamSlug}-`, ''));
+	const data = fragment(
+		valkey,
+		graphql(`
+			fragment ValkeyManifestFragment on Valkey {
+				name
+				size
+				tier
+				maxMemoryPolicy
+			}
+		`)
+	);
+
+	const niceName = $derived($data.name.replace(`valkey-${teamSlug}-`, ''));
 
 	const workloadManifest = $derived(`spec:
   valkey:
     - instance: ${niceName}`);
+
+	const tomlManifest = $derived(`[valkey.${niceName}]
+instance = "${niceName}"
+size = "${$data.size}"
+tier = "${$data.tier}"
+${$data.maxMemoryPolicy ? `max_memory_policy = "${$data.maxMemoryPolicy}"` : ``}
+	`);
+
+	let modalOpen = $state(false);
 </script>
 
 <div class="card">
@@ -39,7 +61,31 @@
 		/>
 	</Heading>
 	<pre class="manifest">{workloadManifest}</pre>
+	<Button variant="tertiary-neutral" size="small" onclick={() => (modalOpen = true)}>
+		Show in Nais TOML format
+	</Button>
 </div>
+
+<Modal bind:open={modalOpen} header="Nais TOML">
+	<BodyLong>
+		The manifest below can be added to your <code>nais.toml</code> file. You can then use
+		<code>nais alpha apply</code> to manage the lifecycle of your Valkey.
+	</BodyLong>
+	{#if $data.name != niceName}
+		<Alert variant="warning" size="small" inline style="margin: 1rem 0">
+			You will need to do a manual change to the resource using <code>kubectl</code> to allow Console
+			to manage this resource.
+		</Alert>
+	{/if}
+	<CopyButton
+		activeText="Manifest copied"
+		text="Copy manifest to clipboard"
+		variant="neutral"
+		copyText={tomlManifest}
+		size="xsmall"
+	/>
+	<pre class="manifest">{tomlManifest}</pre>
+</Modal>
 
 <style>
 	.value {
