6e7a316e77965f36df47b484cbff2c9e26ef4eed
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2025-09-05 15:29:45 +0200

    feat(opensearch): add toml modal

--- src/routes/team/[team]/[env]/opensearch/[opensearch]/Manifest.svelte
+++ src/routes/team/[team]/[env]/opensearch/[opensearch]/Manifest.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
+	import { fragment, graphql, type OpenSearchManifestFragment } from '$houdini';
 	import ExternalLink from '$lib/components/ExternalLink.svelte';
 	import { docURL } from '$lib/doc';
-	import { CopyButton, Heading } from '@nais/ds-svelte-community';
+	import { Alert, BodyLong, Button, CopyButton, Heading, Modal } from '@nais/ds-svelte-community';
 
 	interface Props {
-		instanceName: string;
+		openSearch: OpenSearchManifestFragment;
 		teamSlug: string;
 	}
 
-	let { instanceName, teamSlug }: Props = $props();
+	let { openSearch, teamSlug }: Props = $props();
 
-	const niceName = $derived(instanceName.replace(`opensearch-${teamSlug}-`, ''));
+	const data = fragment(
+		openSearch,
+		graphql(`
+			fragment OpenSearchManifestFragment on OpenSearch {
+				name
+				size
+				tier
+				majorVersion
+			}
+		`)
+	);
+
+	const niceName = $derived($data.name.replace(`opensearch-${teamSlug}-`, ''));
 
 	const workloadManifest = $derived(`spec:
   openSearch:
     - instance: ${niceName}`);
+
+	const tomlManifest = $derived(`[openSearch.${niceName}]
+instance = "${niceName}"
+size = "${$data.size}"
+tier = "${$data.tier}"
+${$data.majorVersion ? `version = "${$data.majorVersion}"` : ``}
+	`);
+
+	let modalOpen = $state(false);
 </script>
 
 <div class="card">
@@ -22,7 +44,8 @@
 
 	<Heading level="3" size="xsmall">Documentation</Heading>
 	<div class="value">
-		<ExternalLink href={docURL('/persistence/opensearch/how-to/create/')}>How-to guide</ExternalLink
+		<ExternalLink href={docURL('/persistence/opensearch/how-to/create-application/')}
+			>How-to guide</ExternalLink
 		>
 	</div>
 
@@ -38,7 +61,31 @@
 		/>
 	</Heading>
 	<pre class="manifest">{workloadManifest}</pre>
+	<Button variant="tertiary-neutral" size="small" onclick={() => (modalOpen = true)}>
+		Show in Nais TOML format
+	</Button>
 </div>
+
+<Modal bind:open={modalOpen} header="Nais TOML">
+	<BodyLong>
+		The manifest below can be added to your <code>nais.toml</code> file. You can then use
+		<code>nais alpha apply</code> to manage the lifecycle of your OpenSearch.
+	</BodyLong>
+	{#if $data.name != niceName}
+		<Alert variant="warning" size="small" inline style="margin: 1rem 0">
+			You will need to do a manual change to the resource using <code>kubectl</code> to allow Console
+			to manage this resource.
+		</Alert>
+	{/if}
+	<CopyButton
+		activeText="Manifest copied"
+		text="Copy manifest to clipboard"
+		variant="neutral"
+		copyText={tomlManifest}
+		size="xsmall"
+	/>
+	<pre class="manifest">{tomlManifest}</pre>
+</Modal>
 
 <style>
 	.value {
