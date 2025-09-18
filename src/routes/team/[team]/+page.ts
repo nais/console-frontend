d9cda25c0b270197f70790f88bd96b71536cab25
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-09-18 15:13:09 +0200

    feat: exclude deployments from activityLog on team overview

--- src/routes/team/[team]/+page.ts
+++ src/routes/team/[team]/+page.ts
@@ -2,7 +2,9 @@ import { ActivityLogActivityType, load_TeamOverview } from '$houdini';
 import { subWeeks } from 'date-fns';
 
 const activityLogFilter = Object.values(ActivityLogActivityType).filter(
-	(type) => type !== ActivityLogActivityType.APPLICATION_SCALED
+	(type) =>
+		type !== ActivityLogActivityType.APPLICATION_SCALED &&
+		type !== ActivityLogActivityType.DEPLOYMENT
 );
 
 export async function load(event) {
