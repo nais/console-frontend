01b0c468a9993d58a097bb327b9555d22237ab84
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-10-01 09:24:33 +0200

    feat: add Issues page for applications and jobs with pagination and error handling

---
+++ src/routes/team/[team]/[env]/app/[app]/issues/+page.ts
@@ -0,0 +1,33 @@
+import {
+	IssueOrderField,
+	load_ApplicationIssues,
+	OrderDirection,
+	type IssueFilter
+} from '$houdini';
+import { urlToOrderDirection, urlToOrderField } from '$lib/components/OrderByMenu.svelte';
+
+const rows = 25;
+
+export async function load(event) {
+	const severity: string | undefined = event.url.searchParams.get('severity') || undefined;
+
+	const after = event.url.searchParams.get('after') || '';
+	const before = event.url.searchParams.get('before') || '';
+
+	return {
+		...(await load_ApplicationIssues({
+			event,
+			variables: {
+				team: event.params.team,
+				env: event.params.env,
+				app: event.params.app,
+				filter: { severity } as IssueFilter,
+				orderBy: {
+					field: urlToOrderField(IssueOrderField, IssueOrderField.SEVERITY, event.url),
+					direction: urlToOrderDirection(event.url, OrderDirection.ASC)
+				},
+				...(before ? { before, last: rows } : { after, first: rows })
+			}
+		}))
+	};
+}
