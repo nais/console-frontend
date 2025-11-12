4940466b9c972b153bf4c00f814b7dd1f4861cc0
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-11-12 08:35:02 +0100

    feat: add storybook stories for Confirm, CriticalIndicator, ExternalLink, Icon, and WorkloadLink components

---
+++ src/lib/components/CriticalIndicator.stories.svelte
@@ -0,0 +1,37 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import CriticalIndicator from './CriticalIndicator.svelte';
+
+	const { Story } = defineMeta({
+		component: CriticalIndicator,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Default" asChild>
+	<CriticalIndicator />
+</Story>
+
+<Story name="In context" asChild>
+	<div style="display: flex; align-items: center; gap: 0.5rem;">
+		<CriticalIndicator />
+		<span>Critical vulnerability detected</span>
+	</div>
+</Story>
+
+<Story name="Multiple" asChild>
+	<div style="display: flex; gap: 2rem;">
+		<div style="display: flex; align-items: center; gap: 0.5rem;">
+			<CriticalIndicator />
+			<span>Issue 1</span>
+		</div>
+		<div style="display: flex; align-items: center; gap: 0.5rem;">
+			<CriticalIndicator />
+			<span>Issue 2</span>
+		</div>
+		<div style="display: flex; align-items: center; gap: 0.5rem;">
+			<CriticalIndicator />
+			<span>Issue 3</span>
+		</div>
+	</div>
+</Story>
---
+++ src/lib/components/ExternalLink.stories.svelte
@@ -0,0 +1,25 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import ExternalLink from './ExternalLink.svelte';
+
+	const { Story } = defineMeta({
+		component: ExternalLink,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Documentation link" asChild>
+	<ExternalLink href="https://doc.nais.io">Read the documentation</ExternalLink>
+</Story>
+
+<Story name="GitHub link" asChild>
+	<ExternalLink href="https://github.com/nais/console-frontend">View source code</ExternalLink>
+</Story>
+
+<Story name="In paragraph" asChild>
+	<p>
+		Learn more about NAIS in the <ExternalLink href="https://doc.nais.io"
+			>official documentation</ExternalLink
+		>.
+	</p>
+</Story>
