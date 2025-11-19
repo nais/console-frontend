611cf5f698e9fc9b8efa86583dc804b96d27d4ad
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-11-19 09:34:02 +0100

    chore: update dependencies and scripts in package.json
    - Bump versions for several devDependencies including Storybook and Svelte.
    - Add eslint-plugin-storybook for improved linting support.
    - Minor updates to script commands for consistency.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
@@ -44,5 +47,6 @@ export default ts.config(
 			'svelte/no-navigation-without-resolve': 'off'
 			/*'svelte/no-unused-class-name': 'error'*/
 		}
-	}
+	},
+	storybook.configs['flat/recommended']
 );
