9d57d4d480847019d583768a523fe1156bcbae02
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-02-26 13:35:07 +0100

    feat: refactor issueTypeLabel function for improved readability

--- src/lib/utils/issueTypeLabel.ts
+++ src/lib/utils/issueTypeLabel.ts
@@ -1,14 +1,17 @@
-export const issueTypeLabel = (type: string) =>
-	type === 'EXTERNAL_INGRESS_CRITICAL_VULNERABILITY'
-		? 'Critical Vulnerability'
-		: type
-				.split('_')
-				.map((part) => {
-					if (part === 'SQLINSTANCE') {
-						return 'SQL Instance';
-					}
+export const issueTypeLabel = (type: string) => {
+	if (type === 'EXTERNAL_INGRESS_CRITICAL_VULNERABILITY') {
+		return 'Critical Vulnerability';
+	}
 
-					const lower = part.toLowerCase();
-					return lower.charAt(0).toUpperCase() + lower.slice(1);
-				})
-				.join(' ');
+	return type
+		.split('_')
+		.map((part) => {
+			if (part === 'SQLINSTANCE') {
+				return 'SQL Instance';
+			}
+
+			const lower = part.toLowerCase();
+			return lower.charAt(0).toUpperCase() + lower.slice(1);
+		})
+		.join(' ');
+};
