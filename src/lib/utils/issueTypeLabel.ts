fc12b0fc6047ae6528f3ec883ba336268e4afb68
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-02-26 13:24:30 +0100

    feat: rename 'External Ingress Critical Vulnerability' to 'Critical Vulnerability' in UI and tests

--- src/lib/utils/issueTypeLabel.ts
+++ src/lib/utils/issueTypeLabel.ts
@@ -1,12 +1,14 @@
 export const issueTypeLabel = (type: string) =>
-	type
-		.split('_')
-		.map((part) => {
-			if (part === 'SQLINSTANCE') {
-				return 'SQL Instance';
-			}
+	type === 'EXTERNAL_INGRESS_CRITICAL_VULNERABILITY'
+		? 'Critical Vulnerability'
+		: type
+				.split('_')
+				.map((part) => {
+					if (part === 'SQLINSTANCE') {
+						return 'SQL Instance';
+					}
 
-			const lower = part.toLowerCase();
-			return lower.charAt(0).toUpperCase() + lower.slice(1);
-		})
-		.join(' ');
+					const lower = part.toLowerCase();
+					return lower.charAt(0).toUpperCase() + lower.slice(1);
+				})
+				.join(' ');
