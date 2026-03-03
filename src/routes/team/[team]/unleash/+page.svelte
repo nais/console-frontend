552f7a097217a274e35b1b0ad7e2687ffb629e4e
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-03-03 16:12:18 +0100

    fix(ui): correct formatting of alert message for unleash instance creation

--- src/routes/team/[team]/unleash/+page.svelte
+++ src/routes/team/[team]/unleash/+page.svelte
@@ -325,8 +325,7 @@
 	</BodyLong>
 	{#if !unleash.ready}
 		<Alert variant="info" size="small" style="margin-bottom: 1rem;">
-			<Loader size="small" /> Your Unleash instance is being created. This usually takes about a
-			minute...
+			<Loader size="small" /> Your Unleash instance is being created. This usually takes about a minute...
 		</Alert>
 	{/if}
 	{#if !unleash.releaseChannelName}
