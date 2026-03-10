9f26554397fed875488caa0baf3cf2f786c32dac
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-03-10 10:13:30 +0100

    fix: update resource request and limit handling in job runs component

--- src/routes/team/[team]/[env]/job/[job]/Runs.svelte
+++ src/routes/team/[team]/[env]/job/[job]/Runs.svelte
@@ -67,116 +67,122 @@
 	);
 </script>
 
-<div>
-	<Heading as="h4" size="small">Resources:</Heading>
-	<ul class="resource-list">
-		<li>
-			Requests:
-			<ul>
-				<li>
-					<div class="resource-list-item">
-						<div>CPU:</div>
-						<div class="data">
-							<code>
-								{#if $data.resources.requests.cpu}
-									{$data.resources.requests.cpu?.toFixed(2)} CPUs
-								{:else}
-									0.2 CPUs (default)
-								{/if}
-							</code>
+{#if $data}
+	<div>
+		<Heading as="h4" size="small">Resources:</Heading>
+		<ul class="resource-list">
+			<li>
+				Requests:
+				<ul>
+					<li>
+						<div class="resource-list-item">
+							<div>CPU:</div>
+							<div class="data">
+								<code>
+									{#if $data.resources?.requests?.cpu}
+										{$data.resources?.requests?.cpu?.toFixed(2)} CPUs
+									{:else}
+										0.2 CPUs (default)
+									{/if}
+								</code>
+							</div>
 						</div>
-					</div>
-				</li>
+					</li>
 
-				<li>
-					<div class="resource-list-item">
-						<div>Memory:</div>
-						<div class="data">
-							<code>
-								{#if $data.resources.requests.memory}
-									{prettyBytes($data.resources.requests.memory, {
-										locale: 'en',
-										minimumFractionDigits: 2,
-										maximumFractionDigits: 2,
-										binary: true
-									})}
-								{:else}
-									{prettyBytes(268435456, {
-										locale: 'en',
-										minimumFractionDigits: 2,
-										maximumFractionDigits: 2,
-										binary: true
-									})} (default)
-								{/if}
-							</code>
+					<li>
+						<div class="resource-list-item">
+							<div>Memory:</div>
+							<div class="data">
+								<code>
+									{#if $data.resources?.requests?.memory}
+										{prettyBytes($data.resources?.requests?.memory, {
+											locale: 'en',
+											minimumFractionDigits: 2,
+											maximumFractionDigits: 2,
+											binary: true
+										})}
+									{:else}
+										{prettyBytes(268435456, {
+											locale: 'en',
+											minimumFractionDigits: 2,
+											maximumFractionDigits: 2,
+											binary: true
+										})} (default)
+									{/if}
+								</code>
+							</div>
 						</div>
-					</div>
-				</li>
-			</ul>
-		</li>
-		<li>
-			Limits:
-			<ul>
-				<li>
-					<div class="resource-list-item">
-						<div>CPU:</div>
-						<div class="data">
-							<code>
-								{#if $data.resources.limits.cpu}
-									{$data.resources.limits.cpu?.toFixed(2)} CPUs
-								{:else}
-									0.5 CPUs (default)
-								{/if}
-							</code>
+					</li>
+				</ul>
+			</li>
+			<li>
+				Limits:
+				<ul>
+					<li>
+						<div class="resource-list-item">
+							<div>CPU:</div>
+							<div class="data">
+								<code>
+									{#if $data.resources?.limits?.cpu}
+										{$data.resources?.limits?.cpu?.toFixed(2)} CPUs
+									{:else}
+										0.5 CPUs (default)
+									{/if}
+								</code>
+							</div>
 						</div>
-					</div>
-				</li>
+					</li>
 
-				<li>
-					<div class="resource-list-item">
-						<div>Memory:</div>
-						<div class="data">
-							<code>
-								{#if $data.resources.limits.memory}
-									{prettyBytes($data.resources.limits.memory, {
-										locale: 'en',
-										minimumFractionDigits: 2,
-										maximumFractionDigits: 2,
-										binary: true
-									})}
-								{:else}
-									{prettyBytes(536870912, {
-										locale: 'en',
-										minimumFractionDigits: 2,
-										maximumFractionDigits: 2,
-										binary: true
-									})} (default)
-								{/if}
-							</code>
+					<li>
+						<div class="resource-list-item">
+							<div>Memory:</div>
+							<div class="data">
+								<code>
+									{#if $data.resources?.limits?.memory}
+										{prettyBytes($data.resources?.limits?.memory, {
+											locale: 'en',
+											minimumFractionDigits: 2,
+											maximumFractionDigits: 2,
+											binary: true
+										})}
+									{:else}
+										{prettyBytes(536870912, {
+											locale: 'en',
+											minimumFractionDigits: 2,
+											maximumFractionDigits: 2,
+											binary: true
+										})} (default)
+									{/if}
+								</code>
+							</div>
 						</div>
-					</div>
-				</li>
-			</ul>
-		</li>
-	</ul>
-</div>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
 
-{#if $data.runs.edges.length === 0}
-	<BodyShort>No runs found</BodyShort>
-{:else}
-	<List title="{$data.runs.edges.length} job run{$data.runs.edges.length > 1 ? 's' : ''}">
-		{#each $data.runs.edges as run (run.node.id)}
-			{#if run.node.instances.pageInfo.totalCount > 0}
-				<JobRunListItem
-					run={run.node}
-					urlBase="/team/{$data.team.slug}/{$data.teamEnvironment.environment
-						.name}/job/{$data.name}/logs?instance="
-				/>
-			{:else}
-				<JobRunListItem run={run.node} />
-			{/if}
-		{/each}
-	</List>
+	{#if ($data.runs?.edges ?? []).length === 0}
+		<BodyShort>No runs found</BodyShort>
+	{:else}
+		<List
+			title="{($data.runs?.edges ?? []).length} job run{($data.runs?.edges ?? []).length > 1
+				? 's'
+				: ''}"
+		>
+			{#each $data.runs?.edges ?? [] as run (run.node.id)}
+				{#if run.node.instances.pageInfo.totalCount > 0 && $data.team?.slug && $data.teamEnvironment?.environment?.name && $data.name}
+					<JobRunListItem
+						run={run.node}
+						urlBase="/team/{$data.team?.slug}/{$data.teamEnvironment?.environment
+							.name}/job/{$data.name}/logs?instance="
+					/>
+				{:else}
+					<JobRunListItem run={run.node} />
+				{/if}
+			{/each}
+		</List>
+	{/if}
 {/if}
 
 <style>
