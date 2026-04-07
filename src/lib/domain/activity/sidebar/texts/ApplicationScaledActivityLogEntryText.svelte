ba75bacda84e00f7c07ab763ae4cb710f0694fe5
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-04-07 09:12:08 +0200

    fix: enhance workload display with environment link and conditional rendering

--- src/lib/domain/activity/sidebar/texts/ApplicationScaledActivityLogEntryText.svelte
+++ src/lib/domain/activity/sidebar/texts/ApplicationScaledActivityLogEntryText.svelte
@@ -3,6 +3,7 @@
 	import { envTagVariant } from '$lib/envTagVariant';
 	import Time from '$lib/ui/Time.svelte';
 	import { BodyShort, Tag } from '@nais/ds-svelte-community';
+	import { activityLogResourceLink } from '../../utils';
 
 	let {
 		data
@@ -15,7 +16,20 @@
 </script>
 
 <div>
-	Workload scaled {data.appScaled.direction} to {data.appScaled.newSize} replicas
+	Workload
+	{#if data.environmentName}
+		<a
+			href={activityLogResourceLink(
+				data.environmentName,
+				data.resourceType,
+				data.resourceName,
+				data.teamSlug
+			)}>{data.resourceName}</a
+		>
+	{:else}
+		{data.resourceName}
+	{/if}
+	scaled {data.appScaled.direction} to {data.appScaled.newSize} replicas
 	{#if data.environmentName}
 		in <Tag size="small" variant={envTagVariant(data.environmentName)}>{data.environmentName}</Tag>
 	{/if}.
