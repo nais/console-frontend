37bb0e3f7b1fa05ed3c1751f9196f324162a2bea
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-04-07 09:16:44 +0200

    feat: add resource links to deployment and job triggered activity logs

--- src/lib/domain/activity/sidebar/texts/DeploymentActivityLogEntryText.svelte
+++ src/lib/domain/activity/sidebar/texts/DeploymentActivityLogEntryText.svelte
@@ -3,6 +3,7 @@
 	import { envTagVariant } from '$lib/envTagVariant';
 	import Time from '$lib/ui/Time.svelte';
 	import { BodyShort, Tag } from '@nais/ds-svelte-community';
+	import { activityLogResourceLink } from '../../utils';
 
 	let {
 		data
@@ -29,7 +30,23 @@
 			>{data.environmentName}</Tag
 		>
 	{:else}
-		Deployment
+		Deployed
+		{#if data.environmentName}
+			<a
+				href={activityLogResourceLink(
+					data.environmentName,
+					data.resourceType,
+					data.resourceName,
+					data.teamSlug
+				)}>{data.resourceName}</a
+			>
+		{:else}
+			{data.resourceName}
+		{/if}
+		{#if data.environmentName}
+			to <Tag size="small" variant={envTagVariant(data.environmentName)}>{data.environmentName}</Tag
+			>
+		{/if}
 	{/if}
 	<BodyShort textColor="subtle" size="small">
 		By {data.actor}
--- src/lib/domain/activity/sidebar/texts/JobTriggeredActivityLogEntryText.svelte
+++ src/lib/domain/activity/sidebar/texts/JobTriggeredActivityLogEntryText.svelte
@@ -3,6 +3,7 @@
 	import { envTagVariant } from '$lib/envTagVariant';
 	import Time from '$lib/ui/Time.svelte';
 	import { BodyShort, Tag } from '@nais/ds-svelte-community';
+	import { activityLogResourceLink } from '../../utils';
 
 	let {
 		data
@@ -15,7 +16,20 @@
 </script>
 
 <div>
-	Job <strong>{data.resourceName}</strong> triggered
+	Job
+	{#if data.environmentName}
+		<a
+			href={activityLogResourceLink(
+				data.environmentName,
+				data.resourceType,
+				data.resourceName,
+				data.teamSlug
+			)}>{data.resourceName}</a
+		>
+	{:else}
+		{data.resourceName}
+	{/if}
+	triggered
 	{#if data.environmentName}
 		in <Tag size="small" variant={envTagVariant(data.environmentName)}>{data.environmentName}</Tag>
 	{/if}.
