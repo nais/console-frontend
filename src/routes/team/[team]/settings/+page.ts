4a391100f185b222411c68adc8f594914d418448
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-04-09 10:35:05 +0200

    refactor: remove user access check from team settings load function

--- src/routes/team/[team]/settings/+page.ts
+++ src/routes/team/[team]/settings/+page.ts
@@ -1,19 +1,7 @@
 import { load_TeamSettings } from '$houdini';
 import { addPageMeta } from '$lib/utils/pageMeta';
-import { error } from '@sveltejs/kit';
-import { get } from 'svelte/store';
 
 export async function load(event) {
-	const pd = await event.parent();
-	const userInfoData = get(pd.UserInfo);
-
-	if (
-		!pd.viewerIsMember &&
-		!(userInfoData.data?.me.__typename === 'User' && userInfoData.data?.me.isAdmin)
-	) {
-		error(403, 'You are not allowed to view this page');
-	}
-
 	return {
 		...(await addPageMeta(event, { title: 'Settings' })),
 		...(await load_TeamSettings({
