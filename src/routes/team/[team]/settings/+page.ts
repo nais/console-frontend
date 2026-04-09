026d7343a206f62c059d6e8ed1ba918d2ec55105
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-04-09 10:54:37 +0200

    feat: add viewerIsMember variable to TeamSettings query and update load function

--- src/routes/team/[team]/settings/+page.ts
+++ src/routes/team/[team]/settings/+page.ts
@@ -2,12 +2,14 @@ import { load_TeamSettings } from '$houdini';
 import { addPageMeta } from '$lib/utils/pageMeta';
 
 export async function load(event) {
+	const parent = await event.parent();
 	return {
 		...(await addPageMeta(event, { title: 'Settings' })),
 		...(await load_TeamSettings({
 			event,
 			variables: {
-				team: event.params.team
+				team: event.params.team,
+				viewerIsMember: parent.viewerIsMember
 			}
 		}))
 	};
