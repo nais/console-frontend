345a7e6d787d3fcd8100428cf5ad6af6a888fcb9
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-04-21 09:22:06 +0200

    fix(activity): show environment in default activity text

--- src/lib/domain/activity/shared/texts/DefaultText.svelte
+++ src/lib/domain/activity/shared/texts/DefaultText.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	import { envTagVariant } from '$lib/envTagVariant';
 	import Time from '$lib/ui/Time.svelte';
-	import { BodyShort } from '@nais/ds-svelte-community';
+	import { BodyShort, Tag } from '@nais/ds-svelte-community';
 	import type { ActivityLogEntry } from './types';
 
 	let {
@@ -11,7 +12,12 @@
 </script>
 
 <div>
-	{data.message}<br />
+	{data.message}
+
+	{#if data.environmentName}
+		in <Tag size="small" variant={envTagVariant(data.environmentName)}>{data.environmentName}</Tag>
+	{/if}
+
 	<BodyShort textColor="subtle" size="small">
 		By {data.actor}
 		<Time time={data.createdAt} distance />
