2ffa76abb040b13e9f43631a5c87c8ac589dfc9d
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-05-04 18:25:35 +0200

    Trim workload activity texts

--- src/lib/domain/activity/workload/fragments.ts
+++ src/lib/domain/activity/workload/fragments.ts
@@ -38,12 +38,6 @@ export const workloadActivityEntryFragment = graphql(`
 				}
 			}
 		}
-		... on ClusterAuditActivityLogEntry {
-			clusterAuditData: data {
-				action
-				resourceKind
-			}
-		}
 		... on JobRunDeletedActivityLogEntry {
 			jobRunDeleted: data {
 				runName
@@ -62,125 +56,6 @@ export const workloadActivityEntryFragment = graphql(`
 			jobUpdated: data {
 				changedFields {
 					field
-				}
-			}
-		}
-		... on ConfigCreatedActivityLogEntry {
-			__typename
-		}
-		... on ConfigDeletedActivityLogEntry {
-			__typename
-		}
-		... on ConfigUpdatedActivityLogEntry {
-			configUpdated: data {
-				updatedFields {
-					field
-				}
-			}
-		}
-		... on GenericKubernetesResourceActivityLogEntry {
-			genericKubernetesData: data {
-				kind
-				changedFields {
-					field
-				}
-			}
-		}
-		... on RepositoryAddedActivityLogEntry {
-			__typename
-		}
-		... on RepositoryRemovedActivityLogEntry {
-			__typename
-		}
-		... on SecretCreatedActivityLogEntry {
-			__typename
-		}
-		... on SecretDeletedActivityLogEntry {
-			__typename
-		}
-		... on SecretValueAddedActivityLogEntry {
-			secretValueAdded: data {
-				valueName
-			}
-		}
-		... on SecretValueRemovedActivityLogEntry {
-			secretValueRemoved: data {
-				valueName
-			}
-		}
-		... on SecretValueUpdatedActivityLogEntry {
-			secretValueUpdated: data {
-				valueName
-			}
-		}
-		... on SecretValuesViewedActivityLogEntry {
-			__typename
-		}
-		... on CredentialsActivityLogEntry {
-			credentialsData: data {
-				permission
-			}
-		}
-		... on ServiceMaintenanceActivityLogEntry {
-			__typename
-		}
-		... on OpenSearchCreatedActivityLogEntry {
-			__typename
-		}
-		... on OpenSearchDeletedActivityLogEntry {
-			__typename
-		}
-		... on OpenSearchUpdatedActivityLogEntry {
-			opensearchData: data {
-				updatedFields {
-					field
-				}
-			}
-		}
-		... on PostgresDeletedActivityLogEntry {
-			__typename
-		}
-		... on PostgresGrantAccessActivityLogEntry {
-			postgresGrantAccessData: data {
-				grantee
-			}
-		}
-		... on UnleashInstanceCreatedActivityLogEntry {
-			__typename
-		}
-		... on UnleashInstanceDeletedActivityLogEntry {
-			__typename
-		}
-		... on UnleashInstanceUpdatedActivityLogEntry {
-			unleashInstanceUpdated: data {
-				allowedTeamSlug
-				revokedTeamSlug
-				updatedReleaseChannel
-			}
-		}
-		... on ValkeyCreatedActivityLogEntry {
-			__typename
-		}
-		... on ValkeyDeletedActivityLogEntry {
-			__typename
-		}
-		... on ValkeyUpdatedActivityLogEntry {
-			valkeyData: data {
-				updatedFields {
-					field
-				}
-			}
-		}
-		... on VulnerabilityUpdatedActivityLogEntry {
-			vulnerabilityUpdated: data {
-				identifier
-				package
-				severity
-				previousSuppression {
-					state
-				}
-				newSuppression {
-					state
 				}
 			}
 		}
