506b74feb82ac8773b5c5b7d749d8c1a2f0a4f95
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2026-06-04 11:08:01 +0200

    Add charts for topic size and partitions to kafka
    Add titles and descriptions to the charts on all insights pages

--- src/routes/team/[team]/[env]/kafka/[kafka]/(single)/insights/+page.svelte
+++ src/routes/team/[team]/[env]/kafka/[kafka]/(single)/insights/+page.svelte
@@ -4,6 +4,7 @@
 	import { PrometheusChartQueryInterval } from '$lib/chart/util';
 	import { changeParams } from '$lib/utils/searchparams';
 	import { ToggleGroup, ToggleGroupItem } from '@nais/ds-svelte-community';
+	import prettyBytes from 'pretty-bytes';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
@@ -18,7 +19,7 @@
 	const fullTopicName = $derived(`${teamSlug}.${topicName}`);
 </script>
 
-<div class="mb-2 flex justify-end">
+<div class="flex justify-end sticky top-0 z-100 p-2">
 	<ToggleGroup
 		bind:value={interval}
 		onchange={() => {
@@ -31,47 +32,76 @@
 	</ToggleGroup>
 </div>
 
-<!-- Messages In (Rate) -->
 <PrometheusChart
 	{environmentName}
+	title="Messages in"
+	description="Rate of messages produced to the topic, averaged over 5 minutes."
 	query={`sum by(topic) (rate(kafka_server_BrokerTopicMetrics_MessagesInPerSec_Count{topic="${fullTopicName}"}[5m]))`}
 	{interval}
 	labelFormatter={() => 'Messages In/sec'}
 	formatYValue={(value) => `${value.toFixed(2)} msg/s`}
 />
 
-<!-- Messages Per Second (Broker) -->
 <PrometheusChart
 	{environmentName}
+	title="Topic size"
+	description="Total size on disk across all partitions and replicas."
+	query={`sum by(topic) (kafka_log_Log_Size_Value{topic="${fullTopicName}"})`}
+	{interval}
+	labelFormatter={() => 'Topic Size'}
+	formatYValue={(value) => prettyBytes(value)}
+	colorizer={() => '#9467bd'}
+/>
+
+<PrometheusChart
+	{environmentName}
+	title="Partition size"
+	description="Size on disk per partition. Useful for spotting partition skew."
+	query={`sum by(partition) (kafka_log_Log_Size_Value{topic="${fullTopicName}"})`}
+	{interval}
+	labelFormatter={(labels) => {
+		const partition = labels.find((l) => l.name === 'partition')?.value;
+		return partition !== undefined ? `Partition ${partition}` : 'Partition';
+	}}
+	formatYValue={(value) => prettyBytes(value)}
+/>
+
+<PrometheusChart
+	{environmentName}
+	title="Messages per second (broker)"
+	description="Rate of messages produced to the topic, averaged over 2 minutes."
 	query={`sum by(topic) (rate(kafka_server_BrokerTopicMetrics_MessagesInPerSec_Count{topic="${fullTopicName}"}[2m]))`}
 	{interval}
 	labelFormatter={() => 'Messages/sec (Broker)'}
 	formatYValue={(value) => `${value.toFixed(2)} msg/s`}
 />
 
-<!-- Bytes In/sec -->
 <PrometheusChart
 	{environmentName}
+	title="Bytes in"
+	description="Rate of data produced to the topic by clients."
 	query={`sum by(topic) (rate(kafka_server_BrokerTopicMetrics_BytesInPerSec_Count{topic="${fullTopicName}"}[2m]))`}
 	{interval}
 	labelFormatter={() => 'Bytes In/sec'}
-	formatYValue={(value) => `${(value / 1024).toFixed(2)} KB/s`}
+	formatYValue={(value) => `${prettyBytes(value)}/s`}
 	colorizer={() => '#2ca02c'}
 />
 
-<!-- Bytes Out/sec -->
 <PrometheusChart
 	{environmentName}
+	title="Bytes out"
+	description="Rate of data consumed from the topic by clients."
 	query={`sum by(topic) (rate(kafka_server_BrokerTopicMetrics_BytesOutPerSec_Count{topic="${fullTopicName}"}[2m]))`}
 	{interval}
 	labelFormatter={() => 'Bytes Out/sec'}
-	formatYValue={(value) => `${(value / 1024).toFixed(2)} KB/s`}
+	formatYValue={(value) => `${prettyBytes(value)}/s`}
 	colorizer={() => '#d62728'}
 />
 
-<!-- Consumer Group Lag -->
 <PrometheusChart
 	{environmentName}
+	title="Consumer group lag"
+	description="Total number of messages consumer groups are behind the latest offset."
 	query={`sum(kafka_consumergroup_group_topic_sum_lag{topic="${fullTopicName}"})`}
 	{interval}
 	labelFormatter={() => 'Consumer Lag'}
