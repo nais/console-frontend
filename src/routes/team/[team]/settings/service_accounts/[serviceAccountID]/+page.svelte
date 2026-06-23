8157c0aa69fee9d15d06a4d15ac4c63ab028b59f
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2026-06-23 15:26:43 +0200

    fix: remove redundant back-link on service account detail page

--- src/routes/team/[team]/settings/service_accounts/[serviceAccountID]/+page.svelte
+++ src/routes/team/[team]/settings/service_accounts/[serviceAccountID]/+page.svelte
@@ -4,7 +4,6 @@
 	import ServiceAccountDetail from '$lib/domain/service-accounts/ServiceAccountDetail.svelte';
 	import GraphErrors from '$lib/ui/GraphErrors.svelte';
 	import PageModal from '$lib/ui/PageModal.svelte';
-	import { ChevronLeftIcon } from '@nais/ds-svelte-community/icons';
 	import type { PageProps } from './$types';
 	import AddBindingPage from './binding/add/+page.svelte';
 	import CreateTokenPage from './token/create/+page.svelte';
@@ -27,11 +26,6 @@
 	});
 </script>
 
-<a href="/team/{teamSlug}/settings/service_accounts" class="back-link">
-	<ChevronLeftIcon aria-hidden="true" />
-	Service Accounts
-</a>
-
 <GraphErrors errors={$query.errors} />
 {#if serviceAccount}
 	<ServiceAccountDetail
@@ -47,20 +41,3 @@
 {:else if page.state.modalHref?.includes('/token/create')}
 	<PageModal content={CreateTokenPage} header="Create API token" />
 {/if}
-
-<style>
-	.back-link {
-		display: inline-flex;
-		align-items: center;
-		gap: var(--ax-space-4);
-		font-size: var(--ax-font-size-small);
-		color: var(--ax-text-subtle);
-		text-decoration: none;
-		margin-bottom: var(--ax-space-16);
-	}
-
-	.back-link:hover {
-		color: var(--ax-text-neutral);
-		text-decoration: underline;
-	}
-</style>
