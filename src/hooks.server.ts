574ea988516d6cb2712794c0dcbcdcfdc8bdce6f
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2026-07-02 15:35:46 +0200

    Test server hook url override

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,13 +3,31 @@ import { logger } from '$lib/logger';
 import type { Handle, HandleFetch } from '@sveltejs/kit';
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
-	const cookies = event.request.headers.get('cookie');
+	const targetGraphqlEndpoint = import.meta.env.VITE_GRAPHQL_ENDPOINT;
+	const targetGraphqlUrl = targetGraphqlEndpoint ? new URL(targetGraphqlEndpoint) : undefined;
+	const requestUrl = new URL(request.url);
+
 	if (
-		(request.url.startsWith('http://nais-api/') ||
-			request.url == import.meta.env.VITE_GRAPHQL_ENDPOINT) &&
-		cookies
+		targetGraphqlUrl &&
+		requestUrl.pathname === targetGraphqlUrl.pathname &&
+		requestUrl.origin !== targetGraphqlUrl.origin
 	) {
-		request.headers.set('cookie', cookies);
+		requestUrl.protocol = targetGraphqlUrl.protocol;
+		requestUrl.host = targetGraphqlUrl.host;
+		request = new Request(requestUrl, request);
+	} else if (targetGraphqlUrl) {
+		console.log(`Request URL: ${requestUrl.href}, Target GraphQL URL: ${targetGraphqlUrl.href}`);
+	}
+
+	const cookies = event.request.headers.get('cookie');
+	if (cookies && targetGraphqlUrl) {
+		const outgoingUrl = new URL(request.url);
+		if (
+			outgoingUrl.origin === targetGraphqlUrl.origin &&
+			outgoingUrl.pathname === targetGraphqlUrl.pathname
+		) {
+			request.headers.set('cookie', cookies);
+		}
 	}
 
 	return fetch(request);
