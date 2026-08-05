9dd4bc61c17ca0e2e0bfd6ef1766205db40d7d99
Frode Sundby <frode.sundby@nav.no>
2026-08-05 14:59:21 +0200

    Improve instance group list text wrapping and columns

--- src/routes/team/[team]/[env]/app/[app]/instancegroup/[instancegroup]/+page.svelte
+++ src/routes/team/[team]/[env]/app/[app]/instancegroup/[instancegroup]/+page.svelte
@@ -629,7 +629,7 @@
 	}
 
 	.instances-list {
-		--instancegroup-list-columns: 22rem 7.5rem 1fr 14rem;
+		--instancegroup-list-columns: 3fr auto 2fr 2fr;
 		font-size: var(--ax-font-size-small);
 		color: var(--ax-text-neutral-subtle);
 	}
@@ -639,11 +639,11 @@
 	}
 
 	.meta-text {
-		white-space: nowrap;
+		word-break: break-word;
 	}
 
 	.exit-info {
-		overflow-wrap: anywhere;
+		word-break: break-word;
 	}
 
 	.instance-link {
