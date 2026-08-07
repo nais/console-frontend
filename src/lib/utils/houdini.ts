ceafd5483a05bee20593e42f04992b3efb64f36a
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-08-07 09:22:56 +0200

    fix(houdini): refactor NonExhaustive type to use constant for typename

--- src/lib/utils/houdini.ts
+++ src/lib/utils/houdini.ts
@@ -1,9 +1,9 @@
-type NonExhaustive = { readonly __typename: "non-exhaustive; don't match this" };
+const NON_EXHAUSTIVE_TYPENAME = "non-exhaustive; don't match this" as const;
+
+type NonExhaustive = { readonly __typename: typeof NON_EXHAUSTIVE_TYPENAME };
 
 export type Exhaustive<T> = T extends NonExhaustive ? never : T;
 
 export function exhaustive<T extends { __typename: string }>(items: readonly T[]): Exhaustive<T>[] {
-	return items.filter(
-		(item): item is Exhaustive<T> => item.__typename !== "non-exhaustive; don't match this"
-	);
+	return items.filter((item): item is Exhaustive<T> => item.__typename !== NON_EXHAUSTIVE_TYPENAME);
 }
