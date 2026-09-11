535cb568bd8f0aa801ffec28816ebe6bd9863f45
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2026-09-11 23:50:47 +0200

    refactor: simplify TeamVulnerabilities query by removing pagination variables and unused component

--- src/routes/team/[team]/vulnerabilities/+page.ts
+++ src/routes/team/[team]/vulnerabilities/+page.ts
@@ -1,8 +1,6 @@
 import { load_TeamVulnerabilities } from '$houdini';
 import { addPageMeta } from '$lib/utils/pageMeta';
 
-const rows = 10;
-
 export async function load(event) {
 	return {
 		...(await addPageMeta(event, {
@@ -12,20 +10,8 @@ export async function load(event) {
 		...(await load_TeamVulnerabilities({
 			event,
 			variables: {
-				team: event.params.team,
-				...paginationVariables('high', event.url),
-				...paginationVariables('elevated', event.url),
-				...paginationVariables('monitor', event.url)
+				team: event.params.team
 			}
 		}))
 	};
-}
-
-function paginationVariables(group: 'high' | 'elevated' | 'monitor', url: URL) {
-	const after = url.searchParams.get(`${group}After`);
-	const before = url.searchParams.get(`${group}Before`);
-
-	return before
-		? { [`${group}Before`]: before, [`${group}Last`]: rows }
-		: { [`${group}After`]: after, [`${group}First`]: rows };
 }
