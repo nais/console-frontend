37f5578250b38cc56bcc301cc1e08f65b91f0b79
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2026-09-18 08:44:42 +0200

    feat: add support for workloads without SBOM and enhance UI for known exploited vulnerabilities

--- src/routes/team/[team]/vulnerabilities/+page.svelte
+++ src/routes/team/[team]/vulnerabilities/+page.svelte
@@ -47,6 +47,8 @@
 					?.elevatedWorkloadCount}
 				monitorWorkloadCount={$TeamVulnerabilities.data.team.vulnerabilitySummary
 					?.monitorWorkloadCount}
+				noSbomWorkloadCount={$TeamVulnerabilities.data.team.vulnerabilitySummary
+					?.noSbomWorkloadCount}
 				knownExploitedCount={$TeamVulnerabilities.data.team.vulnerabilitySummary?.countsByPriority
 					?.knownExploited}
 			/>
